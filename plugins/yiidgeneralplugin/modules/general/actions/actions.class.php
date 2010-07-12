dc298820b542f163d6b84a07516c1ade68d072dd
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-12 13:18:11 +0000

    general plugin

---
+++ plugins/yiidgeneralplugin/modules/general/actions/actions.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * tags
+ *
+ * @package    communipedia
+ * @subpackage frontend-modules
+ * @version    $Id$
+ * @copyright  (c) 2009 ekaabo GmbH
+ */
+class generalActions extends sfActions {
+
+  /**
+   * Executes index action
+   *
+   */
+  public function executeIndex() {
+  }
+}
