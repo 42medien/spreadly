6cbed73514ebab65d33d6055d8e8ac01f1cbbe3b
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-12 15:07:22 +0000

    javascript logger action

--- plugins/yiidgeneralPlugin/modules/general/actions/actions.class.php
+++ plugins/yiidgeneralPlugin/modules/general/actions/actions.class.php
@@ -13,7 +13,10 @@ class generalActions extends sfActions {
    *
    */
   public function executeJs_log(sfWebRequest $pRequest) {
+    $lMsg = $pRequest->getParameter('msg', 'snirgel sitzt im');
+    $lUa = $pRequest->getParameter('useragent', 'affenstall');
     $this->getResponse()->setContentType('application/json');
-     file_put_contents(sfConfig::get('sf_log_dir').'/javascript.log', 'affen', FILE_APPEND);
+    file_put_contents(sfConfig::get('sf_log_dir').'/javascript.log', date('Y-m-d H:i') . ' - ' .$lMsg . ' - ' . $lUa . "\r\n", FILE_APPEND);
+    return sfView::NONE;
   }
 }
