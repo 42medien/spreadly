4d27b714d3706d67cd8381c8f1d16b7012fd7277
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-13 07:01:16 +0000

    extended schema

---
+++ lib/filter/doctrine/OauthNonceFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * OauthNonce filter form.
+ *
+ * @package    yiid
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class OauthNonceFormFilter extends BaseOauthNonceFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/model/doctrine/PersistentVariableTable.class.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class PersistentVariableTable extends Doctrine_Table
+{
+    
+    public static function getInstance()
+    {
+        return Doctrine_Core::getTable('PersistentVariable');
+    }
+}
