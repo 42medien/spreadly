26043585efc5a3133ae6319de4bafae5a9968b04
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-13 07:07:43 +0000

    remove error

--- web/js/main/include/platform-rogue.js
+++ web/js/main/include/platform-rogue.js
@@ -987,4 +987,128 @@ var Utils = {
 	    return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
 	                                                                    replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
 	}  
+};/**
+ * @combine platform
+ */
+
+/**
+ * class to handle the logging for thrown errors
+ * @author KM
+ * @version 1.0
+ */
+var ErrorLogger = {
+	//global var for error-msg send to action
+  aErrorMsg: 'Something went wrong',
+  //global var for user-agend send to action
+  aBrowserInfo: '',
+  
+  /**
+   * handles the correct login on dev or live-system
+   * @author KM
+   * @version 1.0
+   * @param string pMessage
+   * @param string pFileName
+   * @param string pLineNumber
+   */
+  initLog: function(pMessage, pFileName, pLineNumber) {
+    ErrorLogger.aErrorMsg = 'JS-Error: '+pMessage+' in ['+pFileName+'] on line ['+pLineNumber+']';
+    ErrorLogger.aBrowserInfo = ErrorLogger.getBrowserInfo();
+    ErrorLogger.doLog();
+  },
+  
+  /**
+   * sends the error-infos to logging-action and to console
+   * @author KM
+   */
+  doLog: function() {
+    //if we are on a dev-system: log
+    if(ErrorHandler.aDev == 1) {
+    	//take the messages as post-params   
+    	var options = {
+    		'msg':ErrorLogger.aErrorMsg,
+    		'useragent': ErrorLogger.aBrowserInfo
+    	}; 
+      //and send the request
+      jQuery.ajax({
+        type: "POST",
+        url: 'http://www.yiid.local/general/js_log',
+        dataType: "json",
+        data: options
+      });      
+      console.log(ErrorLogger.aErrorMsg);
+    }
+  },
+  
+  /**
+   * returns the BrowserInfo from UserAgent as string
+   * @author KM
+   * @return string lBrowserInfo
+   */
+  getBrowserInfo: function() {
+  	var lBrowserInfo = '';
+  	//call the jquery-browser-object and build a msg-string from the info
+    jQuery.each(jQuery.browser, function(i, val) {
+    	lBrowserInfo = lBrowserInfo+' '+i+' '+val;
+    }); 
+    return lBrowserInfo;	
+  }
+};
+
+
+ /**
+  * ErrorHandler: Methods to handle js-errors
+  * @author KM
+  * @version 1.0
+  */
+
+var ErrorHandler = {
+	
+	aDev: 0,
+  
+ /**
+  * catches the xhr-errors (called as callback in jquery.ajax.error)
+  * @author KM
+  * @param object xhr
+  * @param string ajaOptions
+  * @param string thrownError
+  * @param string pFile
+  * @param string pLineNumber
+  * @version 1.0
+  */  
+  catchXhrError: function(xhr, ajaxOptions, thrownError, pFile, pLineNumber){
+
+  	var lMsg = '';
+  	//if thrown error is set
+  	if(thrownError != undefined) {
+  		lMsg = thrownError;
+  	} else {
+  		lMsg = xhr.statusText;
+  	}
+    //build error message  	
+  	var lMessage = 'Status '+xhr['status']+' - '+lMsg;
+  	//if file/method where error thrown isn't set
+  	if(pFile === undefined) {
+  		pFile = '';
+  	}
+  	//if linenumber, where error thrown is undefined
+  	if(pLineNumber === undefined) {
+  		pLineNumber = '';
+  	}
+  	//init the logger with msg, filename and linenumber if is set
+    ErrorLogger.initLog(lMessage, pFile, pLineNumber);
+    return true;
+  },
+  
+  /**
+   * catches all window errors
+   */
+  catchGlobalError: function(){
+  	//if global window error
+		window.onerror = function(pMessage, pFileName, pLineNumber){
+			//init logger
+      ErrorLogger.initLog(pMessage, pFileName, pLineNumber);
+      //and return true
+      return true;
+		}; 	
+  }
 };
