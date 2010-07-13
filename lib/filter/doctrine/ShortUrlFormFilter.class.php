10db216c1e5392a0d6307abc8839a30179caf732
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-13 08:42:07 +0000

    extended schema

---
+++ lib/filter/doctrine/ShortUrlFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ShortUrl filter form.
+ *
+ * @package    yiid
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ShortUrlFormFilter extends BaseShortUrlFormFilter
+{
+  public function configure()
+  {
+  }
+}
