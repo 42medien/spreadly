0f27fa1522a865c1ef4a8a0a3abe48328ba92182
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-14 09:13:06 +0000

    two functions

--- lib/model/doctrine/CommunityTable.class.php
+++ lib/model/doctrine/CommunityTable.class.php
@@ -1,11 +1,38 @@
 <?php
+/**
+ * Enter description here...
+ *
+ * @author Matthias Pfefferle
+ */
+class CommunityTable extends Doctrine_Table {
+  /**
+   * return instance
+   *
+   * @return CommunityTable
+   */
+  public static function getInstance() {
+    return Doctrine_Core::getTable('Community');
+  }
 
+  /**
+   * find community by "slug"
+   *
+   * @author Matthias Pfefferle
+   * @param string $pSlug
+   * @return Community|null
+   */
+  public static function retrieveBySlug($pSlug) {
+    return self::getInstance()->findBy("slug", $pSlug);
+  }
 
-class CommunityTable extends Doctrine_Table
-{
-    
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('Community');
-    }
+  /**
+   * find community by "community"
+   *
+   * @author Matthias Pfefferle
+   * @param string $pCommunity
+   * @return Community|null
+   */
+  public static function retrieveByCommunity($pCommunity) {
+    return self::getInstance()->findBy("community", $pSlug);
+  }
 }
