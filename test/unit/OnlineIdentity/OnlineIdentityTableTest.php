d1e832f518bb652f8689a9f6fb77a379edda0b91
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-14 12:24:00 +0000

    onlineidentity tests

---
+++ test/unit/OnlineIdentity/OnlineIdentityTableTest.php
@@ -0,0 +1,13 @@
+<?php
+require_once dirname(__file__).'/../../lib/BaseTestCase.php';
+
+class OnlineIdentityTableTest extends BaseTestCase {
+
+  public function setUpBeforeClass() {
+
+  }
+
+  public function testGetName() {
+
+  }
+}
