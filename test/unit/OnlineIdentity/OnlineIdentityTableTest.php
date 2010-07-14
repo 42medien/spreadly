c5e367953604a2199289247ffa9d9b2c471af3f4
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-14 12:39:19 +0000

    fixture loading for oi tests

--- test/unit/OnlineIdentity/OnlineIdentityTableTest.php
+++ test/unit/OnlineIdentity/OnlineIdentityTableTest.php
@@ -3,11 +3,15 @@ require_once dirname(__file__).'/../../lib/BaseTestCase.php';
 
 class OnlineIdentityTableTest extends BaseTestCase {
 
-  public function setUpBeforeClass() {
-
+  public static function setUpBeforeClass() {
+    Doctrine::loadData('./fixtures');
   }
 
   public function testGetName() {
 
+  }
+
+
+  public function testGetOnlineIdentities() {
   }
 }
