69971fe748dcc43a18f602aa949c6eabfa461d94
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-15 15:52:07 +0000

    community tests

---
+++ test/unit/Community/CommunityTableTest.php
@@ -0,0 +1,16 @@
+<?php
+require_once dirname(__file__).'/../../lib/BaseTestCase.php';
+
+class  CommunityTableTest extends BaseTestCase {
+
+
+  public static function setUpBeforeClass() {
+    Doctrine::loadData(dirname(__file__).'/fixtures');
+  }
+
+  public function testRetrieveByDomain() {
+    $result = CommunityTable::retrieveByDomain("google");
+
+    $this->assertTrue(3 == count($result));
+  }
+}
