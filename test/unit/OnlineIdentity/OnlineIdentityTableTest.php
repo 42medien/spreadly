9005bba30d1905477e19af56da12ac879aea05f0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-15 14:22:48 +0000

    added unittests

--- test/unit/OnlineIdentity/OnlineIdentityTableTest.php
+++ test/unit/OnlineIdentity/OnlineIdentityTableTest.php
@@ -3,12 +3,25 @@ require_once dirname(__file__).'/../../lib/BaseTestCase.php';
 
 class  AuthTokenTableTest extends BaseTestCase {
 
+
   public static function setUpBeforeClass() {
     Doctrine::loadData(dirname(__file__).'/fixtures');
   }
 
-  public function testGetOnlineIdentities() {
+  public function testRetrieveByIdentifier() {
+    /*$c = new Community();
+    $c->setCommunity("yiid");
 
+    $o = new OnlineIdentity();
+    $o->setIdentifier("hugo");
+    $o->setIdentityType(OnlineIdentityTable::TYPE_IDENTITY);
+    $o->setCommunity($c);
+    $o->save();*/
+
+    $result = OnlineIdentityTable::retrieveByIdentifier("hugo", "google", OnlineIdentityTable::TYPE_IDENTITY);
+
+    $this->assertEquals('OnlineIdentity', get_class($result));
+    $this->assertEquals("hugo", $result->getIdentifier());
   }
 
 
