73e17930c327db43e56edbdae3b3b14c95ae7aca
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-16 14:37:26 +0000

    layout.php

--- apps/platform/modules/stream/templates/_new_stream.php
+++ apps/platform/modules/stream/templates/_new_stream.php
@@ -1,35 +1,35 @@
 <ul id="new_shares">
-  <li class="clearfix" id="stream_item_1">
+  <li class="clearfix stream_item" id="stream_item_1">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix item_active" id="stream_item_2">
+  <li class="clearfix stream_item item_active" id="stream_item_2">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_3">
+  <li class="clearfix stream_item" id="stream_item_3">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_4">
+  <li class="clearfix stream_item" id="stream_item_4">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_5">
+  <li class="clearfix stream_item" id="stream_item_5">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_6">
+  <li class="clearfix stream_item" id="stream_item_6">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_7">
+  <li class="clearfix stream_item" id="stream_item_7">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_8">
+  <li class="clearfix stream_item" id="stream_item_8">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_9">
+  <li class="clearfix stream_item" id="stream_item_9">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_10">
+  <li class="clearfix stream_item" id="stream_item_10">
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix" id="stream_item_11">
+  <li class="clearfix stream_item" id="stream_item_11">
     <?php include_partial('new_stream_item'); ?>
   </li>
 </ul>
@@ -37,7 +37,7 @@
 <!-- 
 <ul id="new_shares">
 <?php foreach($lShares as $lShare) { ?>
-  <li class="clearfix" id="stream_item_<?php echo $lShare->getId(); ?>">
+  <li class="clearfix stream_item" id="stream_item_<?php echo $lShare->getId(); ?>">
     <?php include_partial('new_stream_item', array($lShare)); ?>
   </li>
 <?php } ?>
