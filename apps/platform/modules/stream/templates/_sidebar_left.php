97afad4bcac4fabe870a76bd746f02f47c373155
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-16 13:19:54 +0000

    layout

--- apps/platform/modules/stream/templates/_sidebar_left.php
+++ apps/platform/modules/stream/templates/_sidebar_left.php
@@ -7,21 +7,36 @@
   </div>
           
   <p class="filter_headline">All Networks</p>
-  <ul class="normal_list" id="all_networks_list">
+  <ul class="normal_list clearfix" id="all_networks_list">
     <li><a href="#" class="icon_service icon_facebook">facebook.com</a></li>
     <li><a href="#" class="icon_service icon_twitter">twitter.com</a></li>
   </ul>
               
   <p class="filter_headline">Friends Active</p>
   <ul class="normal_list" id="friends_active_list">
-  <?php for($i=0;$i<=2;$i++) { ?>
     <li class="clearfix">      
       <a href="#">
         <?php echo image_tag('/img/global/yiid-logo.png', array('width' => 16, 'height' => 16, 'class' => 'icon_user')); ?>
         <span class="text_user"><?php echo truncate_text('Matthias Affenkopf', 18, '...'); ?></span>
       </a>
     </li>
-  <?php } ?>
+    <li class="clearfix">    
+      <a href="#">
+        <?php echo image_tag('/img/global/yiid-logo.png', array('width' => 16, 'height' => 16, 'class' => 'icon_user')); ?>
+        <span class="text_user"><?php echo truncate_text('Karina Mies', 18, '...'); ?></span>
+      </a>
+    </li>
+    <li class="clearfix">    
+      <a href="#">
+        <?php echo image_tag('/img/global/yiid-logo.png', array('width' => 16, 'height' => 16, 'class' => 'icon_user')); ?>
+        <span class="text_user"><?php echo truncate_text('Christian Weyand', 18, '...'); ?></span>
+      </a>
+    </li>
+    <li class="clearfix">    
+      <a href="#">
+        <?php echo image_tag('/img/global/yiid-logo.png', array('width' => 16, 'height' => 16, 'class' => 'icon_user')); ?>
+        <span class="text_user"><?php echo truncate_text('Dirk Müller', 18, '...'); ?></span>
+      </a>
   </ul>
 
 </div>
