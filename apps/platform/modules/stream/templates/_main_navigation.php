4a6d346409ddcb79cdb9d52a732b53f0f2f9e242
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-20 07:29:34 +0000

    layout

--- apps/platform/modules/stream/templates/_main_navigation.php
+++ apps/platform/modules/stream/templates/_main_navigation.php
@@ -1,12 +1,59 @@
-<div id="main_nav_outer">
+<!-- Bei Klick auf Whats Hot wird dem div mit ID main_nav_outer die Klasse .whats_hot_active zugewiesen -->
+<div id="main_nav_outer" class="whats_hot_active">
   <div id="main_nav_inner1" class="left"></div>
-  <div id="main_nav_inner2" class="left main_filter" data-obj='{"action":"stream/hot", "callback":"Stream.show"}'><p>What's Hot</p></div>
+  <div id="main_nav_inner2" class="left main_filter" data-obj='{"action":"stream/hot", "callback":"Stream.show"}'>
+    <p>What's Hot</p>
+  </div>
   <div id="main_nav_inner3" class="left"></div>
-  <div id="main_nav_inner4" class="left main_filter" data-obj='{"action":"stream/not", "callback":"Stream.show"}'><p>What's Not</p></div>
+  <div id="main_nav_inner4" class="left main_filter" data-obj='{"action":"stream/not", "callback":"Stream.show"}'>
+    <p>What's Not</p>
+  </div>
   <div id="main_nav_inner5" class="left"></div>
   <div class="right" id="new-nav">
     <div id="main_nav_inner6" class="left"></div>
-    <div id="main_nav_inner7" class="left main_filter" data-obj='{"action":"stream/new", "callback":"Stream.show"}'><p>New</p></div>
+    <div id="main_nav_inner7" class="left main_filter" data-obj='{"action":"stream/new", "callback":"Stream.show"}'>
+      <p>New</p>
+    </div>
+    <div id="main_nav_inner8" class="left"></div>
+  </div>
+</div>
+
+<!-- Bei Klick auf Whats Not wird dem div mit ID main_nav_outer die Klasse .whats_not_active zugewiesen -->
+<div id="main_nav_outer" class="whats_not_active">
+  <div id="main_nav_inner1" class="left"></div>
+  <div id="main_nav_inner2" class="left main_filter" data-obj='{"action":"stream/hot", "callback":"Stream.show"}'>
+    <p>What's Hot</p>
+  </div>
+  <div id="main_nav_inner3" class="left"></div>
+  <div id="main_nav_inner4" class="left main_filter" data-obj='{"action":"stream/not", "callback":"Stream.show"}'>
+    <p>What's Not</p>
+  </div>
+  <div id="main_nav_inner5" class="left"></div>
+  <div class="right" id="new-nav">
+    <div id="main_nav_inner6" class="left"></div>
+    <div id="main_nav_inner7" class="left main_filter" data-obj='{"action":"stream/new", "callback":"Stream.show"}'>
+      <p>New</p>
+    </div>
+    <div id="main_nav_inner8" class="left"></div>
+  </div>
+</div>
+
+<!-- Bei Klick auf Whats New wird dem div mit ID main_nav_outer die Klasse .whats_new zugewiesen -->
+<div id="main_nav_outer" class="whats_new">
+  <div id="main_nav_inner1" class="left"></div>
+  <div id="main_nav_inner2" class="left main_filter" data-obj='{"action":"stream/hot", "callback":"Stream.show"}'>
+    <p>What's Hot</p>
+  </div>
+  <div id="main_nav_inner3" class="left"></div>
+  <div id="main_nav_inner4" class="left main_filter" data-obj='{"action":"stream/not", "callback":"Stream.show"}'>
+    <p>What's Not</p>
+  </div>
+  <div id="main_nav_inner5" class="left"></div>
+  <div class="right" id="new-nav">
+    <div id="main_nav_inner6" class="left"></div>
+    <div id="main_nav_inner7" class="left main_filter" data-obj='{"action":"stream/new", "callback":"Stream.show"}'>
+      <p>New</p>
+    </div>
     <div id="main_nav_inner8" class="left"></div>
   </div>
 </div>
