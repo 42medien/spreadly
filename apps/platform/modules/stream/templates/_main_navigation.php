fe8a30aae45ba9cdcebc4dffc3fc887faa18fe76
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-20 06:56:10 +0000

    forjs

--- apps/platform/modules/stream/templates/_main_navigation.php
+++ apps/platform/modules/stream/templates/_main_navigation.php
@@ -1,12 +1,12 @@
 <div id="main_nav_outer">
   <div id="main_nav_inner1" class="left"></div>
-  <div id="main_nav_inner2" class="left"><p>What's Hot</p></div>
+  <div id="main_nav_inner2" class="left main_filter" data-obj='{"action":"stream/hot", "callback":"Stream.show"}'><p>What's Hot</p></div>
   <div id="main_nav_inner3" class="left"></div>
-  <div id="main_nav_inner4" class="left"><p>What's Not</p></div>
+  <div id="main_nav_inner4" class="left main_filter" data-obj='{"action":"stream/not", "callback":"Stream.show"}'><p>What's Not</p></div>
   <div id="main_nav_inner5" class="left"></div>
-  <div class="right">
+  <div class="right" id="new-nav">
     <div id="main_nav_inner6" class="left"></div>
-    <div id="main_nav_inner7" class="left"><p>New</p></div>
+    <div id="main_nav_inner7" class="left main_filter" data-obj='{"action":"stream/new", "callback":"Stream.show"}'><p>New</p></div>
     <div id="main_nav_inner8" class="left"></div>
   </div>
 </div>
