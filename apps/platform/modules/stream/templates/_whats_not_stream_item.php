57952b8715c62ab50b5556f7b744b479ca6f8759
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-21 13:50:09 +0000

    pager

--- apps/platform/modules/stream/templates/_whats_not_stream_item.php
+++ apps/platform/modules/stream/templates/_whats_not_stream_item.php
@@ -3,31 +3,43 @@
     <div class="whats_hot_stream_icon left">
       <span class="stream_icon stream_icon_location">&nbsp;</span>
     </div>
-    <div class="left">
-      <div class="info_area clearfix">
-          <div class="left so_headline_left">
-            <span class="icon_small_service icon_small_facebook left">&nbsp;</span>
-            <?php echo link_to('Matthias Affenkopf', '@homepage', array('class' => 'text_important left')); ?>
-            <span class="url left">via Twitter 2 minutes ago</span>
-          </div>
-          <div class="right so_headline_right">
-            <a href="#" class="icon_like icon_small_use like-dislike">3 like</a>
-            <a href="#" class="icon_dislike icon_small_use like-dislike">3 dislike</a>
-          </div>
+    <div class="left whats_hot_stream_head_left">
+      <div class="clearfix">
+        <div class="left so_headline_left">
+          <span class="icon_small_service icon_small_facebook left">&nbsp;</span>
+          <span class="url left">via Twitter 2 minutes ago</span>
         </div>
-        <div class="clearfix whats_hot_info_area">
-          <p class="text_important">Berlin, Germany</p>
-          <p class="url">type / 52° 30' 35.59° N 13° 22' 25.76° E</p>
-          <p>Intensive trials are under way as the world's largest solar-powered yacht...</p>
+        <div class="right so_headline_right">
+          <a href="#" class="icon_like icon_small_use like-dislike"><?php echo $pObject->getLikeCount(); ?>  like</a>
+          <a href="#" class="icon_dislike icon_small_use like-dislike"><?php echo $pObject->getDislikeCount(); ?> dislike</a>
         </div>
+      </div>
+      <div class="clearfix whats_hot_info_area">
+        <p class="text_important">Berlin, Germany</p>
+        <p class="url">type / 52° 30' 35.59° N 13° 22' 25.76° E</p>
+        <p>Intensive trials are under way as the world's largest solar-powered yacht...</p>
+      </div>
     </div>
     <div class="right preview_information">
       <?php echo image_tag('/img/test/map_preview.png'); ?>
     </div>
   </div>
-  
+
   <!-----------  Das folgende div muss auf style="display:none;" gesetzt und nur bei hover angezeigt werden ------------------------------>
   <div class="actions clearfix">
+    <div class="sharing_friends left">
+      
+      <?php for($i = 0; $i < 4; $i++) { ?>
+        <div class="sharing_friend left">
+          <div class="sharing_friend_outer">&nbsp;</div>
+          <div class="sharing_friend_inner">
+            <?php echo image_tag('/img/test/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?>
+          </div>
+        </div>
+      <?php } ?>
+      
+    </div>
+  
     <div class="action_content right">
       <a href="#" class="icon_comment icon_small_use text_action">comment</a>
       <a href="#" class="icon_favorite icon_small_use text_action">favorite</a>
