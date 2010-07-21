af92a5f909a7be7c4fcb38ddbd820166dda8d375
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-21 09:45:41 +0000

    pager

--- apps/platform/modules/stream/templates/_whats_hot_stream_item.php
+++ apps/platform/modules/stream/templates/_whats_hot_stream_item.php
@@ -3,23 +3,22 @@
     <div class="whats_hot_stream_icon left">
       <span class="stream_icon stream_icon_location">&nbsp;</span>
     </div>
-	  <div class="left">
-      <div class="info_area clearfix">
-          <div class="left so_headline_left">
-            <span class="icon_small_service icon_small_facebook left">&nbsp;</span>
-            <?php echo link_to('Matthias Affenkopf', '@homepage', array('class' => 'text_important left')); ?>
-            <span class="url left">via Twitter 2 minutes ago</span>
-          </div>
-          <div class="right so_headline_right">
-            <a href="#" class="icon_like icon_small_use like-dislike"><?php echo $pObject->getLikeCount(); ?>  like</a>
-            <a href="#" class="icon_dislike icon_small_use like-dislike"><?php echo $pObject->getDislikeCount(); ?> dislike</a>
-          </div>
+	  <div class="left whats_hot_stream_head_left">
+      <div class="clearfix">
+        <div class="left so_headline_left">
+          <span class="icon_small_service icon_small_facebook left">&nbsp;</span>
+          <span class="url left">via Twitter 2 minutes ago</span>
         </div>
-        <div class="clearfix whats_hot_info_area">
-	        <p class="text_important">Berlin, Germany</p>
-	        <p class="url">type / 52° 30' 35.59° N 13° 22' 25.76° E</p>
-	        <p>Intensive trials are under way as the world's largest solar-powered yacht...</p>
-	      </div>
+        <div class="right so_headline_right">
+          <a href="#" class="icon_like icon_small_use like-dislike"><?php echo $pObject->getLikeCount(); ?>  like</a>
+          <a href="#" class="icon_dislike icon_small_use like-dislike"><?php echo $pObject->getDislikeCount(); ?> dislike</a>
+        </div>
+      </div>
+      <div class="clearfix whats_hot_info_area">
+	      <p class="text_important">Berlin, Germany</p>
+	      <p class="url">type / 52° 30' 35.59° N 13° 22' 25.76° E</p>
+	      <p>Intensive trials are under way as the world's largest solar-powered yacht...</p>
+	    </div>
 	  </div>
 	  <div class="right preview_information">
       <?php echo image_tag('/img/test/map_preview.png'); ?>
