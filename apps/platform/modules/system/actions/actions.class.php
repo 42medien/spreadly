5eb3f5fd3c55e51128accba26aff241daf7cf441
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-21 15:11:13 +0000

    added system module for system pages like 404

---
+++ apps/platform/modules/system/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * system actions.
+ *
+ * @package    yiid
+ * @subpackage system
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class systemActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
