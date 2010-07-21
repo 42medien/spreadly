c1448fc3820c76bbfc9e686b31a441d620bd1bf0
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-21 15:54:34 +0000

    pager

--- apps/platform/modules/stream/templates/_whats_hot_stream_item.php
+++ apps/platform/modules/stream/templates/_whats_hot_stream_item.php
@@ -29,34 +29,11 @@
 	<div class="actions clearfix">
     <ul class="sharing_friends left">
       
-      <?php for($j=0;$j<5;$j++) { ?>
-	      <li class="sharing_friend left">
-		      <div class="sharing_friend_outer">&nbsp;</div>
-		      <div class="sharing_friend_inner">
-		        <?php echo image_tag('/img/test/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?>
-		      </div>
-          <div class="clearfix user_hover_area">
-            <div class="user_hover_area_top">
-              <div class="clearfix">
-	              <div class="sharing_user_image left">
-	                <?php echo image_tag('/img/test/yiid-logo.png', array('height' => '50px', 'width' => '50px')); ?>
-	              </div>
-	              <div class="sharing_user_info left">
-	                <h4>Matthias Affenkopf</h4>
-	                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore...</p>
-	              </div>
-	            </div>
-              <ul class="sharing_user_friends clearfix left">
-                <?php for($i = 0; $i < 5; $i++) { ?>
-                  <li class="left"><?php echo image_tag('/img/test/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?></li>
-                <?php } ?>
-              </ul>
-              <a href="/" class="sharing_user_link left">245 Friends</a>
-            </div>
-            <div class="user_hover_area_bottom"></div>
-          </div>
-		    </li>
-		  <?php } ?>
+      <?php include_partial('sharing_friend', array('lLikeType' => 'like')); ?>
+      <?php include_partial('sharing_friend', array('lLikeType' => 'dislike')); ?>
+      <?php include_partial('sharing_friend', array('lLikeType' => 'like')); ?>
+      <?php include_partial('sharing_friend', array('lLikeType' => 'dislike')); ?>
+      <?php include_partial('sharing_friend', array('lLikeType' => 'like')); ?>
 	    
     </ul>
 	
