a6c752c38190b2e94bac0ca74ce0f92fd830bee0
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-23 08:23:33 +0000

    sprites

--- apps/platform/modules/stream/templates/_sharing_friend.php
+++ apps/platform/modules/stream/templates/_sharing_friend.php
@@ -1,13 +1,13 @@
 <li class="sharing_friend left">
   <div class="sharing_friend_outer user_<?php echo $lLikeType; ?>"></div>
   <div class="sharing_friend_inner">
-    <?php echo image_tag('/img/test/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?>
+    <?php echo image_tag('/img/global/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?>
   </div>
   <div class="clearfix user_hover_area" style="display: none;">
     <div class="user_hover_area_top">
       <div class="clearfix">
         <div class="sharing_user_image left">
-          <?php echo image_tag('/img/test/yiid-logo.png', array('height' => '50px', 'width' => '50px')); ?>
+          <?php echo image_tag('/img/global/yiid-logo.png', array('height' => '50px', 'width' => '50px')); ?>
         </div>
         <div class="sharing_user_info left">
           <h4>Matthias Affenkopf</h4>
@@ -16,7 +16,7 @@
       </div>
       <ul class="sharing_user_friends clearfix left">
         <?php for($i = 0; $i < 5; $i++) { ?>
-          <li class="left"><?php echo image_tag('/img/test/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?></li>
+          <li class="left"><?php echo image_tag('/img/global/yiid-logo.png', array('height' => '30px', 'width' => '30px')); ?></li>
         <?php } ?>
       </ul>
       <a href="/" class="sharing_user_link left">245 Friends</a>
