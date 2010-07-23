3673448313c711bef8b8e610c30c592695297945
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-23 09:59:09 +0000

    landing

---
+++ apps/platform/templates/layout_landing.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_stylesheets() ?>
+    <?php //echo cdn_javascript_tag('include/'.sfConfig::get('app_release_name').'.min.js'); ?>
+  </head>
+  <body class="bg_light">
+    <div id="container" class="bd_round clearfix">
+
+      <div id="header" class="clearfix">
+        <div id="header_sub" class="left"></div>
+        <div id="header_main" class="left"></div>
+        <div id="header_supp" class="left">
+          <?php include_partial('global/main_navigation'); ?>
+        </div>
+	    </div>
+
+      <div id="content" class="clearfix">
+
+        
+
+	    </div>
+
+      <?php include_partial('global/footer'); ?>
+
+    </div>
+
+    <div id="footer" class="clearfix">
+
+	  </div>
+  <script type="text/javascript" src="/js/100_main/include/platform-<?php echo sfConfig::get('app_release_name') ?>.min.js"></script>
+  <script type="text/javascript">
+    jQuery(document).ready( function() {
+      <?php include_partial('global/js_init_general.js'); ?>
+      <?php if (has_slot('js_document_ready')) { ?>
+        <?php include_slot('js_document_ready'); ?>
+      <?php } ?>
+    });
+  </script>
+  </body>
+</html>
