8160eec3eff7749dd467b857baae5d1486c95c29
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-26 13:51:30 +0000

    landing

--- apps/platform/modules/stream/templates/_new_stream.php
+++ apps/platform/modules/stream/templates/_new_stream.php
@@ -1,5 +1,5 @@
 <ul id="new_shares">
-  <?php for($i=0;$i<=10;$i++) { ?>
+  <?php for($i=0;$i<=3;$i++) { ?>
   <li class="clearfix stream_item" id="stream_item_<?php echo $i ?>" data-obj='{"action":"StreamItem.getDetailAction", "callback":"ItemDetail.show", "itemid":"<?php echo $i ?>", "css":"{\"itemid\":\"stream_item_<?php echo $i ?>\"}"}'>
     <?php include_partial('new_stream_item'); ?>
   </li>
@@ -16,6 +16,4 @@
 </ul> 
  -->
 
-<div class="clearfix main_stream_pager">
-  <a href="/" class="pager_load_more">Load more...</a>
-</div>
+<?php include_partial('stream/stream_pager'); ?>
