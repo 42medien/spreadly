393de42dc90b0825461a1f10001f15c595147c93
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-26 13:45:09 +0000

    landing

--- apps/platform/modules/stream/templates/_new_stream.php
+++ apps/platform/modules/stream/templates/_new_stream.php
@@ -1,43 +1,15 @@
 <ul id="new_shares">
-  <li class="clearfix stream_item" id="stream_item_1">
+  <?php for($i=0;$i<=10;$i++) { ?>
+  <li class="clearfix stream_item" id="stream_item_<?php echo $i ?>" data-obj='{"action":"StreamItem.getDetailAction", "callback":"ItemDetail.show", "itemid":"<?php echo $i ?>", "css":"{\"itemid\":\"stream_item_<?php echo $i ?>\"}"}'>
     <?php include_partial('new_stream_item'); ?>
   </li>
-  <li class="clearfix stream_item item_active" id="stream_item_2">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_3">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_4">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_5">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_6">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_7">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_8">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_9">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_10">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
-  <li class="clearfix stream_item" id="stream_item_11">
-    <?php include_partial('new_stream_item'); ?>
-  </li>
+  <?php } ?>
 </ul>
 
 <!-- 
 <ul id="new_shares">
 <?php foreach($lShares as $lShare) { ?>
-  <li class="clearfix stream_item" id="stream_item_<?php echo $lShare->getId(); ?>">
+  <li class="clearfix stream_item" id="stream_item_<?php echo $lShare->getId(); ?>" data-obj='{"action":"StreamItem.getDetailAction", "callback":"ItemDetail.show", "itemid":"<?php echo $i ?>", "css":"{\"itemid\":\"stream_item_<?php echo $i ?>\"}"}'>>
     <?php include_partial('new_stream_item', array($lShare)); ?>
   </li>
 <?php } ?>
