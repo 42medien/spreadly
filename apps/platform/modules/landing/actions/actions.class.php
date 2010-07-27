69a7c8a7ef0f10ebf44502f4df03e3f298f92f03
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-27 11:59:29 +0000

    changed default path

--- apps/platform/modules/landing/actions/actions.class.php
+++ apps/platform/modules/landing/actions/actions.class.php
@@ -1,22 +1,22 @@
 <?php
-
 /**
  * landing actions.
  *
+ * @author     Matthias Pfefferle
  * @package    yiid
  * @subpackage landing
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class landingActions extends sfActions
-{
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-  	
+class landingActions extends sfActions {
+  /**
+   * Executes index action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeIndex(sfWebRequest $request) {
+    if ($this->getUser()->isAuthenticated()) {
+      $this->redirect("@stream_hot");
+    }
   }
 }
