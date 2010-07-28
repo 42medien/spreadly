0bb162c2323f5b68184c1dfd973d236fb115ef93
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-28 07:46:36 +0000

    i18n __()

--- apps/platform/modules/landing/templates/indexSuccess.php
+++ apps/platform/modules/landing/templates/indexSuccess.php
@@ -1,9 +1,9 @@
 <div id="signin_area">
   <div id="signin_via_services" <?php if ($pAuthType != "delegated") { echo 'style="display: none;"'; } ?>>
-	  <h3>No registration needed - please use your favourite service to login</h3>
+	  <h3><?php echo __('No registration needed - please use your favourite service to login', null, "platform"); ?></h3>
 	  <p>
-      (For returning users: please use the same service you've used before or login with user and password
-      <a href="/" onclick="Utils.toggleTwoAreas('signin_via_username', 'signin_via_services');return false;">here</a>.)
+      <?php echo __('(For returning users: please use the same service you have used before or login with user and password %1.)', 
+        array('%1' => '<a href="/" onclick="Utils.toggleTwoAreas(\'signin_via_username\', \'signin_via_services\');return false;">here</a>'), "platform"); ?>
 	  </p>
 
 	  <ul class="service_icons_big normal_list clearfix">
@@ -18,17 +18,14 @@
 	    </li>
 	  </ul>
 
-	  <p>If you haven't used the Yiid button before, we will automatically create an account for you on Yiid.com, where you can manage your services,
-	  see your and your friends "Likes" and much more. By using Yiid you agree to the <a href="/">Terms and Conditions</a> of Yiid.
-	  If you're a recurring user, simply choose a service that you've used before - we will know you and enable your former settings.</p>
+	  <p><?php echo __('If you have not used the Yiid button before, we will automatically create an account for you on Yiid.com, where you can manage your services,
+	  see your and your friends "Likes" and much more. By using Yiid you agree to the %1 of Yiid.
+	  If you are a recurring user, simply choose a service that you have used before - we will know you and enable your former settings.', array('%1' => '<a href="/">Terms and Conditions</a>'), "platform"); ?></p>
 	</div>
 
 	<div id="signin_via_username" <?php if ($pAuthType != "basic") { echo 'style="display: none;"'; } ?>>
-    <h3>Returning user? Log in here.</h3>
-    <p>
-      (First time users please go back
-      <a href="/" onclick="Utils.toggleTwoAreas('signin_via_services', 'signin_via_username');return false;">here</a>.)
-    </p>
+    <h3><?php echo __('Returning user? Log in here.', null, "platform"); ?></h3>
+    <p><?php echo __('(First time users please go back %1.)', array('%1' => '<a href="/" onclick="Utils.toggleTwoAreas(\'signin_via_services\', \'signin_via_username\');return false;">here</a>'), "platform"); ?></p>
 
     <form action="<?php echo url_for('auth/basic'); ?>" method="post" id="signin_via_username_form">
     
@@ -53,31 +50,32 @@
 
    </form>
 
-   <p>Password forgotten? Then simply login with a service you've used before ... <a href="/" onclick="Utils.toggleTwoAreas('signin_via_services', 'signin_via_username');return false;">here</a>.</p>
+   <p><?php echo __('Password forgotten? Then simply login with a service you have used before ... %1.', 
+   array('%1' => '<a href="/" onclick="Utils.toggleTwoAreas(\'signin_via_services\', \'signin_via_username\');return false;">here</a>'), 'platform'); ?></p>
 	</div>
 </div>
 
 <div class="center_area clearfix">
-  <h2>What is Yiid?</h2>
+  <h2><?php echo __('What is Yiid?', null, 'platform'); ?></h2>
 </div>
 
 <div id="what_is_yiid_description" class="clearfix">
   <div class="description_with_image left">
-    <p>Find cool new articles, products or people on the web and use the Yiid button to share what you've found with your friends.</p>
+    <p><?php echo __('Find cool new articles, products or people on the web and use the Yiid button to share what you have found with your friends.', null, 'platform'); ?></p>
     <div id="yiid_description1" class="center_area"></div>
   </div>
 
   <div class="description_wave left"></div>
 
   <div class="description_with_image left">
-    <p>Share to Facebook, Twitter and many more of your profiles simultanously with just one click.</p>
+    <p><?php echo __('Share to Facebook, Twitter and many more of your profiles simultanously with just one click.', null, 'platform'); ?></p>
     <div id="yiid_description2" class="center_area"></div>
   </div>
 
   <div class="description_wave left"></div>
 
   <div class="description_with_image left">
-    <p>See all your friends shares on Facebook, Twitter and many other networks combined in your personal dashboard.</p>
+    <p><?php echo __('See all your friends shares on Facebook, Twitter and many other networks combined in your personal dashboard.', null, 'platform'); ?></p>
     <div id="yiid_description3" class="center_area"></div>
   </div>
 </div>
--- apps/platform/templates/_main_navigation.php
+++ apps/platform/templates/_main_navigation.php
@@ -1,5 +1,5 @@
 <ul id="nav_main" class="right clearfix">
-  <li><?php echo link_to('Home', '@homepage', array('id' => 'nav_home')); ?></li>
-  <li><?php echo link_to('Profile', '@homepage', array('id' => 'nav_profile')); ?></li>
-  <li id="nav_main_settings"><?php echo link_to('Settings', '@homepage', array('id' => 'nav_settings')); ?></li>
+  <li><?php echo link_to(__('Home'), '@homepage', array('id' => 'nav_home')); ?></li>
+  <li><?php echo link_to(__('Profile'), '@homepage', array('id' => 'nav_profile')); ?></li>
+  <li id="nav_main_settings"><?php echo link_to(__('Settings'), '@homepage', array('id' => 'nav_settings')); ?></li>
 </ul>
