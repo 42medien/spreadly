d404ba4438e77d3ad254ccf5e95380520d20f11f
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-07-28 09:25:04 +0000

    css combine

--- apps/platform/templates/_includes.php
+++ apps/platform/templates/_includes.php
@@ -1,4 +1,9 @@
-<?php // include_stylesheets() ?>
-<?php echo cdn_stylesheet_tag('include/'.sfConfig::get('app_release_name').'.min.css'); ?>
+<?php if(sfConfig::get('app_settings_dev') == 1) { ?>
+  <?php echo cdn_stylesheet_tag('001_layout.css'); ?>
+  <?php echo cdn_stylesheet_tag('002_color.css'); ?>
+  <?php echo cdn_stylesheet_tag('003_typo.css'); ?>
+<?php } else { ?>
+  <?php echo cdn_stylesheet_tag('include/'.sfConfig::get('app_release_name').'.min.css'); ?>
+<?php } ?>
     
 <?php //echo cdn_javascript_tag('include/'.sfConfig::get('app_release_name').'.min.js'); ?>
