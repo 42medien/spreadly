0ab1d70055f23befa898ba5597146b88d64ff171
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-06 13:10:29 +0000

    userrelation migration bullshit

--- lib/model/doctrine/UserRelationTable.class.php
+++ lib/model/doctrine/UserRelationTable.class.php
@@ -63,7 +63,7 @@ class UserRelationTable extends Doctrine_Table
 
     $lQueryArray = array('$addToSet' => array('contacts_oi' => array('$each' => array_filter($pIdentityIds)), 'contact_uid' => array('$each' => array_filter($pContactUserIds)) ));
 
-    print_r($lQueryArray);die();
+    var_dump($lQueryArray);die();
     return $lCollection->update(array('user_id' => $pUserId), $lQueryArray, array('upsert' => true));
   }
 
