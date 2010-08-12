eea9ea4f87ad6623c37f09643d90c22674268fd4
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-12 07:29:34 +0000

    zeuchs

--- apps/platform/templates/_footer.php
+++ apps/platform/templates/_footer.php
@@ -7,4 +7,4 @@
 <?php echo cdn_image_tag("/img/global/ajax-loader-bar-circle.gif", array('id' => 'general-ajax-loader')); ?>
 
 
-<?php YiidStatsSingleton::trackVisit('http://www.yiid.local'.$_SERVER['REQUEST_URI'], 'like', 'de', 1); ?>
+<?php // YiidStatsSingleton::trackVisit('http://www.yiid.local'.$_SERVER['REQUEST_URI'], 'like', 'de', 1); ?>
