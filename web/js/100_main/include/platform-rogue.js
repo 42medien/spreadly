01d65702a5be20b12dac698a408fff44a0bbf106
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-17 11:17:55 +0000

    login areas

--- web/js/100_main/include/platform-rogue.js
+++ web/js/100_main/include/platform-rogue.js
@@ -1632,6 +1632,36 @@ replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
 /**
  * @combine platform
  */
+ 
+ 
+ 
+/**
+ * Class to handle global landing effects
+ * @author Christian Schätzle
+ * @version 1.0
+ */
+var Landing = {
+		
+		init: function() {
+			Landing.toggleLoginAreas();
+		},
+		
+		/**
+	   * @description toggles between the two login areas
+	   */  
+		toggleLoginAreas: function() {
+			
+			jQuery('.toggle_login_area').live("click", function() {
+				var id = this.id;
+				var area_ids = Utils.explode('-', id);
+				
+				Utils.toggleTwoAreas(area_ids[0], area_ids[1]);
+			});
+		}
+		
+}/**
+ * @combine platform
+ */
 
 /**
  * handles the behaviour of the details of a social objects (sidebar-right)
