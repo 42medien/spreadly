c3b4734f85ec1454789e5a6b52949a2faa12dba7
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-18 13:59:31 +0000

    togglevalue

---
+++ web/js/100_main/global/104_jquery.toggleValue.js
@@ -0,0 +1,20 @@
+/**
+ * @combine platform
+ */
+jQuery.fn.toggleValue = function(){
+  var lText = jQuery(this).val();
+  var lNewText = '';
+  jQuery(this).bind('click', function() {
+    lNewText = jQuery(this).val();
+    if(lNewText == lText){
+      jQuery(this).val('');
+    }
+  });
+
+  jQuery(this).blur(function(){
+    lNewText = jQuery(this).val();
+    if(lNewText == '') {
+      jQuery(this).val(lText);
+    }
+  });
+};
