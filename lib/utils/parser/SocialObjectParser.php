5fb18eba12ffb20dc01889375e5b12f69d985dc4
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-23 09:52:49 +0000

    initial release

--- lib/utils/parser/SocialObjectParser.php
+++ lib/utils/parser/SocialObjectParser.php
@@ -2,7 +2,21 @@
 /**
  * a parser for social objects
  *
+ * @author Matthias Pfefferle
  */
 class SocialObjectParser {
-  
+
+  /**
+   * Enter description here...
+   *
+   * @param string $pUrl
+   */
+  public static function fetch($pUrl) {
+    $lResult = OpenGraph::fetch($pUrl);
+
+
+    var_dump(get_meta_tags($pUrl));
+
+    var_dump($lResult);
+  }
 }
