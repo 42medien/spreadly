3a493ecfcb3c752f713247e5a9fa98a1490b2880
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-08-23 07:45:32 +0000

    language selection

--- apps/platform/templates/_main_navigation.php
+++ apps/platform/templates/_main_navigation.php
@@ -1,13 +1,13 @@
-<div style="height: 60px;" class="right clearfix">
-	<div style="height: 20px;text-align: right;padding: 5px 28px 0 0;" class="clearfix">
+<div id="nav_main_area" class="right clearfix">
+	<div id="nav_main_supp" class="clearfix">
     <div class="right">
       <?php echo link_to(__('Logout'), '@auth_signout', array('class' => 'url')); ?>
     </div>
-    <div class="right" style="margin-right: 20px;">
+    <div class="right" id="language_switch_area">
       <?php include_component('system', 'language_switch_icons'); ?>
     </div>
 	</div>
-	<ul id="nav_main" class="clearfix" style="height: 25px;">
+	<ul id="nav_main" class="clearfix">
 	  <li><?php echo link_to(__('Home'), '@homepage', array('id' => 'nav_home')); ?></li>
 	  <li><?php echo link_to(__('Profile'), '@homepage', array('id' => 'nav_profile')); ?></li>
 	  <li id="nav_main_settings"><?php echo link_to(__('Settings'), '@homepage', array('id' => 'nav_settings')); ?></li>
