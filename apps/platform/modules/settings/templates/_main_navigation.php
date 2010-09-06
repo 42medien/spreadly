af0d5072b681c5768cf68d08e9f5bc39c9df1d5f
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-06 09:50:43 +0000

    delete settings

--- apps/platform/modules/settings/templates/_main_navigation.php
+++
@@ -1,7 +0,0 @@
-<div id="main_nav_outer" class="whats_hot_active">
-  <div id="main_nav_inner1" class="left"></div>
-  <div id="main_nav_inner2" class="left main_filter">
-    <p><?php echo __('SETTINGS_NAV_PRIVACY', null, 'platform'); ?></p>
-  </div>
-  <div id="main_nav_inner3" class="left"></div>
-</div>
--- apps/platform/modules/settings/templates/_sidebar_right.php
+++
@@ -1,3 +0,0 @@
-<div id="stream_right" class="bg_light bd_normal_light">
-  <?php //include_partial('stream/item_detail', array('pObject' => $pObject, 'pActivities' => $pActivities)); ?>
-</div>
