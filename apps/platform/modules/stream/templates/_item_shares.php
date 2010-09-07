261c01675621320d77aa79a96b7f2e15be8482db
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-07 10:00:30 +0000

    hover

--- apps/platform/modules/stream/templates/_item_shares.php
+++ apps/platform/modules/stream/templates/_item_shares.php
@@ -1,34 +1,34 @@
 <?php use_helper('YiidUrl', 'Avatar'); ?>
 <?php if(count($pActivities) > 0) { ?>
-<?php foreach ($pActivities as $lActivity) { ?>
-<?php $lUser = UserTable::getInstance()->retrieveByPK($lActivity->getUId()); ?>
-  <li class="clearfix">
-    <div class="so_share_image left">
-      <?php echo avatar_tag($lUser->getDefaultAvatar(), 30, array('alt' => $lUser->getFullname(), 'class' => '', 'rel' => '')); ?>
-    </div>
-    <div class="so_share_spread left">
-      
-    </div>
-    <div class="so_share_information left">
-      <span class="user_share text_important"><?php echo $lUser->getFullname(); ?></span>
-      <span class="url"><?php echo __('%2 ago', array('%2' => $lActivity->getPublishingTime()), 'platform'); ?></span><br/>
-      <span class="url"><?php echo __('Shared with', null, 'platform'); ?></span>
-      <?php $lActivityCids = $lActivity->getCids(); ?>
-      <?php foreach($lActivityCids as $lCid) { ?>
-        <?php $lCommunityName = CommunityTable::getInstance()->retrieveByPk($lCid)->getName(); ?>
-        <?php $lCommunitySlug = CommunityTable::getInstance()->retrieveByPk($lCid)->getSlug(); ?>
-        <span class="icon_small_service_right icon_small_<?php echo $lCommunitySlug; ?>" title="<?php echo __('Shared with %1', array('%1' => $lCommunityName), 'platform'); ?>">&nbsp;</span>
-      <?php } ?>
-    </div>
-    <div class="right">
-      <?php if($lActivity->getScore() == 1) { ?>
-        <span class="thumb_up icon_detail_right">&nbsp;</span>
-      <?php } else {?>
-        <span class="thumb_down icon_detail_right">&nbsp;</span>
-      <?php } ?>
-    </div>
-  </li>
-<?php } ?>
+	<?php foreach ($pActivities as $lActivity) { ?>
+	<?php $lUser = UserTable::getInstance()->retrieveByPK($lActivity->getUId()); ?>
+	  <li class="clearfix">
+	    <div class="so_share_image left">
+	      <?php echo avatar_tag($lUser->getDefaultAvatar(), 30, array('alt' => $lUser->getFullname(), 'class' => '', 'rel' => '')); ?>
+	    </div>
+	    <div class="so_share_spread left">
+	      
+	    </div>
+	    <div class="so_share_information left">
+	      <span class="user_share text_important"><?php echo $lUser->getFullname(); ?></span>
+	      <span class="url"><?php echo __('%2 ago', array('%2' => $lActivity->getPublishingTime()), 'platform'); ?></span><br/>
+	      <span class="url"><?php echo __('Shared with', null, 'platform'); ?></span>
+	      <?php $lActivityCids = $lActivity->getCids(); ?>
+	      <?php foreach($lActivityCids as $lCid) { ?>
+	        <?php $lCommunityName = CommunityTable::getInstance()->retrieveByPk($lCid)->getName(); ?>
+	        <?php $lCommunitySlug = CommunityTable::getInstance()->retrieveByPk($lCid)->getSlug(); ?>
+	        <span class="icon_small_service_right icon_small_<?php echo $lCommunitySlug; ?>" title="<?php echo __('Shared with %1', array('%1' => $lCommunityName), 'platform'); ?>">&nbsp;</span>
+	      <?php } ?>
+	    </div>
+	    <div class="right">
+	      <?php if($lActivity->getScore() == 1) { ?>
+	        <span class="thumb_up icon_detail_right">&nbsp;</span>
+	      <?php } else {?>
+	        <span class="thumb_down icon_detail_right">&nbsp;</span>
+	      <?php } ?>
+	    </div>
+	  </li>
+	<?php } ?>
 <?php } else { ?>
   <li class="clearfix">
     <?php echo __("NO_ENTRIES"); ?>
