baa2b1a97d4f2724fe62d93c6b7a45ed4120bf7c
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-13 14:30:06 +0000

    italiano

---
+++ web/w/like/inc/lang/it.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * the italian translations
+ *
+ * @author Matthias Pfefferle
+ * @author Thomas Huhn
+ */
+$i18n = array(
+  "global" => array(
+    "SETTINGS_TITLE" => "Cambia i settaggi",
+    "SETTINGS_VALUE" => "Settaggi",
+),
+  "like" => array(
+    "POS_BUTTON_TITLE" => "Mi piace",
+    "NEG_BUTTON_TITLE" => "Non mi piace",
+    "POS_BUTTON_VALUE" => "Piace",
+    "POS_BUTTON_ACTION_VALUE" => "Ti piace",
+    "NEG_BUTTON_ACTION_VALUE" => "Non ti piace",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => "ad altro/i piace",
+    "NEG_TEXT_VALUE_1" => "Tu e ",
+    "NEG_TEXT_VALUE_2" => " ad altro/i non piace",
+),
+  "pro" => array(
+    "POS_BUTTON_TITLE" => "Sono favorevole",
+    "NEG_BUTTON_TITLE" => "Sono contrario",
+    "POS_BUTTON_VALUE" => "Favorevole",
+    "POS_BUTTON_ACTION_VALUE" => "Sei favorevole",
+    "NEG_BUTTON_ACTION_VALUE" => "Sei contrario",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => " altro/i siete favorevoli",
+    "NEG_TEXT_VALUE_1" => "Tu e ",
+    "NEG_TEXT_VALUE_2" => " altro/i siete contrari",
+),
+  "recommend" => array(
+    "POS_BUTTON_TITLE" => "Raccomando",
+    "NEG_BUTTON_TITLE" => "Non raccomando",
+    "POS_BUTTON_VALUE" => "Raccoamnda",
+    "POS_BUTTON_ACTION_VALUE" => "Lo raccomandi",
+    "NEG_BUTTON_ACTION_VALUE" => "Non lo raccomandi",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => " altro/i lo raccomandate.",
+    "NEG_TEXT_VALUE_1" => "Tu e ",
+    "NEG_TEXT_VALUE_2" => "altro/i non lo raccomandate.",
+
+),
+  "visit" => array(
+    "POS_BUTTON_TITLE" => "Ci sono stato",
+    "NEG_BUTTON_TITLE" => "Non ci sono stato",
+    "POS_BUTTON_VALUE" => "sono stato",
+    "POS_BUTTON_ACTION_VALUE" => "Ci sei stato",
+    "NEG_BUTTON_ACTION_VALUE" => "Non ci sei stato",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => " altro/i ci siete stati.",
+    "NEG_TEXT_VALUE_1" => "Tu e ",
+    "NEG_TEXT_VALUE_2" => " altro/i non ci siete stati.",
+),
+  "nice" => array(
+    "POS_BUTTON_TITLE" => "Questo è stupendo",
+    "NEG_BUTTON_TITLE" => "Questo è brutto",
+    "POS_BUTTON_VALUE" => "Stupendo",
+    "POS_BUTTON_ACTION_VALUE" => "è stupendo",
+    "NEG_BUTTON_ACTION_VALUE" => "è brutto",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => " altro/i pensano che sia stupendo.",
+    "NEG_TEXT_VALUE_1" => "tu e ",
+    "NEG_TEXT_VALUE_2" => " altro/i pensano che sia brutto.",
+),
+   "buy" => array(
+  "POS_BUTTON_TITLE" => "Lo voglio",
+  "NEG_BUTTON_TITLE" => "Non lo voglio",
+  "POS_BUTTON_VALUE" => "Voglio!",
+  "POS_BUTTON_ACTION_VALUE" => "Lo vuoi",
+  "NEG_BUTTON_ACTION_VALUE" => "Non lo vuoi",
+  "POS_TEXT_VALUE_1" => "Tu e ",
+  "POS_TEXT_VALUE_2" => " altro/i lo vogliono.",
+  "NEG_TEXT_VALUE_1" => "Tu e ",
+  "NEG_TEXT_VALUE_2" => " altro/i non lo vogliono.",
+),
+  "rsvp" => array(
+    "POS_BUTTON_TITLE" => "Assisterò",
+    "NEG_BUTTON_TITLE" => "Non assisterò ",
+    "POS_BUTTON_VALUE" => "Contami",
+    "POS_BUTTON_ACTION_VALUE" => "assistendo",
+    "NEG_BUTTON_ACTION_VALUE" => "non assistendo",
+    "POS_TEXT_VALUE_1" => "Tu e ",
+    "POS_TEXT_VALUE_2" => " altro/i assisterete.",
+    "NEG_TEXT_VALUE_1" => "Tu e ",
+    "NEG_TEXT_VALUE_2" => " altro/i avete declinato.",
+));
+?>
