0440b9d38b935204f95449c06b38860548fffe15
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-15 11:25:54 +0000

    two first daemons

---
+++ batch/daemons/importContacts.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php -q
+<?php
+// Setup
+$options = array(
+    'appName' => 'importContacts',
+    'appDir' => dirname(__FILE__),
+    'appDescription' => '',
+    'authorName' => 'Matthias Pfefferle',
+    'authorEmail' => 'matthias@ekaabo.de',
+    'sysMaxExecutionTime' => '0',
+    'sysMaxInputTime' => '0',
+    'sysMemoryLimit' => '64M',
+    'appRunAsGID' => 0,
+    'appRunAsUID' => 0,
+);
+
+?>
