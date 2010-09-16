cf131ea196329ecf577ee2e3c51a993f0bae488c
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-16 07:35:36 +0000

    added to widget js

--- web/js/100_main/global/107_jquery.topslider.js
+++ web/js/100_main/global/107_jquery.topslider.js
@@ -1,36 +1,37 @@
 /**
- * @nocombine platform
+ * @combine platform
+ * @combine widget
  */
 
 /**
  * jQuery-Plugin to fadein/out an message list
- * 
+ *
  * @param object pParams
  * @param string pParams[msg]: jsonstring with messages
  * @param string pParams[speed]: optional for speed of fadein/out
  * @param string pParams[timeout]: optional for time to fadeout
- * 
+ *
  */
 jQuery.fn.topslider = function(pParams) {
   var lParams = pParams, lSpeed, lTimeout, lThis, lMsgs;
   lSpeed = (lParams.speed)?lParams.speed:1500;
   lTimeout = (lParams.timeout)?lParams.timeout:2000;
-  lThis = this;  
+  lThis = this;
   lMsgs = jQuery.parseJSON(lParams["msg"]);
   jQuery(lThis).empty();
   var lList = "<ul class='error-msg-list'>";
-  for(var i=0; i<lMsgs.length; i++) { 
+  for(var i=0; i<lMsgs.length; i++) {
     lList += "<li>";
     lList += lMsgs[i];
     lList += "</li>";
   }
   lList += "</ul>";
-  
+
   jQuery(lThis).append(lList);
   jQuery(lThis).show('blind', null, lSpeed);
   window.setTimeout(function() {
     jQuery(lThis).hide('blind', null, lSpeed);
-    jQuery(lThis).empty(); 
+    jQuery(lThis).empty();
   },lTimeout);
 };
 
