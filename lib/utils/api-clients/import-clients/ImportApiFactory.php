3fbef944eeb48d87355724095921db65724ec94d
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-20 09:51:50 +0000

    import api factory

---
+++ lib/utils/api-clients/import-clients/ImportApiFactory.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * PostApi Factory
+ *
+ * @author Matthias Pfefferle
+ */
+class ImportApiFactory {
+
+  /**
+   * returns the matching PostApiClient
+   *
+   * @author Matthias Pfefferle
+   * @param mixed $pCommunity the community name or the community id
+   * @param array $pParams
+   * @return Object
+   */
+  static public function factory($pCommunity, $pParams = null) {
+    // check if $pCommunity is the name or the id
+    if (is_numeric($pCommunity)) {
+      $lCommunityObject = CommunityTable::getInstance()->retrieveByPk($pCommunity);
+      $lCommunity = $lCommunityObject->getCommunity();
+    } else {
+      $lCommunity = $pCommunity;
+    }
+
+    // return matching object
+    switch ($lCommunity) {
+      case "facebook":
+        return new FacebookImportApiClient();
+        break;
+      case "twitter":
+        return new TwitterImportApiClient();
+        break;
+      /*case "google":
+        return new GooglePostApiClient();
+        break;*/
+      default:
+        return null;
+    }
+  }
+}
