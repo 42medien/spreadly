f686b9c1282a011462af7d4d540cad525d01dbd8
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-21 11:30:55 +0000

    stats

--- apps/platform/modules/stream/templates/_sidebar_right.php
+++ apps/platform/modules/stream/templates/_sidebar_right.php
@@ -1,3 +1,3 @@
-<div id="stream_right" class="bg_light bd_normal_light">
+<div id="stream_right">
   <?php //include_partial('stream/item_detail', array('pObject' => $pObject, 'pActivities' => $pActivities)); ?>
 </div>
--- apps/platform/modules/stream/templates/indexSuccess.php
+++ apps/platform/modules/stream/templates/indexSuccess.php
@@ -1,11 +1,22 @@
 <div id="stream_left">
-  <div id="stream_left_top" class="clearfix">
-    <?php include_partial('main_navigation'); ?>
-  </div>
-
-  <div id="stream_left_bottom" class="bg_light clearfix">
-    <ul id="new_shares" class="hot_stream">
-      <?php include_partial('stream/whats_hot_stream', array('pSocialObjects' => $pSocialObjects)); ?>
-    </ul>
-  </div>
+  <?php if(count($pSocialObjects)) { ?>
+	  <div id="stream_left_top" class="clearfix">
+	    <?php include_partial('main_navigation'); ?>
+	  </div>
+	
+	  <div id="stream_left_bottom" class="bg_light clearfix">
+		    
+		  <ul id="new_shares" class="hot_stream">
+		    <?php include_partial('stream/whats_hot_stream', array('pSocialObjects' => $pSocialObjects)); ?>
+		  </ul>
+	  </div>
+      
+  <?php } else { ?>
+    <div id="stream_left_bottom" class="bg_light clearfix">
+	    <ul id="new_shares" class="hot_stream">  
+	      <?php include_partial('stream/empty_stream'); ?>
+	    </ul>
+	  </div>
+    
+  <?php } ?>
 </div>
