4d376972ee9b67f09fc3f0ac83b2169eb2023d1f
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-21 12:29:12 +0000

    stats

--- apps/platform/modules/system/templates/_language_switch_icons.php
+++ apps/platform/modules/system/templates/_language_switch_icons.php
@@ -1,5 +1,5 @@
 <?php foreach($lLanguages as $lLang) { ?>
-  <?php if ($sf_user->getCulture() != $lLang) { ?>
+  <?php if ($sf_user->getShortedCulture() != $lLang) { ?>
     <a href="<?php echo url_for('@update_language?lang='.$lLang); ?>" 
       class="icon_language_select" id="language_selecten_icon_<?php echo $lLang; ?>_inactive" 
       title="<?php echo __('Show yiid in %1', array('%1' => $lCultureInstance->getLanguage($lLang)), 'platform'); ?>">&nbsp;</a>
