e59d0e339ded936fd3fca7697cf20418f91a8c08
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-22 09:07:56 +0000

    php doc and error handling

--- lib/utils/api-clients/import-clients/YiidImportContacts.php
+++ lib/utils/api-clients/import-clients/YiidImportContacts.php
@@ -1,23 +1,26 @@
 <?php
+// required because its called like a batch outside of the symfony context
 require_once(dirname(__FILE__).'/../../../../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('platform', 'batch', true);
 sfContext::createInstance($configuration);
 
 /**
- * Enter description here...
+ * class to import the contacts from an online identity
  *
  * @author Matthias Pfefferle
  */
 class YiidImportContacts {
   public static function import($pMessage) {
-    sfContext::getInstance()->getLogger()->debug("{YiidImportContacts} Debug: " . print_r($pMessage, true));
-
     $lOnlineIdentity = OnlineIdentityTable::getInstance()->find($pMessage[0]['Body']);
 
     if ($lOnlineIdentity) {
-      $lObject = ImportApiFactory::factory($lOnlineIdentity->getCommunityId());
-      $lObject->importContacts($lOnlineIdentity->getUserId(), $lOnlineIdentity);
+      try {
+        $lObject = @ImportApiFactory::factory($lOnlineIdentity->getCommunityId());
+        @$lObject->importContacts($lOnlineIdentity->getUserId(), $lOnlineIdentity);
+      } catch (Exception $e) {
+        sfContext::getInstance()->getLogger()->err("{Daemon} ImpotContacts: " . $e->getMessage());
+      }
     }
   }
 }
