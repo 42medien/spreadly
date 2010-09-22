cb07fbb6a8011bba38bd7598bcbd4c94d7148e10
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-22 10:59:55 +0000

    stats

--- apps/platform/modules/stream/templates/_empty_stream.php
+++
@@ -1,5 +0,0 @@
-<li class="clearfix stream_item" id="empty_stream">
-  <h3><?php echo __('Hello %1,', array('%1' => $sf_user->getUser()->getUsername()), 'platform'); ?></h3>
-  <p><?php echo __('this is your stream. As you can see at the moment your stream is empty.<br/>As soon as we imported your friends you can see them on the left. 
-      This is also the point when your stream will be filled with Websites, that were shared by your friends.', null, 'platform'); ?></p>
-</li>
