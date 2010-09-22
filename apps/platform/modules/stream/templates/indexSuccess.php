a6ef0ba1be6f863891368a889f65f946d26dfa5c
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-22 10:58:42 +0000

    stats

--- apps/platform/modules/stream/templates/indexSuccess.php
+++ apps/platform/modules/stream/templates/indexSuccess.php
@@ -1,22 +1,11 @@
 <div id="stream_left">
-  <?php if(count($pSocialObjects)) { ?>
-	  <div id="stream_left_top" class="clearfix">
-	    <?php include_partial('main_navigation'); ?>
-	  </div>
+	<div id="stream_left_top" class="clearfix">
+	  <?php include_partial('main_navigation'); ?>
+	</div>
 	
-	  <div id="stream_left_bottom" class="bg_light clearfix">
-		    
-		  <ul id="new_shares" class="hot_stream">
-		    <?php include_partial('stream/whats_hot_stream', array('pSocialObjects' => $pSocialObjects)); ?>
-		  </ul>
-	  </div>
-      
-  <?php } else { ?>
-    <div id="stream_left_bottom" class="bg_light clearfix">
-	    <ul id="new_shares" class="hot_stream">  
-	      <?php include_partial('stream/empty_stream'); ?>
-	    </ul>
-	  </div>
-    
-  <?php } ?>
+	<div id="stream_left_bottom" class="bg_light clearfix">
+    <ul id="new_shares" class="hot_stream">
+		  <?php include_partial('stream/whats_hot_stream', array('pSocialObjects' => $pSocialObjects)); ?>
+		</ul>
+	</div>
 </div>
