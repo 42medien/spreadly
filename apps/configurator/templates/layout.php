d6bab3cabb979996d2c8f6d4df86f412d280fd23
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-23 13:19:41 +0000

    title tags

--- apps/configurator/templates/layout.php
+++ apps/configurator/templates/layout.php
@@ -2,8 +2,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title><?php echo __('YIID it'); ?></title>
     <link rel="shortcut icon" href="/favicon.ico" />
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
     <?php include_cdn_stylesheets(); ?>
     <!--[if IE 7]>
 	    <?php echo cdn_stylesheet_tag('configurator_ie7.css'); ?>
