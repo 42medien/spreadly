5e5663a2abf1fdd9639e13d6a81c19a2c80b4ce4
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-24 13:26:34 +0000

    title tags

--- apps/platform/modules/stream/templates/indexSuccess.php
+++ apps/platform/modules/stream/templates/indexSuccess.php
@@ -4,6 +4,10 @@
 	</div>
 	
 	<div id="stream_left_bottom" class="bg_light clearfix">
+    <div id="stream_breadcrumb">
+      <?php echo __('You see:');?> <span id="breadcrumb_filter_friend" class="text_important"><?php echo __('All Friends'); ?></span> | <span id="breadcrumb_filter_community" class="text_important"><?php echo __('All Networks'); ?></span> 
+    </div>
+	
     <ul id="new_shares" class="hot_stream">
 		  <?php include_partial('stream/whats_hot_stream', array('pSocialObjects' => $pSocialObjects)); ?>
 		</ul>
