4a1537bf862de289b12611594150ce8e93da1e13
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-24 13:12:31 +0000

    title tags

--- plugins/yiidPlugin/modules/general/templates/_footer.php
+++ plugins/yiidPlugin/modules/general/templates/_footer.php
@@ -5,21 +5,11 @@
   </div>
    -->
 
-  <?php foreach($pCategories as $lCategory) { ?>
-  <ul class="normal_list left">
-    <li><h5><?php echo $lCategory->getTitle(); ?></h5></li>
-    <?php foreach($lCategory->getCms() as $lCms) { ?>
-      <?php if ($lCms->getActive()) { ?>
-        <?php if ($lCms->getLink()) { ?>
-          <li><?php echo link_to($lCms->getHeadline(), $lCms->getLink()); ?></li>
-        <?php } else { ?>
-          <?php //var_dump($lCms->getPage());?>
-          <li><?php echo link_to($lCms->getHeadline(), 'static/index?category='.$lCms->getCmsCategory()->getPage().'&page='.$lCms->getPage());  ?></li>
-        <?php } ?>
-      <?php } ?>
-    <?php } ?>
+  <ul class="normal_list clearfix">
+    <li><?php echo link_to('Impressum', 'static/imprint');  ?></li>
+    <li>-</li>
+    <li><?php echo link_to('AGB', 'static/tos');  ?></li>
   </ul>
-  <?php } //end foreach categories ?>
 </div>
 
 <?php echo cdn_image_tag("img/global/ajax-loader-bar-circle.gif", array('id' => 'general-ajax-loader')); ?>
