f31258a91d832391158f55d4a6ad46ff49cd5b18
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-27 13:14:47 +0000

    stats

--- apps/platform/modules/stream/templates/_breadcrumb.php
+++ apps/platform/modules/stream/templates/_breadcrumb.php
@@ -1,7 +1,7 @@
 <?php echo __('You see:');?>
 <span id="breadcrumb_filter_friend" class="text_important">
     <?php echo ($pUserName)? $pUserName: __('All Friends'); ?>
-</span> |
+</span> &raquo;
 <span id="breadcrumb_filter_community" class="text_important">
   <?php echo ($pComName)? $pComName: __('All Networks'); ?>
 </span>
