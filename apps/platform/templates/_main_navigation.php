94782b4f814d43b3a4e3c076f80a7d119f97a451
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-28 11:34:16 +0000

    privacy

--- apps/platform/templates/_main_navigation.php
+++ apps/platform/templates/_main_navigation.php
@@ -6,7 +6,7 @@
 	    </div>
     <?php } ?>
     <?php $lAction = $sf_params->get('action'); ?>
-    <?php if($lAction != 'imprint' && $lAction != 'tos') { ?>
+    <?php if($lAction != 'imprint' && $lAction != 'tos' && $lAction != 'privacy') { ?>
 		  <div class="right" id="language_switch_area">
 		    <?php include_component('system', 'language_switch_icons'); ?>
 		  </div>
@@ -14,8 +14,8 @@
 	</div>
 	<ul id="nav_main" class="right clearfix">
     <?php if($sf_user->isAuthenticated()) { ?>
-      <li><?php echo link_to(__('Home'), '@homepage', array('id' => 'nav_home')); ?></li>
-      <li id="nav_main_settings"><?php echo link_to(__('Settings'), '@auth_add_services', array('id' => 'nav_settings')); ?></li>
+      <li><?php echo link_to(__('Home', null, 'configurator'), '@homepage', array('id' => 'nav_home')); ?></li>
+      <li id="nav_main_settings"><?php echo link_to(__('Settings', null, 'widget'), '@auth_add_services', array('id' => 'nav_settings')); ?></li>
     <?php } else { ?>
       <li><?php echo link_to(__('Start'), '@homepage', array('id' => 'nav_home')); ?></li>
     <?php } ?>
