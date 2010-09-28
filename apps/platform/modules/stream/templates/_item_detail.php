7dcf9a1358fcbf4de4a9a3aebfe285c6ad086f09
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-28 11:33:30 +0000

    activity details

--- apps/platform/modules/stream/templates/_item_detail.php
+++ apps/platform/modules/stream/templates/_item_detail.php
@@ -37,7 +37,7 @@
 
       <div id="preview">
         <div id="yiid-widget">
-          <iframe src="http://widgets.<?php echo sfConfig::get("app_settings_host"); ?>/w/like/like.php?<?php echo 'url='.rawurlencode('http://www.yiid.com').'&cult='.$sf_user->getCulture().'&type=like&color=%23000000&short='; ?>" style="overflow:hidden; width:240px; height: 23px; padding: 3px 0;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowTransparency="true"></iframe>
+          <iframe src="http://widgets.<?php echo sfConfig::get("app_settings_host"); ?>/w/like/like.php?<?php echo 'url='.urlencode('http://www.yiid.com').'&cult='.$sf_user->getCulture().'&type=like&color=%23000000&short='; ?>" style="overflow:hidden; width:240px; height: 23px; padding: 3px 0;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" allowTransparency="true"></iframe>
         </div>
       </div>
 
