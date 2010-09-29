162c09b074b29045d0745ebb637a706c463c36b1
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-29 12:55:34 +0000

    configurator footer

--- apps/configurator/config/configuratorConfiguration.class.php
+++ apps/configurator/config/configuratorConfiguration.class.php
@@ -2,7 +2,30 @@
 
 class configuratorConfiguration extends sfApplicationConfiguration
 {
-  public function configure()
-  {
+	protected $aPlatformRouting = null;
+	
+  public function configure() {
+  	
   }
+ 
+  public function generatePlatformUrl($name, $parameters = array())
+  {
+    return 'http://www.yiid.local'.$this->getPlatformRouting()->generate($name, $parameters);
+  }
+ 
+  public function getPlatformRouting()
+  {
+    if (!$this->aPlatformRouting)
+    {
+      $this->aPlatformRouting = new sfPatternRouting(new sfEventDispatcher());
+ 
+      $config = new sfRoutingConfigHandler();
+      $routes = $config->evaluate(array(sfConfig::get('sf_apps_dir').'/platform/config/routing.yml'));
+ 
+      $this->aPlatformRouting->setRoutes($routes);
+    }
+ 
+    return $this->aPlatformRouting;
+  }
+  
 }
