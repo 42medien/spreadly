61fc9e6690311fd0606ef58219fa88bc6fc0fbbb
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-29 12:46:29 +0000

    fixed bug [0007456]

--- apps/configurator/templates/_footer.php
+++ apps/configurator/templates/_footer.php
@@ -1,6 +1,6 @@
 <div id="nav_supp" class="clearfix">
-  <?php echo link_to_frontend(__('TERMS_OF_SERVICE', null, 'configurator'), 'static/index?category=rechtliches&page=agb'); ?>
-  <?php echo link_to_frontend(__('PRIVACY', null, 'configurator'), 'static/index?category=rechtliches&page=privacy'); ?>
-  <?php echo link_to_frontend(__('CONTACT', null, 'configurator'), 'static/contactform'); ?>
+  <?php echo link_to(__('TERMS_OF_SERVICE', null, 'configurator'), 'http://www.yiid.com/imprint'); ?>
+  <?php echo link_to(__('TERMS_OF_SERVICE', null, 'configurator'), 'http://www.yiid.com/tos'); ?>
+  <?php echo link_to(__('PRIVACY', null, 'configurator'), 'http://www.yiid.com/privacy'); ?>
   <?php echo link_to(__('Webmasters &amp; Developers'), '@webmasters'); ?>
 </div>
