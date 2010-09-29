ed10b721c975b9f2ec614f652eb3c31c0fe8176e
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-09-29 16:40:24 +0000

    bugfix

--- apps/widget/templates/layout.php
+++ apps/widget/templates/layout.php
@@ -6,7 +6,7 @@
     <?php include_http_metas() ?>
     <?php include_metas() ?>
 
-    <?php include_cdn_stylesheets(); ?>
+    <?php include_stylesheets(); ?>
     <script type="text/javascript" src="/js/100_main/include/likepopup-<?php echo sfConfig::get('app_release_name') ?>.js"></script>
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
