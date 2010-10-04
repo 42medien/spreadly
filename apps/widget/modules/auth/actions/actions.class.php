f6e1ff02e01b8603346f46253a77755611201478
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-04 13:42:07 +0000

    moved auth-actions from popup to auth module

---
+++ apps/widget/modules/auth/actions/actions.class.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * auth actions.
+ *
+ * @package    yiid
+ * @subpackage auth
+ * @author     Matthias Pfefferle
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class authActions extends sfActions {
+  /**
+   * Executes index action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeSignin(sfWebRequest $request) {
+    $this->getResponse()->setSlot('js_document_ready', $this->getPartial('popup/js_popup_ready'));
+    if ($this->getUser()->isAuthenticated()) {
+      $this->redirect("@settings");
+    }
+
+    sfProjectConfiguration::getActive()->loadHelpers('I18N');
+    $this->getUser()->setFlash('headline', __('SETTINGS', null, 'widget'));
+
+
+    /* Uncomment for displaying error
+     $this->getUser()->setFlash('headline', 'Login');
+     $this->getUser()->setFlash('error', 'You have been logged out.');
+     $this->getUser()->setFlash('error_msg', 'Please choose the same service you have used the last time to log in again.');
+     */
+  }
+
+  public function executeSignout(sfWebRequest $request) {
+    $this->getUser()->signOut();
+    $this->redirect('@signin');
+  }
+
+  public function executeSigninto(sfWebRequest $request) {
+  // if the user is already loged in, redirect to the stream
+    if ($lService = $request->getParameter("service")) {
+      $lObject = AuthApiFactory::factory($lService);
+      $lObject->doAuthentication();
+    } else {
+      $this->redirect('@signin');
+    }
+  }
+
+  public function executeComplete_signin(sfWebRequest $request) {
+    if ($lToken = $request->getParameter('oauth_token')) {
+      $lToken = OauthRequestTokenTable::retrieveByTokenKey($lToken);
+      $lToken = $lToken->toOAuthToken();
+      $lToken->verifier = $request->getParameter('oauth_verifier');
+    } elseif ($lToken = $request->getParameter('code')) {
+      // do nothing
+    }
+
+    $lObject = AuthApiFactory::factory($request->getParameter('service'));
+
+    // check if it is a signin/signup or if the user wants
+    // to add a new online-identity
+    if ($this->getUser()->isAuthenticated() && $this->getUser()->getUserId()) {
+      try {
+        $lObject->addIdentifier($this->getUser()->getUser(), $lToken);
+      } catch (Exception $e) {
+        $this->getUser()->setFlash("error", $e->getMessage(), true);
+      }
+    } else {
+      $lUser = $lObject->doSignin($this->getUser(), $lToken);
+      $this->getUser()->signIn($lUser);
+      //UserRelationTable::doShit($lUser->getId());
+    }
+    $this->pOnlineIdenities = OnlineIdentityTable::getPublishingEnabledByUserId($this->getUser()->getUserId());
+    CookieUtils::generateWidgetIdentityCookie($this->pOnlineIdenities);
+
+    $this->redirect('@settings');
+  }
+
+  public function executeCreate_account(sfWebRequest $request) {
+
+  }
+}
