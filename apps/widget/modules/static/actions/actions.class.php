13b007a366b7b281a0e5f0d21ca4f41ae4200134
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-04 12:53:43 +0000

    static module

---
+++ apps/widget/modules/static/actions/actions.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * static actions.
+ *
+ * @package    yiid
+ * @subpackage static
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class staticActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+
+  public function executeSignin(sfWebRequest $request) {
+  }
+}
