1273bd3da2d3520d6049f40b40498e414aa6ba13
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-06 11:58:39 +0000

    discovery

---
+++ apps/platform/modules/discovery/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * discovery actions.
+ *
+ * @package    yiid
+ * @subpackage discovery
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class discoveryActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+
+  public function executeHostmeta() {
+    $this->getResponse()->setContentType('application/xrd+xml');
+
+  }
+}
---
+++ apps/platform/modules/discovery/templates/hostmetaSuccess.php
@@ -0,0 +1,5 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n" ?>
+<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
+  <Subject>http://www.yiid.com/dumdidum</Subject>
+  <Link rel="http://oexchange.org/spec/0.8/rel/related-target" href="http://www.yiid.com/static/like" type="text/html" />
+</XRD>
