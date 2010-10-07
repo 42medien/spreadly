a63f8e3811e22d0229b651a47459d1c946eb96de
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-07 12:06:21 +0000

    facebook pushapi subsription test

--- apps/platform/modules/realtime/actions/actions.class.php
+++ apps/platform/modules/realtime/actions/actions.class.php
@@ -17,7 +17,7 @@ class realtimeActions extends sfActions
 
   public function executeSubscribeFacebook(sfWebRequest $request) {
 
-   $lPostBody = array('object'=>user, 'fields'=> 'name,picture,feed', 'callback_url' => 'http://www.yiiddev.com/realtime/facebookpush', 'verify_token'=> 'affen12');
+   $lPostBody = array('object'=> 'user', 'fields'=> 'name,picture,feed', 'callback_url' => 'http://www.yiiddev.com/realtime/facebookpush', 'verify_token'=> 'affen12');
 
    $lJsonObject = json_decode(UrlUtils::sendPostRequest("https://graph.facebook.com/353988911612/subscriptions?access_token=353988911612|fd118fd8fb5da426565eebb7-1336043259|xgFkdHetCbudF2lv0tWi7ockbFY", $lPostBody));
 
