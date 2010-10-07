742c8e501967abefc0064b4e701d7476037fc514
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-07 15:29:51 +0000

    removed configurator app

--- apps/configurator/templates/_navigation.php
+++
@@ -1,7 +0,0 @@
-<div id="nav_main" class="clearfix">
-  <div id="nav_wave_left"></div>
-  <div id="nav_wave_middle"></div>
-  <div id="nav_wave_right" class="left">
-    <?php include_component('auth', 'login_box'); ?>
-  </div>
-</div>
