56cc19a7a9b505547a7a1a70e1058f8595b35d75
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-08 15:10:34 +0000

    404 site

---
+++ config/unavailable.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <meta name="title" content="Yiid.com" />
+		<meta name="robots" content="index, follow" />
+		<meta name="description" content="Yiid helps you sharing your interesting websites" />
+		<meta name="keywords" content="yiid, social, network, sharing" />
+		<meta name="language" content="en" />
+		<title>yiid is offline</title>
+		
+		<link rel="stylesheet" type="text/css" media="screen" href="/css/001_layout.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="/css/002_color.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="/css/003_typo.css" />
+	  <link rel="shortcut icon" href="/img/global/favicon_16x16.ico" />
+
+  </head>
+  <body class="bg_light clearfix">
+    <div id="container" class="bd_round clearfix">
+      <div id="error-msg-box">
+
+      </div>
+      <div id="header" class="clearfix">
+        <div id="header_sub" class="left">
+          <a href="http://www.yiid.com" id="yiid_logo">&nbsp;</a>
+        </div>
+        <div id="header_main" class="left"></div>
+        <div id="header_supp" class="left"></div>
+      </div>
+
+      <div id="content" class="clearfix">
+        <div id="error_box_404_unavailable" class="center_area container_border rounded_corners">
+	        <h1>Yiid.com is down for maintenance</h1>
+	        <p>yiid.com is currently being revised. Do please take a moment of patience. yiid.com is accessible again in a few minutes.</p>
+	        <p>yiid.com wird gerade überarbeitet. Habt bitte einen Moment Geduld. Gleich ist yiid.com wieder erreichbar.</p>
+	      </div>
+      </div>
+
+      <div id="footer" class="clearfix">
+			  <ul class="normal_list clearfix">
+			    <li><a href="http://www.yiid.com/imprint">Imprint</a></li>
+			    <li>-</li>
+			    <li><a href="http://www.yiid.com/tos">Tos</a></li>
+			    <li>-</li>
+			    <li><a href="http://www.yiid.com/privacy">Privacy</a></li>
+			    <li>-</li>
+			    <li><a href="mailto:info@yiid.com">Contact</a></li>
+			  </ul>
+			</div>
+
+    </div>
+  </body>
+</html>
