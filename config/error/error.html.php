b394084a7f588abbfa08f5d863351bbf61c95e1c
christian <christian@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-14 08:28:34 +0000

    error site

---
+++ config/error/error.html.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Yiid.com" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Yiid helps you sharing your interesting websites" />
+    <meta name="keywords" content="yiid, social, network, sharing" />
+    <meta name="language" content="en" />
+    <title>yiid is offline</title>
+    
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/001_layout.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/002_color.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/003_typo.css" />
+    <link rel="shortcut icon" href="/img/global/favicon_16x16.ico" />
+
+  </head>
+  <body class="bg_light clearfix">
+    <div id="container" class="bd_round clearfix">
+      <div id="error-msg-box">
+
+      </div>
+      <div id="header" class="clearfix">
+        <div id="header_sub" class="left">
+          <a href="http://www.yiid.com" id="yiid_logo">&nbsp;</a>
+        </div>
+        <div id="header_main" class="left"></div>
+        <div id="header_supp" class="left"></div>
+      </div>
+
+      <div id="content" class="clearfix">
+        <div id="error_box_500" class="center_area container_border rounded_corners">
+				  <h2>Sorry! There was an error in yiid.</h2>
+				  <p class="text_important">Please try again or send us a contact mail.</p>
+          <h2 id="second_message">Sorry! Innerhalb von yiid trat ein Fehler auf.</h2>
+          <p class="text_important">Bitte versuchen sie es erneut oder senden sie uns eine Kontakt-Email.</p>
+				</div>
+        
+      </div>
+
+      <div id="footer" class="clearfix">
+        <ul class="normal_list clearfix">
+          <li><a href="http://www.yiid.com/imprint">Imprint</a></li>
+          <li>-</li>
+          <li><a href="http://www.yiid.com/tos">Tos</a></li>
+          <li>-</li>
+          <li><a href="http://www.yiid.com/privacy">Privacy</a></li>
+          <li>-</li>
+          <li><a href="mailto:info@yiid.com">Contact</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </body>
+</html>
