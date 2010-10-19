0d3be93b4418a89c45dae344baa07c0693ce4508
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-10-19 14:08:31 +0000

    linkedin contact import

---
+++ lib/utils/api-clients/import-clients/LinkedinImportClient.php
@@ -0,0 +1,44 @@
+<?php
+require_once(dirname(__FILE__).'/../../../../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('platform', 'batch', true);
+sfContext::createInstance($configuration);
+
+/**
+ * class to import twitter contacts
+ *
+ * @author Matthias Pfefferle
+ * @author Karina Mies
+ */
+class LinkedinImportClient {
+  /**
+   * import twitter contacts
+   *
+   * @author Matthias Pfefferle
+   * @author Karina Mies
+   */
+  public static function importContacts($pUserId, $pOnlineIdentity) {
+    $lToken = AuthTokenTable::getByUserAndOnlineIdentity($pUserId, $pOnlineIdentity->getId());
+    // get api informations
+    if (!$lToken) {
+      $pOnlineIdentity->setSocialPublishingEnabled(false);
+      $pOnlineIdentity->save();
+      throw new Exception('damn theres no token!', '666');
+    }
+    $lConsumer = new OAuthConsumer(sfConfig::get("app_linkedin_oauth_token"), sfConfig::get("app_linkedin_oauth_secret"));
+    $lXml = OAuthClient::get($lConsumer, $lToken->getTokenKey(), $lToken->getTokenSecret(), "http://api.linkedin.com/v1/people/~/connections:(id)");
+
+    $lObject = simplexml_load_string($lXml);
+
+    $lArray = array();
+
+    foreach ($lObject->person as $lPerson) {
+      $lArray[] = $lPerson->id;
+    }
+
+    $pOnlineIdentity->setFriendIds(implode(",", $lArray));
+    $pOnlineIdentity->save();
+
+    sfContext::getInstance()->getLogger()->info("{TwitterImportClient} ID ".$pOnlineIdentity->getId()." loaded and contacts sent to twitter import queue!!!!");
+  }
+}
