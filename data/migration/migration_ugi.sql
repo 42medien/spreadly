9d4908a0f5188203248dc94498d811e35b369600
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-11-03 09:12:01 +0000

    removed useremailaddress from schema

--- data/migration/migration_ugi.sql
+++ data/migration/migration_ugi.sql
@@ -5,3 +5,6 @@ ALTER TABLE `online_identity` ADD `location_raw` varchar( 255 ) NULL DEFAULT NUL
 
 ALTER TABLE `online_identity` DROP `identifier`; 
 ALTER TABLE `user` ADD `relationship_state` INTEGER( 4 ) NULL DEFAULT 0 ;
+
+## delete user email adresses
+DROP TABLE user_email_address;
