99e3b8f5a46438162da1309372143fdf94000b9a
christian.weyand <christian.weyand@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-11-08 10:40:35 +0000

    changes sql migration workflow

--- data/migration/migration_ugi.sql
+++ data/migration/migration_ugi.sql
@@ -1,3 +1,9 @@
+#########################################################################
+#########################################################################
+#######   NUR FELDER HINZUFÜGEN, DROPS IN das *_nachrelease.sql  ########
+#########################################################################
+#########################################################################
+
 ALTER TABLE `online_identity` ADD `birthdate` DATE NULL DEFAULT NULL;
 ALTER TABLE `online_identity` ADD `gender` VARCHAR( 1 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL ;
 ALTER TABLE `online_identity` ADD `relationship_state` INTEGER( 4 ) NULL DEFAULT 0 ;
@@ -5,9 +11,3 @@ ALTER TABLE `online_identity` ADD `location_raw` varchar( 255 ) NULL DEFAULT NUL
 
 ALTER TABLE `online_identity` DROP `identifier`; 
 ALTER TABLE `user` ADD `relationship_state` INTEGER( 4 ) NULL DEFAULT 0 ;
-
-## delete user email adresses
-DROP TABLE user_email_address;
-ALTER TABLE `user` DROP `contacts_list`; 
-ALTER TABLE `user` DROP `friends_list`; 
-ALTER TABLE `user` DROP `community_object_id`; 
