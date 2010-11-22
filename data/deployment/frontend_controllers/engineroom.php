95c1a75dfadba9f8f006158e04d8ebe018a80e46
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-11-22 14:08:31 +0000

    deleted frontend controllers

--- data/deployment/frontend_controllers/engineroom.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
-
-$configuration = ProjectConfiguration::getApplicationConfiguration('engineroom', 'local', false);
-sfContext::createInstance($configuration)->dispatch();
