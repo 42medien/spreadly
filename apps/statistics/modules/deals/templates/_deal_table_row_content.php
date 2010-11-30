12ada47ff2763fadfed10c3421a8f573cff2ca2e
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-11-30 13:55:56 +0000

    dealid

--- apps/statistics/modules/deals/templates/_deal_table_row_content.php
+++ apps/statistics/modules/deals/templates/_deal_table_row_content.php
@@ -15,8 +15,8 @@
 				20
       </td>
       <td style="text-align: center;" class="editinplaceclick" id="deal-quantity-<?php echo $pDeal->getId(); ?>"
-      	<?php if($pDeal->getCouponType() == "single"){ ?>data-obj='{"action":"/deals/save_quantity?deal_id=<?php echo $pDeal->getId(); ?>", "type":"text",  "params": "{\"id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
-      	<?php } else { ?> data-obj='{"action":"/deals/paste_codes?deal_id=<?php echo $pDeal->getId(); ?>", "type":"layer", "params": "{\"id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
+      	<?php if($pDeal->getCouponType() == "single"){ ?>data-obj='{"action":"/deals/save_quantity", "type":"text",  "params": "{\"deal_id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
+      	<?php } else { ?> data-obj='{"action":"/deals/paste_codes?deal_id=<?php echo $pDeal->getId(); ?>", "type":"layer", "params": "{\"deal_id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
       	<?php } ?>>
 				<?php echo $pDeal->getCouponQuantity(); ?>
       </td>
