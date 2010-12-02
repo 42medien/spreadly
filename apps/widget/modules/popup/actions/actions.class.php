85abb92a66ac85cc3a2d39670086ec241f35f4b5
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-02 16:26:13 +0000

    removed deal actions

--- apps/widget/modules/popup/actions/actions.class.php
+++ apps/widget/modules/popup/actions/actions.class.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 /**
  * popup actions.
@@ -19,9 +19,28 @@ class popupActions extends sfActions {
     $this->getUser()->setFlash('error', 'Folgede Fehler sind aufgetreten:');
   }
 
+  // @todo remove temp data
   public function executeSettings(sfWebRequest $request) {
     $this->getResponse()->setSlot('js_document_ready', $this->getPartial('popup/js_popup_ready'));
     $this->getUser()->setFlash('headline', __('SETTINGS', null, 'widget'));
+
+    $lUser = $this->getUser()->getUser();
+    if($request->getMethod() == sfRequest::POST) {
+      $checkedOnlineIdentities = $request->getParameter('enabled_services', array());
+
+      // @todo define methods in objects
+      OnlineIdentityTable::toggleSocialPublishingStatus($this->getUser()->getUser()->getOnlineIdentitesAsArray(), $checkedOnlineIdentities);
+
+      // @todo adding autolike
+
+      $this->getUser()->setFlash("onload", "window.close();", false);
+    }
+
+    $this->pIdentities = OnlineIdentityTable::getPublishingEnabledByUserId($this->getUser()->getUserId());
+
+    CookieUtils::generateWidgetIdentityCookie($this->pIdentities);
+    sfProjectConfiguration::getActive()->loadHelpers('I18N');
+
     $this->setLayout('layout_twocol');
   }
 
