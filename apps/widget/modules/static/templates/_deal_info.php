8fa0c60de606e81984b5653b9af92b31f5174b24
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-03 16:30:09 +0000

    a

--- apps/widget/modules/static/templates/_deal_info.php
+++ apps/widget/modules/static/templates/_deal_info.php
@@ -1,10 +1,10 @@
 <?php use_helper("Date"); ?>
 <?php if ($pYiidActivity && $pDeal) { ?>
-<div class="content-box yellow dashed">
+<div class="yellow dashed">
   <div class="coupon-summary deal_summary-mirror"><?php echo $pDeal->getSummary() ?></div>
   <div class="coupon-description deal_description-mirror">
     <p><?php echo __("Your code"); ?>:</p>
-    <p><?php echo $pYiidActivity->getCCode(); ?>
+    <p><strong><?php echo $pYiidActivity->getCCode(); ?></strong></p>
   </div>
   <div class="coupon-foot">
     <p><?php echo __("Be sure to use it until %date. Visit us here:", array("%date" =>format_date($pDeal->getEndDate()))); ?></p>
