42da808bd41412496654dd1bdb52735acf132ba0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-03 11:23:22 +0000

    fixed base test

--- test/lib/BaseTestCase.php
+++ test/lib/BaseTestCase.php
@@ -18,77 +18,7 @@ abstract class BaseTestCase extends PHPUnit_Framework_TestCase{
   }
 
   public static function resetMongo() {
-    BatchSocialObjetTable::doRemoveAll(true);
-    BatchYiidActivityTable::doRemoveAll(true);
-    BatchSessionStorage::doRemoveAll(true);
+    MongoDbConnector::getInstance()->getDatabase("yiid")->drop();
   }
 }
-
-
-class BatchSocialObjetTable extends SocialObjectTable {
-
-  /**
-   * Clears whole Database (buildlocal/dev) - not for production!
-   *
-   * @author Christian Weyand
-   * @param boolean $pAreYouSure
-   * @deprecated don't use this in production!
-   */
-  public static function doRemoveAll($pAreYouSure = false) {
-    if ($pAreYouSure) {
-      $lCollection = self::getMongoCollection();
-      return $lResults = $lCollection->remove();
-    }
-    return false;
-  }
-
-}
-
-class BatchYiidActivityTable extends YiidActivityTable {
-
-  /**
-   * Clears whole Database (buildlocal/dev) - not for production!
-   *
-   * @author Christian Weyand
-   * @param boolean $pAreYouSure
-   * @deprecated don't use this in production!
-   */
-  public static function doRemoveAll($pAreYouSure = false) {
-    if ($pAreYouSure) {
-      $lCollection = self::getMongoCollection();
-      return $lResults = $lCollection->remove();
-    }
-    return false;
-  }
-
-}
-
-
-
-class BatchSessionStorage {
-
-
-  const MONGO_COLLECTION_NAME = 'session';
-
-  public static function getMongoCollection() {
-    return MongoDbConnector::getInstance()->getCollection(sfConfig::get('app_mongodb_database_name'), self::MONGO_COLLECTION_NAME);
-  }
-
-  /**
-   * Clears whole Database (buildlocal/dev) - not for production!
-   *
-   * @author Christian Weyand
-   * @param boolean $pAreYouSure
-   * @deprecated don't use this in production!
-   */
-  public static function doRemoveAll($pAreYouSure = false) {
-    if ($pAreYouSure) {
-      $lCollection = self::getMongoCollection();
-      return $lResults = $lCollection->remove();
-    }
-    return false;
-  }
-
-}
-
 ?>
