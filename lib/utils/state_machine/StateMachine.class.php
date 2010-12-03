74e8ed22a885e9fa7502690ed10f02e138b907cf
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-03 15:25:03 +0000

    added strtolower

--- lib/utils/state_machine/StateMachine.class.php
+++ lib/utils/state_machine/StateMachine.class.php
@@ -5,31 +5,31 @@
  * @author hannes
  */
 class StateMachine extends Doctrine_Template {
-  
+
   public function setTableDefinition() {
     $options = $this->getOptions();
     $this->getTable()->setColumn($this->getOption('column'), 'enum', null, array('default' => $this->getOption('initial'), 'values' => $this->getOption('states')));
     $this->addListener(new StateMachineListener($this->getOptions()));
   }
-  
+
   public function setUp() {
     // Setting default column name, if none was set in yaml schema
     $this->_options['column'] = $this->getOption('column', 'state');
-    
+
     // Transforming all froms in events to arrays, if they are not arrays already
     foreach ($this->getOption('events') as $event => $t) {
       $this->_options['events'][$event]['from'] = is_string($t['from']) ? array($t['from']) : $t['from'];
     }
   }
-  
+
   public function transitionTo($pState) {
     return $this->transitionFor($this->getEventNameForState($pState));
   }
-  
+
   public function canTransitionTo($pState) {
     return $this->canTransitionFor($this->getEventNameForState($pState));
-  }  
-  
+  }
+
   public function getEventNames() {
     $lEvents = $this->getOption('events');
     $lRes = array();
@@ -38,7 +38,7 @@ class StateMachine extends Doctrine_Template {
     }
     return $lRes;
   }
-  
+
   private function getEventNameForState($pState) {
     $lEvents = $this->getOption('events');
     foreach ($lEvents as $lEventName => $lEventData) {
@@ -48,7 +48,7 @@ class StateMachine extends Doctrine_Template {
     }
     return null;
   }
-  
+
   // Transitions to new state, if it is allowed and fires an event
   public function transitionFor($event) {
     if($this->canTransitionFor($event)) {
@@ -56,13 +56,15 @@ class StateMachine extends Doctrine_Template {
       $this->setState($events[$event]['to']);
       $this->getInvoker()->save();
       $prefix = $this->getInvoker()->getTable()->getTableName();
-      $eventName = $prefix.".event.".$event;
+      $eventName = strtolower($prefix).".event.".strtolower($event);
+
       sfContext::getInstance()->getEventDispatcher()->notify(new sfEvent($this->getInvoker(), $eventName, array("event" => $event)));
+
       return true;
     }
     throw new sfException("Could not transition for event: ".$event);
   }
-  
+
   public function getState() {
     $state = $this->getOption('column');
     return $this->getInvoker()->$state;
@@ -70,9 +72,9 @@ class StateMachine extends Doctrine_Template {
 
   public function setState($newState) {
     $state = $this->getOption('column');
-    $this->getInvoker()->$state = $newState;      
+    $this->getInvoker()->$state = $newState;
   }
-  
+
   public function canTransitionFor($event) {
     $events = $this->getOption('events');
     if(in_array($this->getState(), $events[$event]['from'])) {
@@ -80,7 +82,7 @@ class StateMachine extends Doctrine_Template {
     }
     return false;
   }
-  
+
   public function __call($name, $args) {
     $events = $this->getOption('events');
     // All event-names are callable functions and work the same as transitionFor
