bb0032355dc1ff0e46f9eee79e3cf5c733c10d25
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-06 10:58:01 +0000

    some changes

--- apps/widget/modules/static/templates/_deal_info.php
+++ apps/widget/modules/static/templates/_deal_info.php
@@ -1,14 +1,14 @@
 <?php use_helper("Date"); ?>
-<?php if ($pYiidActivity && $pDeal) { ?>
+<?php if ($pYiidActivity && $lDeal = $pYiidActivity->getDeal()) { ?>
 <div class="yellow dashed">
-  <div class="coupon-summary deal_summary-mirror"><?php echo $pDeal->getSummary() ?></div>
+  <div class="coupon-summary deal_summary-mirror"><?php echo $lDeal->getSummary() ?></div>
   <div class="coupon-description deal_description-mirror">
     <p><?php echo __("Your code"); ?>:</p>
     <p><strong><?php echo $pYiidActivity->getCCode(); ?></strong></p>
   </div>
   <div class="coupon-foot">
-    <p><?php echo __("Be sure to use it until %date. Visit us here:", array("%date" =>format_date($pDeal->getEndDate()))); ?></p>
-    <p><?php echo link_to($pDeal->getRedeemUrl(), $pDeal->getRedeemUrl()); ?></p>
+    <p><?php echo __("Be sure to use it until %date. Visit us here:", array("%date" =>format_date($lDeal->getEndDate()))); ?></p>
+    <p><?php echo link_to($lDeal->getRedeemUrl(), $lDeal->getRedeemUrl()); ?></p>
   </div>
 </div>
 <?php } ?>
