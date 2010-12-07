6cb0436c84184a5d1b05201914a4f4dae89de9c0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-07 09:43:51 +0000

    auth navi slot

---
+++ apps/widget/modules/auth/actions/components.class.php
@@ -0,0 +1,5 @@
+<?php
+class authComponents extends sfComponents {
+  public function executeNavbar(sfWebRequest $request) {}
+}
+?>
---
+++ apps/widget/modules/auth/templates/_navbar.php
@@ -0,0 +1,7 @@
+<div id="nav_main">
+  <div id="static_nav_main_box">
+    <h1 class="light_text big_size">
+      <?php echo __('Authorisation'); ?>
+    </h1>
+  </div>
+</div>
