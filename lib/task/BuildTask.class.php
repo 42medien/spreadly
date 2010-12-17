84918ecb780110af6e47c2a04b3002a1f56ffd30
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2010-12-17 11:23:19 +0000

    added s3-sync

--- lib/task/BuildTask.class.php
+++ lib/task/BuildTask.class.php
@@ -59,6 +59,10 @@ EOF;
     $this->runTask('yiid:build-js', array(), array('application' => $options['application'], 'env' => $options['env']));
     // build css
     $this->runTask('yiid:build-css', array(), array('application' => $options['application'], 'env' => $options['env']));
+
+    if ($options['env'] != "dev") {
+      $this->runTask('yiid:s3-sync', array(), array('env' => $options['env']));
+    }
   }
 
   /**
