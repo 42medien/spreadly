2820c1234f2bb1452261bc1761abc30078fe6319
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-01-24 13:56:29 +0000

    irgendwas

--- apps/statistics/modules/analytics/templates/_range_content.php
+++ apps/statistics/modules/analytics/templates/_range_content.php
@@ -6,11 +6,21 @@
 <h2><?php echo __('%community% range key data', array('%community%' => $pCom)); ?><?php echo __('from %datefrom% - %dateto%', array('%datefrom%' => $pFrom, '%dateto%' => $pTo)); ?></h2>
 <div class="content-box bg-white third-box left">
 	<ul class="analytics-stats-list">
-		<li><h3><?php echo __('Total values'); ?></h3>
+		<li><h3><?php echo __('Total values for Likes'); ?></h3>
 			<ul>
-				<li><?php echo __('%likes% Likes', array('%likes%' => 1786)); ?></li>
+				<li><?php echo __('%views% Views', array('%views%' => 1786)); ?></li>
 				<li><?php echo __('%dislikes% Dislikes', array('%dislikes%' => 23)); ?></li>
 				<li><?php echo __('%clickbacks% Clickbacks', array('%clickbacks%' => 88)); ?></li>
+			</ul>
+		</li>
+		<li><h3><?php echo __('Total values for Dislikes'); ?></h3>
+			<ul>
+				<li><?php echo __('%views% Views', array('%views%' => 1786)); ?></li>
+			</ul>
+		</li>
+		<li><h3><?php echo __('Responses'); ?></h3>
+			<ul>
+				<li><?php echo __('%clickbacks% Clickbacks', array('%clickbacks%' => 1786)); ?></li>
 			</ul>
 		</li>
 	</ul>
@@ -19,8 +29,8 @@
 	<ul class="analytics-stats-list">
 		<li><h3><?php echo __('Mean values'); ?></h3>
 			<ul>
-				<li><?php echo __('%likes% Likes / Day', array('%likes%' => 1786)); ?></li>
-				<li><?php echo __('%dislikes% Dislikes / Day', array('%dislikes%' => 23)); ?></li>
+				<li><?php echo __('%views% Views / Like', array('%views%' => 1786)); ?></li>
+				<li><?php echo __('%views% Views / Dislike', array('%views%' => 1786)); ?></li>
 				<li><?php echo __('%clickbacks% Clickbacks / Day', array('%clickbacks%' => 88)); ?></li>
 			</ul>
 		</li>
@@ -30,8 +40,8 @@
 	<ul class="analytics-stats-list">
 		<li><h3><?php echo __('Percentage'); ?></h3>
 			<ul>
-				<li><?php echo __('%likes%% Dislike / Like Ratio', array('%likes%' => 0,12)); ?></li>
-				<li><?php echo __('%dislikes%% Clickback / Like Ratio', array('%dislikes%' => 197)); ?></li>
+				<li><?php echo __('%likes%% Likes, %dislikes%% Dislikes', array('%likes%' => 99, '%dislikes%' => 1)); ?></li>
+				<li><?php echo __('%clickbacks%% Clickbacks per 100 Likes', array('%clickbacks%' => 2)); ?></li>
 			</ul>
 		</li>
 	</ul>
