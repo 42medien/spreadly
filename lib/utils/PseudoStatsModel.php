41438f327e505578a88eac23883a00fa725936f0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-01-28 12:49:16 +0000

    pseudo stats model

---
+++ lib/utils/PseudoStatsModel.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * some kind of a mongo model... it returns an array of default values
+ *
+ * @author Matthias Pfefferle
+ */
+class PseudoStatsModel {
+  public static $age          = array("u_18", "b_18_24", "b_25_34", "b_35_54", "o_55");
+  public static $gender       = array("m", "f", "u");
+  public static $relationship = array("singl", "eng", "compl", "mar", "rel", "ior", "wid", "u");
+
+  /**
+   * prefilles an array with "0"s
+   *
+   * @param array $array
+   * @return array
+   */
+  public static function preFillArray($array) {
+    $result = array();
+    foreach ($array as $value) {
+      $result[$value] = 0;
+    }
+    return $result;
+  }
+
+  /**
+   * returns a nulled array with all possible date fields
+   *
+   * @return array
+   */
+  public static function getDefaultAgeArray() {
+    return self::preFillArray(self::$age);
+  }
+
+  /**
+   * returns a nulled array with all possible date fields
+   *
+   * @return array
+   */
+  public static function getDefaultGenderArray() {
+    return self::preFillArray(self::$gender);
+  }
+
+  /**
+   * returns a nulled array with all possible date fields
+   *
+   * @return array
+   */
+  public static function getDefaultRelationshipArray() {
+    return self::preFillArray(self::$relationship);
+  }
+}
