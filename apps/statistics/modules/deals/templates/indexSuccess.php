12a9999818e06fe17d48d8523da67a10256d0448
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-01 17:01:27 +0000

    remove uncommented html

--- apps/statistics/modules/deals/templates/indexSuccess.php
+++ apps/statistics/modules/deals/templates/indexSuccess.php
@@ -1,10 +1,3 @@
-<!--  div id="main_area_navigation" class="clearfix">
-	<ul class="clearfix">
-    <li><?php //echo __('Create Deal'); ?></li>
-    <li class="main_navigation active"><a class="nav-tab" href="deals/create_deal_form">&raquo; <?php echo __('Step 1/2: Set Your Deal Data'); ?></a></li>
-  	<li class="main_navigation active hide"><a class="nav-tab" href="deals/get_code_form">&raquo; <?php echo __('Step 2/2: Confirm Deal and Get Code'); ?></a></li>
-	</ul>
-</div -->
   <div id="create-deal-content">
 		<?php include_partial('deals/create_index');?>
 		<?php //include_partial('deals/deal_in_process', array('pIsNew' => false));?>
