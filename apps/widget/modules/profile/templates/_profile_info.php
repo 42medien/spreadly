b9740a982854d05dcd011fc43a1424a6948b7ff4
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-04 17:13:42 +0000

    title for favicons

--- apps/widget/modules/profile/templates/_profile_info.php
+++ apps/widget/modules/profile/templates/_profile_info.php
@@ -2,12 +2,12 @@
             		<div class="rcor">
               		<ul class="clearfix profileinfo">
                 		<li class="prothumb">
-                			<img src="/img/macrco-thumb.jpg" width="64" height="64" alt="Marco Ripanti" title="Marco Ripanti" />
+                			<img src="/img/macrco-thumb.jpg" width="64" height="64" alt="<?php echo $pUser->getUsername(); ?>" title="<?php echo $pUser->getUsername(); ?>" />
                 		</li>
                   	<li class="pronameblock">
                   		<h1><?php echo $pUser->getUsername(); ?></h1><?php echo $pUser->getDescription(); ?>
                   		<div class="scicon">
-                  		<?php foreach ($pIdentities as $lIdentity) { ?><a href="<?php echo url_for($lIdentity->getProfileUri()) ?>" target="_blank"><img src="/img/<?php echo $lIdentity->getCommunity()->getCommunity(); ?>-favicon.gif" width="17" height="17" alt="<?php echo $lIdentity->getCommunity()->getCommunity(); ?>" title="<?php echo $lIdentity->getCommunity()->getCommunity(); ?>" /></a><?php } ?>
+                  		<?php foreach ($pIdentities as $lIdentity) { ?><a href="<?php echo url_for($lIdentity->getProfileUri()) ?>" target="_blank"><img src="/img/<?php echo $lIdentity->getCommunity()->getCommunity(); ?>-favicon.gif" width="17" height="17" alt="<?php echo $lIdentity->getName(); ?>" title="<?php echo $lIdentity->getName(); ?>" /></a><?php } ?>
                   		</div>
 										</li>
 										<li class="friends-box">
