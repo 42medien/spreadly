212eda159b87c488863350325b931d7a28bf7d00
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-04 17:22:19 +0000

    remove static list

--- apps/widget/modules/settings/templates/indexSuccess.php
+++ apps/widget/modules/settings/templates/indexSuccess.php
@@ -21,48 +21,6 @@
 	                    		</a>
 	                    	</li>
 											<?php } ?>
-
-
-                    	<li>
-                    		<label class="radio-btn">
-                    			<input type="checkbox" class="checkbox" name="" />
-                    		</label>
-                    		<a href="#">
-                    			<span>
-                    				<img src="/img/tweet-profilleicon.gif" width="17" height="17" alt="pics.nase-bohren.de" title="pics.nase-bohren.de" />
-                    			</span>twitter.com/ripanti
-                    		</a>
-                    	</li>
-                      <li>
-                      	<label class="radio-btn">
-                      		<input type="checkbox" class="checkbox" name="" />
-                      	</label>
-                      	<a href="#">
-                      		<span>
-                      			<img src="/img/facebookprofileicon.gif" width="17" height="17" alt="pics.nase-bohren.de" title="pics.nase-bohren.de" />
-                      		</span>facebook.com/ripanti
-                      	</a>
-                      </li>
-                      <li>
-                      	<label class="radio-btn">
-                      		<input type="checkbox" class="checkbox" name="" />
-                      	</label>
-                      	<a href="#">
-                      		<span>
-                      			<img src="/img/in-profileicon.gif" width="17" height="17" alt="pics.nase-bohren.de" title="pics.nase-bohren.de" />
-                      		</span>inkedin.com/ripanti
-                      	</a>
-                      </li>
-                      <li>
-                      	<label class="radio-btn">
-                      		<input type="checkbox" class="checkbox" name="" />
-                      	</label>
-                      	<a href="#">
-                      		<span>
-                      			<img src="/img/googlechat-icon.gif" width="19" height="17" alt="pics.nase-bohren.de" title="pics.nase-bohren.de" />
-                      		</span>google.com/p/ripanti
-                      	</a>
-                      </li>
                     </ul>
                     <div class="morecomments add alignright">
                     	Add more accounts: <?php echo link_to(image_tag("/img/facebook-add.gif"), "@signinto?service=facebook&r=s"); ?><?php echo link_to(image_tag("/img/twiiter-add.gif"), "@signinto?service=twitter&r=s"); ?><?php echo link_to(image_tag("/img/in-add.gif"), "@signinto?service=linkedin&r=s"); ?><?php echo link_to(image_tag("/img/buzz-add.gif"), "@signinto?service=google&r=s"); ?></div>
