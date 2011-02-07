175b4235533ed738faa96278b23e93488eef29a5
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-07 12:51:40 +0000

    moved discovery

---
+++ apps/statistics/modules/discovery/templates/hostmetaSuccess.php
@@ -0,0 +1,11 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n" ?>
+<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'
+    xmlns:hm='http://host-meta.net/xrd/1.0'>
+    <hm:Host>www.spread.ly</hm:Host>
+
+    <Link
+        rel="http://oexchange.org/spec/0.8/rel/resident-target"
+        type="application/xrd+xml"
+        href="http://www.spread.ly/discovery/oexchange" >
+    </Link>
+</XRD>
---
+++ apps/statistics/modules/discovery/templates/oexchangeSuccess.php
@@ -0,0 +1,44 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n" ?>
+<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
+
+    <Subject>http://www.spread.ly/discovery/oexchange</Subject>
+
+    <Property
+        type="http://www.oexchange.org/spec/0.8/prop/vendor">spread.ly</Property>
+    <Property
+        type="http://www.oexchange.org/spec/0.8/prop/title">yiid livestream of likes and dislikes</Property>
+    <Property
+        type="http://www.oexchange.org/spec/0.8/prop/name">spread.ly</Property>
+    <Property
+        type="http://www.oexchange.org/spec/0.8/prop/prompt">Dis/Like any kind of content</Property>
+
+    <Link
+        rel= "icon"
+        href="http://www.spread.ly/favicon.ico"
+        type="image/vnd.microsoft.icon"
+        />
+
+    <Link
+        rel= "icon32"
+        href="http://www.spread.ly/img/global/favicon_32x32.png"
+        type="image/vnd.microsoft.icon"
+        />
+
+    <Link
+        rel= "likebutton"
+        href="http://www.spread.ly/img/likebutton.png"
+        type="image/png"
+        />
+
+    <Link
+        rel= "dislikebutton"
+        href="http://www.spread.ly/img/dislikebutton.png"
+        type="image/png"
+        />
+
+    <Link
+        rel= "http://www.oexchange.org/spec/0.8/rel/offer"
+        href="http://widgets.spread.ly/static/like"
+        type="text/html"
+        />
+</XRD>
