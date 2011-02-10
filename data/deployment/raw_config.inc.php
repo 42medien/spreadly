a3aeb56f16dc270ec29d830bfef7ef4b914abbae
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-10 14:53:21 +0000

    removed some settings-params

--- data/deployment/raw_config.inc.php
+++ data/deployment/raw_config.inc.php
@@ -11,17 +11,10 @@ class LikeSettings {
   const MONGO_DATABASENAME = '##MONGO_DB##';
   const MONGO_STATS_DATABASENAME = '##MONGO_STATS_DB##';
 
-  const WIDGET_USED_LIKE = 1;
-  const WIDGET_USED_DISLIKE = -1;
-
   const COOKIE_DOMAIN  = '.##YIID_HOST##';
   const SF_SESSION_COOKIE  = '##COOKIE_NAME##';
 
-  const JS_LIKE_PATH =  'http://widgets.##YIID_HOST##/api/like';
-  const JS_DISLIKE_PATH =  'http://widgets.##YIID_HOST##/api/dislike';
-
-  const JS_POPUP_PATH = 'http://widgets.##YIID_HOST##/popup/settings';
-  const JS_STATIC_PATH = 'http://widgets.##YIID_HOST##/static/like';
+  const JS_POPUP_PATH = 'http://widgets.##YIID_HOST##/like';
 
   const JS_GETFRIENDS_PATH = 'http://widgets.##YIID_HOST##/widget/load_friends';
 
