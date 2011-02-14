a0f501f7cd6170ec56e4f12b8b33c8250db797bb
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-14 16:36:18 +0000

    some fixes

--- apps/statistics/modules/discovery/templates/oexchangeSuccess.php
+++ apps/statistics/modules/discovery/templates/oexchangeSuccess.php
@@ -1,44 +1,27 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n" ?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
+  <Subject>http://spreadly.com/discovery/oexchange</Subject>
 
-    <Subject>http://www.spread.ly/discovery/oexchange</Subject>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/vendor">spread.ly</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/title">yiid livestream of likes and dislikes</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/name">spread.ly</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/prompt">Like any kind of content</Property>
 
-    <Property
-        type="http://www.oexchange.org/spec/0.8/prop/vendor">spread.ly</Property>
-    <Property
-        type="http://www.oexchange.org/spec/0.8/prop/title">yiid livestream of likes and dislikes</Property>
-    <Property
-        type="http://www.oexchange.org/spec/0.8/prop/name">spread.ly</Property>
-    <Property
-        type="http://www.oexchange.org/spec/0.8/prop/prompt">Dis/Like any kind of content</Property>
+  <Link
+    rel= "icon"
+    href="http://spreadly.com/favicon.ico"
+    type="image/vnd.microsoft.icon"
+  />
 
-    <Link
-        rel= "icon"
-        href="http://www.spread.ly/favicon.ico"
-        type="image/vnd.microsoft.icon"
-        />
+  <Link
+    rel= "icon32"
+    href="http://spreadly.com/img/global/favicon_32x32.png"
+    type="image/png"
+  />
 
-    <Link
-        rel= "icon32"
-        href="http://www.spread.ly/img/global/favicon_32x32.png"
-        type="image/vnd.microsoft.icon"
-        />
-
-    <Link
-        rel= "likebutton"
-        href="http://www.spread.ly/img/likebutton.png"
-        type="image/png"
-        />
-
-    <Link
-        rel= "dislikebutton"
-        href="http://www.spread.ly/img/dislikebutton.png"
-        type="image/png"
-        />
-
-    <Link
-        rel= "http://www.oexchange.org/spec/0.8/rel/offer"
-        href="http://widgets.spread.ly/static/like"
-        type="text/html"
-        />
+  <Link
+    rel= "http://www.oexchange.org/spec/0.8/rel/offer"
+    href="http://widgets.spread.ly/like"
+    type="text/html"
+  />
 </XRD>
