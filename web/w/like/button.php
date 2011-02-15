83ef688b6bb291899ee81faa39e3a2a476a7417a
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-15 13:25:18 +0000

    button

---
+++ web/w/like/button.php
@@ -0,0 +1,19 @@
+<?php include("buttonheader.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>spread.ly</title>
+  <!--[if IE 7]>
+    <script type="text/javascript" src="./../js/widget/iepngfix_tilebg.js"></script>
+  <![endif]-->
+  <!--[if lte IE 6]>
+    <style type="text/css">a { behavior: url(./../js/widget/iepngfix.htc); }</style>
+  <![endif]-->
+  <style type="text/css">
+  </style>
+</head>
+<body>
+  <a href="<?php echo $lPopupUrl ?>" rel="like" onclick="window.open(this.href, 'popup', 'width=580,height=435,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;" class="button">i like this really</a>
+</body>
+</html>
