c969bed4f4b82cff254151b170aa28cf97895703
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-16 10:49:06 +0000

    Styled dashboard

--- apps/statistics/modules/analytics/templates/indexSuccess.php
+++ apps/statistics/modules/analytics/templates/indexSuccess.php
@@ -1,34 +1,28 @@
-<?php use_helper('Date', 'DomainProfiles') ?>
+<?php slot('content') ?>
+  <h3 class="verifytitle">Claim your sites</h3>
 
-<div class="grboxtop"><span></span></div>
-<div class="grboxmid">
-	<div class="grboxmid-content">
-		<div class="graybox clearfix veryficationbox">
-	    <h3 class="verifytitle">Claim your sites</h3>
+  <div class="data-tablebox">
+  	<table width="77%" border="0" cellspacing="0" cellpadding="0">
+    	<tr>
+  			<th width="1" height="32" align="left" valign="middle" class="first"><div><?php echo __('Rank'); ?></div></th>
+  	    <th width="575" align="center" valign="middle" class="last"><div><?php echo __('Website');?></div></th>
+    	</tr>
+    	<?php if(count($pVerifiedDomains) > 0) { ?>
+  		<?php $i = 1;?>
+  		<?php foreach($pVerifiedDomains as $lDomain) { ?>
+  			<tr>
+  				<td height="44" align="center" class="first"><?php echo $i; ?></td>
+  		    <td align="center" valign="middle" class="last"><?php echo link_to($lDomain->getUrl(), '@analytics?host_id='.$lDomain->getId()); ?></td>
+  			</tr>
+      <?php $i++; } ?>
+      <?php } else { ?>
+  			<tr>
+  				<td height="44" align="center" class="first">&nbsp;</td>
+  				<td height="44" align="center" class="last"><?php echo __('No websites claimed');?></td>
+  			</tr>
+      <?php } ?>
+  	</table>
+  </div>
+<?php end_slot(); ?>
 
-			<div class="data-tablebox">
-				<table width="77%" border="0" cellspacing="0" cellpadding="0">
-			  	<tr>
-						<th width="1" height="32" align="left" valign="middle" class="first"><div><?php echo __('Rank'); ?></div></th>
-				    <th width="575" align="center" valign="middle" class="last"><div><?php echo __('Website');?></div></th>
-			  	</tr>
-			  	<?php if(count($pVerifiedDomains) > 0) { ?>
-					<?php $i = 1;?>
-					<?php foreach($pVerifiedDomains as $lDomain) { ?>
-						<tr>
-							<td height="44" align="center" class="first"><?php echo $i; ?></td>
-					    <td align="center" valign="middle" class="last"><?php echo link_to($lDomain->getUrl(), '@analytics?host_id='.$lDomain->getId()); ?></td>
-						</tr>
-			    <?php $i++; } ?>
-			    <?php } else { ?>
-						<tr>
-							<td height="44" align="center" class="first">&nbsp;</td>
-							<td height="44" align="center" class="last"><?php echo __('No websites claimed');?></td>
-						</tr>
-			    <?php } ?>
-				</table>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="grboxbot"><span></span></div>
+<?php include_partial('global/graybox'); ?>
