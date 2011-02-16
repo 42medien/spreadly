8ea3801f6c82a63419f4689380b64b383f7349dd
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-16 14:38:33 +0000

    navigation but awesomer

--- apps/statistics/templates/_navigation.php
+++ apps/statistics/templates/_navigation.php
@@ -9,12 +9,15 @@
   
   <ul id="topnavigation" class="alignleft">
   	<?php if($sf_user->isAuthenticated()): ?>
-  		<li><a href="#" class="welcome_user"><?php echo __('Hello').' '.$sf_user->getUsername(); ?></a></li>
-      <li><a href="<?php echo url_for('@dashboard'); ?>" title="Dashboard" <?php if($sf_context->getModuleName()=='likebutton' && $sf_context->getActionName()=='dashboard') { echo 'class="active"';} ?>><span><?php echo __('Dashboard'); ?></span></a></li>
-      <li><a href="<?php echo url_for('@configurator'); ?>" title="Buttons" <?php if($sf_context->getModuleName()=='likebutton' && $sf_context->getActionName()=='index') { echo 'class="active"';} ?>><span><?php echo __('Buttons'); ?></span></a></li>
-      <li><a href="<?php echo url_for('domain_profiles/index'); ?>" title="Domains" <?php if($sf_context->getModuleName()=='domain_profiles') { echo 'class="active"';} ?>><span><?php echo __('Domains'); ?></span></a></li>
-      <li><a href="<?php echo url_for('@analytics_overview'); ?>" title="Analytics" <?php if($sf_context->getModuleName()=='analytics' && ($sf_context->getActionName()=='index' || $sf_context->getActionName()=='statistics')) { echo 'class="active"';} ?>><span><?php echo __('Analytics'); ?></span></a></li>
-      <li class="last"><a href="<?php echo url_for('@deals'); ?>" title="Deals"<?php if($sf_context->getModuleName()=='deals') { echo 'class="active"';} ?>><span><?php echo __('Deals'); ?></span></a></li>
+  	  <?php $module = $sf_context->getModuleName(); ?>
+  	  <?php $action = $sf_context->getActionName(); ?>
+
+  		<li><div class="welcome_user"><?php echo __('Hello').' '.$sf_user->getUsername(); ?></div></li>
+      <li><a href="<?php echo url_for('@dashboard'); ?>" title="Dashboard" <?php if($module=='likebutton' && $action=='dashboard') { echo 'class="active"';} ?>><span><?php echo __('Dashboard'); ?></span></a></li>
+      <li><a href="<?php echo url_for('@configurator'); ?>" title="Buttons" <?php if($module=='likebutton' && $action=='index') { echo 'class="active"';} ?>><span><?php echo __('Buttons'); ?></span></a></li>
+      <li><a href="<?php echo url_for('domain_profiles/index'); ?>" title="Domains" <?php if($module=='domain_profiles') { echo 'class="active"';} ?>><span><?php echo __('Domains'); ?></span></a></li>
+      <li><a href="<?php echo url_for('@analytics_overview'); ?>" title="Analytics" <?php if($module=='analytics' && ($action=='index' || $action=='statistics')) { echo 'class="active"';} ?>><span><?php echo __('Analytics'); ?></span></a></li>
+      <li class="last"><a href="<?php echo url_for('@deals'); ?>" title="Deals"<?php if($module=='deals') { echo 'class="active"';} ?>><span><?php echo __('Deals'); ?></span></a></li>
     <?php endif; ?>
   </ul>
   
