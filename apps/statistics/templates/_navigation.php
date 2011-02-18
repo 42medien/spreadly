703d3f866acfbcfe01362361e7dc99241031bca8
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-18 08:32:56 +0000

    removed dashboard

--- apps/statistics/templates/_navigation.php
+++ apps/statistics/templates/_navigation.php
@@ -6,21 +6,21 @@
     <a href="<?php echo url_for('@update_language?lang=en'); ?>" class="alignleft"><img src="/img/uk-flag-icon.png" width="25" height="26" alt="UK" title="UK" /></a>
     <a href="<?php echo url_for('@update_language?lang=de'); ?>" class="alignleft"><img src="/img/germany-flag.png" width="25" height="26" alt="Deutsch" title="Deutsch" /></a>
   </div>
-  
+
   <ul id="topnavigation" class="alignleft">
   	<?php if($sf_user->isAuthenticated()): ?>
   	  <?php $module = $sf_context->getModuleName(); ?>
   	  <?php $action = $sf_context->getActionName(); ?>
 
   		<li><div class="welcome_user"><?php echo __('Hello').' '.$sf_user->getUsername(); ?></div></li>
-      <li><a href="<?php echo url_for('@dashboard'); ?>" title="Dashboard" <?php if($module=='likebutton' && $action=='dashboard') { echo 'class="active"';} ?>><span><?php echo __('Dashboard'); ?></span></a></li>
+      <!-- li><a href="<?php echo url_for('@dashboard'); ?>" title="Dashboard" <?php if($module=='likebutton' && $action=='dashboard') { echo 'class="active"';} ?>><span><?php echo __('Dashboard'); ?></span></a></li-->
       <li><a href="<?php echo url_for('@configurator'); ?>" title="Buttons" <?php if($module=='likebutton' && $action=='index') { echo 'class="active"';} ?>><span><?php echo __('Buttons'); ?></span></a></li>
       <li><a href="<?php echo url_for('domain_profiles/index'); ?>" title="Domains" <?php if($module=='domain_profiles') { echo 'class="active"';} ?>><span><?php echo __('Domains'); ?></span></a></li>
       <li><a href="<?php echo url_for('@analytics_overview'); ?>" title="Analytics" <?php if($module=='analytics' && ($action=='index' || $action=='statistics')) { echo 'class="active"';} ?>><span><?php echo __('Analytics'); ?></span></a></li>
       <li class="last"><a href="<?php echo url_for('@deals'); ?>" title="Deals"<?php if($module=='deals') { echo 'class="active"';} ?>><span><?php echo __('Deals'); ?></span></a></li>
     <?php endif; ?>
   </ul>
-  
+
   <?php include_component('auth', 'login_box'); ?>
 <?php end_slot(); ?>
 <?php include_partial('global/graybox'); ?>
