bd324998a81dc2ec6e300e8888de0ce5692cc05d
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-02-28 15:42:44 +0000

    fixed image path

--- apps/widget/templates/layout.php
+++ apps/widget/templates/layout.php
@@ -56,7 +56,7 @@
           </ul>
           <div class="profile-info spread_profile alignright" >
             <div class="alignleft spread <?php if ($sf_context->getModuleName()=='like') { echo 'active'; } ?>"><?php echo link_to(image_tag("/img/spread-it-label.png"), '@widget_like'); ?><?php if ($sf_context->getModuleName()=='like') { echo '<span></span>'; } ?></div>
-            <span class="alignleft"><img src="/img/marco.jpg" width="25" height="26" alt="<?php echo $sf_user->getUser()->getUsername(); ?>" title="<?php echo $sf_user->getUser()->getUsername(); ?>" class="mic-pic" />Hi <?php echo $sf_user->getUser()->getUsername(); ?>,</span>
+            <span class="alignleft"><img src="<?php echo avatar_path($sf_user->getUser()->getDefaultAvatar(), '30'); ?>" width="25" height="26" alt="<?php echo $sf_user->getUser()->getUsername(); ?>" title="<?php echo $sf_user->getUser()->getUsername(); ?>" class="mic-pic" />Hi <?php echo $sf_user->getUser()->getUsername(); ?>,</span>
             <span class="alignleft likebtn"><?php echo image_tag("/img/like-icon.png", array("alt"=>"Like", "title"=>"Like")) ?></span>
             <span class="comment_label alignleft"><?php echo $sf_user->getUser()->getLikeCount(); ?></span><?php echo link_to(__('Logout'), '@signout');?>
           </div>
