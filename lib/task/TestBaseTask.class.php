77f893b4fda1e117c113bedc9be9c975116bdef8
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-03 09:45:45 +0000

    adding test tasks

---
+++ lib/task/TestBaseTask.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class TestBaseTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'yiid';
+    $this->name             = 'test-base';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [TestBase|INFO] task does things.
+Call it with:
+
+  [php symfony TestBase|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->log("I'm the BaseTask …");
+
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $database = $databaseManager->getDatabase($options['connection']);
+    //$connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    // add your code here
+    $this->runTask('yiid:test-child', array(), array('application' => $options['application'], 'env' => $options['env']));
+  }
+}
