00d1a189b91d4dae937268fc8d48a1a5e4b50fcb
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-08 13:03:18 +0000

    fixing loggin links

--- apps/statistics/templates/_navigation.php
+++ apps/statistics/templates/_navigation.php
@@ -1,14 +1,19 @@
 <?php slot('content') ?>
   <div class="languagebox alignright">
-  	<?php if($sf_user->isAuthenticated()): ?>
-  	  <div class="welcome_user alignleft"><?php echo __('Hello').' '.$sf_user->getUsername(); ?></div>
-  	  <?php echo link_to(__('Logout'), '@sf_guard_signout', array("class" => "alignleft logout")); ?>
-  	<?php else: ?>
-  	  <?php echo link_to(__('Login'), '@sf_guard_signin', array("class" => "colorbox")); ?>
-      <?php echo __('or'); ?> <?php echo link_to(__('Register'), '@sf_guard_register', array("class" => "colorbox")); ?>
-  	<?php endif; ?>
     <a href="<?php echo url_for('@update_language?lang=en'); ?>" class="alignright"><img src="/img/uk-flag-icon.png" width="25" height="26" alt="UK" title="UK" /></a>
     <a href="<?php echo url_for('@update_language?lang=de'); ?>" class="alignright"><img src="/img/germany-flag.png" width="25" height="26" alt="Deutsch" title="Deutsch" /></a>
+  	<?php if($sf_user->isAuthenticated()){ ?>
+  		<div class="alignright">
+	  	  <div class="welcome_user alignleft"><?php echo __('Hello').' '.$sf_user->getUsername(); ?></div>
+	  	  <?php echo link_to(__('Logout'), '@sf_guard_signout', array("class" => "alignleft logout")); ?>
+  	  </div>
+  	<?php } ?>
+  	<?php if(!$sf_user->isAuthenticated()) {?>
+  		<div class="login-links alignright">
+	  	  <?php echo link_to(__('Login'), '@sf_guard_signin', array("class" => "colorbox")); ?>
+	      <?php echo __('or'); ?> <?php echo link_to(__('Register'), '@sf_guard_register', array("class" => "colorbox")); ?>
+	   </div>
+  	<?php } ?>
   </div>
 
   <ul id="topnavigation" class="alignleft">
