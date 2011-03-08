37b64eb42091e5d9d7ab15cd287af3a6b96df643
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-08 15:38:07 +0000

    fixed bug

--- apps/widget/modules/profile/templates/_profile_info.php
+++ apps/widget/modules/profile/templates/_profile_info.php
@@ -7,7 +7,7 @@
       </li>
       <li class="pronameblock">
       	<h1><?php echo $pUser->getUsername(); ?></h1>
-      	<span title="<?php echo $pUser->getDescription(); ?>"><?php echo truncate_text($pUser->getDescription(), 40); ?></span>
+      	<span><?php echo truncate_text(strip_tags($pUser->getDescription()), 40); ?></span>
         <div class="scicon">
          	<?php foreach ($pIdentities as $lIdentity) { ?><a href="<?php echo url_for($lIdentity->getProfileUri()) ?>" target="_blank" title="<?php echo $lIdentity->getName().": ".$lIdentity->getUrl(); ?>"><img src="/img/<?php echo $lIdentity->getCommunity()->getCommunity(); ?>-favicon.gif" width="17" height="17" alt="<?php echo $lIdentity->getName().": ".$lIdentity->getUrl(); ?>" /></a><?php } ?>
         </div>
