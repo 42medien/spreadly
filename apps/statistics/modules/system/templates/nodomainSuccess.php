885f211855cf428617a243ff57c86079f67ad1cd
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-09 09:37:53 +0000

    added english text

--- apps/statistics/modules/system/templates/nodomainSuccess.php
+++ apps/statistics/modules/system/templates/nodomainSuccess.php
@@ -1,8 +1,8 @@
 <div class="clearfix" id="nodomain-content">
-		<h1><?php echo __("Sie haben noch nicht bestätigt, dass Sie Inhaber einer Domain sind."); ?></h1>
+		<h1><?php echo __("You have not yet confirmed that you are the owner of a domain."); ?></h1>
 	<p>
-		<?php echo __('Bitte erledigen Sie diesen Schritt als Erstes, da dies die Voraussetzung für die Nutzung von Statistik- und Dealfunktionen ist.'); ?>
+		<?php echo __('The confirmation of your domain ownership is required for activating the deal function of the Spread.ly button as well as the statistics section. Please confirm it now.'); ?>
 	</p>
-	<?php echo link_to('<span>'.__('Ja, jetzt eine Domain bestätigen').'</span>', 'domain_profiles/index', array('class' => 'button')); ?>
-	<?php echo link_to('<span>'.__('Nein danke').'</span>', '@configurator', array('class' => 'button')); ?>
+	<?php echo link_to('<span>'.__('Yes, I own of a domain').'</span>', 'domain_profiles/index', array('class' => 'button')); ?>
+	<?php echo link_to('<span>'.__('No thanks').'</span>', '@configurator', array('class' => 'button')); ?>
 </div>
