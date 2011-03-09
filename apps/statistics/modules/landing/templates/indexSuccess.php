5151e68cad380921d33684b7a756a2028feef138
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-09 13:13:53 +0000

    fixed miriams changes

--- apps/statistics/modules/landing/templates/indexSuccess.php
+++ apps/statistics/modules/landing/templates/indexSuccess.php
@@ -5,7 +5,7 @@
 					<div class="panel_width">
 						<div class="graybox buttonbox">
 							<h2><?php echo __("Get your button NOW!"); ?></h2>
-							<h3><?php echo __("Three easy steps to get your content shared on all important social networks:"); ?></h3>
+							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, LinkedIn, GoogleBuzz"); ?></h3>
 							<div class="bannerimg"><?php echo image_tag("/img/button-banner-".$sf_user->getCulture().".png", array("class"=>"banner")); ?><a href="<?php echo url_for("@configurator"); ?>" class="goimg" title="Get Your Own Button Now"><img src="/img/get-yourown-img.png" alt="Get Your Own Button Now" title="Get Your Own Button Now"/></a></div>
 						</div>
 					</div>
@@ -20,7 +20,7 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><?php echo __("Share"); ?><span><?php echo __("Benefit from huge scope"); ?></span></h3>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Share"); ?></span><span><?php echo __("Benefit from huge scope"); ?></span></h3>
 							<p><?php echo __("Users of your site comment on your contents and products. They share their feedback with a click on the Spread.ly-button on different social networks. Their contacts can react by clicking and commenting."); ?></p>
 						</div>
 					</div>
@@ -32,7 +32,7 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><?php echo __("Deal"); ?><span><?php echo __("recommendation marketing"); ?></span></h3>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Deal"); ?></span><span><?php echo __("Recommendation marketing"); ?></span></h3>
 							<p><?php echo __("Create your deal for a defined period of time. Thank your visitors and customers for their recommendations with individual vouchers. Deals ensure additional recommendations easily."); ?></p>
 						</div>
 					</div>
@@ -44,7 +44,7 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><?php echo __("Analytics"); ?><span><?php echo __("Get to know your fans"); ?></span></h3>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Analytics"); ?></span><span><?php echo __("Get to know your fans"); ?></span></h3>
 							<p><?php echo __("Spread.ly offers an analytic tool that measures effectiveness of recommendations. You get demographics analyzed in anonymized form and come to know the value of each recommendation."); ?></p>
 						</div>
 					</div>
--- apps/statistics/templates/layout.php
+++ apps/statistics/templates/layout.php
@@ -46,7 +46,7 @@
     <div id="header">
     	<div class="clearfix">
       	<div id="logo-spread" class="alignleft"><a href="<?php echo url_for("@homepage"); ?>"><img src="/img/spread-logo-domain.png" width="306" height="128" alt="" /></a></div>
-        <span class="alignleft easy-tagline">"The Better Button"</span></div>
+        <span class="alignleft easy-tagline">first choice for social sharing</span></div>
 			</div>
 			<!--Content start here -->
 			<div id="content" class="clearfix">
