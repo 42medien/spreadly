5b96fb5760eef1ba9b32c61f3351f7eed9a9e9c9
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-10 10:16:58 +0000

    german button

--- apps/statistics/modules/landing/templates/indexSuccess.php
+++ apps/statistics/modules/landing/templates/indexSuccess.php
@@ -7,7 +7,7 @@
 							<h2><?php echo __("Get your button NOW!"); ?></h2>
 							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, GoogleBuzz, LinkedIn."); ?><br />
 							<?php echo __("Like to thank for recommendations? Just create your deal!"); ?></h3>
-							<div class="bannerimg"><?php echo image_tag("/img/button-banner-".$sf_user->getCulture().".png", array("class"=>"banner")); ?><a href="<?php echo url_for("@configurator"); ?>" class="goimg" title="Get Your Own Button Now"><img src="/img/get-yourown-img.png" alt="Get Your Own Button Now" title="Get Your Own Button Now"/></a></div>
+							<div class="bannerimg"><?php echo image_tag("/img/button-banner-".$sf_user->getCulture().".png", array("class"=>"banner")); ?><a href="<?php echo url_for("@configurator"); ?>" class="goimg" title="Get Your Own Button Now"><img src="/img/get-yourown-img-<?php echo $sf_user->getCulture() ?>.png" alt="Get Your Own Button Now" title="Get Your Own Button Now"/></a></div>
 						</div>
 					</div>
 					<div class="alignnone"></div>
