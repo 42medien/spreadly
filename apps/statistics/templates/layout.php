9151ddb8fb259dc72f4eed4af487f3102ad466bb
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-11 10:54:44 +0000

    google analytics code

--- apps/statistics/templates/layout.php
+++ apps/statistics/templates/layout.php
@@ -73,7 +73,18 @@
 	        <?php include_slot('js_document_ready'); ?>
 	      <?php } ?>
 	    });
-		</script>
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-21966328-1']);
+      _gaq.push(['_setDomainName', '.spreadly.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
 		<img id="general-ajax-loader" style="display:none;" src="/img/global/ajax-loader-bar-circle.gif" />
 	</body>
 </html>
