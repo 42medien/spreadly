7389b6121412f98d7a466c2d984631159169dae0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-15 10:26:20 +0000

    fixed hochkomma problem

--- apps/widget/modules/like/templates/indexSuccess.php
+++ apps/widget/modules/like/templates/indexSuccess.php
@@ -78,8 +78,8 @@ if ($lImages && count($lImages) > 0) {
     </div>
 
     <input type="hidden" name="like[thumb]" id="like-img-value" value="<?php echo $lImage; ?>" />
-    <input type="hidden" name="like[title]" value="<?php echo $pYiidMeta->getTitle(); ?>" />
-    <input type="hidden" name="like[descr]" value="<?php echo $pYiidMeta->getDescription(); ?>" />
+    <input type="hidden" name="like[title]" value="<?php echo htmlspecialchars($pYiidMeta->getTitle()); ?>" />
+    <input type="hidden" name="like[descr]" value="<?php echo htmlspecialchars($pYiidMeta->getDescription()); ?>" />
     <input type="hidden" name="like[url]" value="<?php echo $pYiidMeta->getUrl(); ?>" />
     <input type="hidden" name="like[tags]" value="<?php echo $sf_request->getParameter('tags'); ?>" />
     <input type="hidden" name="like[clickback]" value="<?php echo $sf_request->getParameter('clickback'); ?>" />
