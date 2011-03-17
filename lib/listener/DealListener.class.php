e3b7cc05a8d45717ec3c7469629dc759a97ca92a
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-17 09:48:20 +0000

    added some log messages to deal listener

--- lib/listener/DealListener.class.php
+++ lib/listener/DealListener.class.php
@@ -2,30 +2,38 @@
 class DealListener {
 
   public static function eventSubmit($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventSubmit");
     $deal = $event->getSubject();
 
     $admins = sfGuardUserTable::getInstance()->findByIsSuperAdmin(true);
     foreach ($admins as $admin) {
+      sfContext::getInstance()->getLogger()->notice("{DealListener} Sending email to ".$admin->getEmailAddress());
       sfContext::getInstance()->getMailer()->composeAndSend('deals@ekaabo.com', $admin->getEmailAddress(), '[Deal submitted]: '.preg_replace('/\n/', '', $deal->getSummary()), sfConfig::get("app_settings_url").'/backend.php/deal/'.$deal->getId().'/edit' );
     }
   }
 
   public static function eventApprove($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventApprove");
   }
 
   public static function eventDeny($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventDeny");
   }
 
   public static function eventPause($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventPause");
   }
 
   public static function eventResume($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventResume");
   }
 
   public static function eventTrash($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} eventTrash");
   }
 
   public static function updateMongoDeal($event) {
+    sfContext::getInstance()->getLogger()->notice("{DealListener} updateMongoDeal");
     $deal = $event->getSubject();
     $params = $event->getParameters();
 
