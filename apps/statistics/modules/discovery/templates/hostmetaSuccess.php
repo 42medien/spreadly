a7eb33fa23ffd437dfb3bb9948a4d29a235eebfa
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-17 07:27:40 +0000

    fixed oexchange discovery

--- apps/statistics/modules/discovery/templates/hostmetaSuccess.php
+++ apps/statistics/modules/discovery/templates/hostmetaSuccess.php
@@ -1,11 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8" ?>'."\n" ?>
 <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'
     xmlns:hm='http://host-meta.net/xrd/1.0'>
-    <hm:Host>www.spread.ly</hm:Host>
+    <hm:Host>spreadly.com</hm:Host>
 
-    <Link
-        rel="http://oexchange.org/spec/0.8/rel/resident-target"
-        type="application/xrd+xml"
-        href="http://www.spread.ly/discovery/oexchange" >
-    </Link>
+    <Link rel="http://oexchange.org/spec/0.8/rel/resident-target" type="application/xrd+xml" href="http://spreadly.com/discovery/oexchange" />
 </XRD>
--- apps/statistics/modules/discovery/templates/oexchangeSuccess.php
+++ apps/statistics/modules/discovery/templates/oexchangeSuccess.php
@@ -2,26 +2,12 @@
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
   <Subject>http://spreadly.com/discovery/oexchange</Subject>
 
-  <Property type="http://www.oexchange.org/spec/0.8/prop/vendor">spread.ly</Property>
-  <Property type="http://www.oexchange.org/spec/0.8/prop/title">yiid livestream of likes and dislikes</Property>
-  <Property type="http://www.oexchange.org/spec/0.8/prop/name">spread.ly</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/vendor">ekaabo GmbH</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/title">first choice for social sharing</Property>
+  <Property type="http://www.oexchange.org/spec/0.8/prop/name">Spread.ly</Property>
   <Property type="http://www.oexchange.org/spec/0.8/prop/prompt">Like any kind of content</Property>
 
-  <Link
-    rel= "icon"
-    href="http://spreadly.com/favicon.ico"
-    type="image/vnd.microsoft.icon"
-  />
-
-  <Link
-    rel= "icon32"
-    href="http://spreadly.com/img/global/favicon_32x32.png"
-    type="image/png"
-  />
-
-  <Link
-    rel= "http://www.oexchange.org/spec/0.8/rel/offer"
-    href="http://widgets.spread.ly/like"
-    type="text/html"
-  />
+  <Link rel= "icon" href="http://spreadly.com/favicon.ico" type="image/vnd.microsoft.icon" />
+  <Link rel= "icon32" href="http://spreadly.com/img/favicon-rounded-32.png" type="image/png" />
+  <Link rel= "http://www.oexchange.org/spec/0.8/rel/offer" href="http://spread.ly/" type="text/html" />
 </XRD>
