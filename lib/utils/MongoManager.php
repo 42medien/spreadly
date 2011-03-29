48a3c413740b3fe1f9f9a6bea81245f20e5caec2
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-03-29 13:46:03 +0000

    added mongomanager

---
+++ lib/utils/MongoManager.php
@@ -0,0 +1,57 @@
+<?php
+use Doctrine\Common\ClassLoader,
+    Doctrine\Common\Annotations\AnnotationReader,
+    Doctrine\ODM\MongoDB\Configuration,
+    Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver,
+    Doctrine\MongoDB\Connection,
+    Doctrine\ODM\MongoDB\DocumentManager,
+    \sfConfig;
+
+class MongoManager {
+  static private $document_manager = null;
+  static private $stats_document_manager = null;
+
+  private function __construct(){}
+  private function __clone(){}
+
+  public static function getStatsDM() {
+    if (null === self::$document_manager) {
+      self::$stats_document_manager = self::instantiateDocumentManager("stats");
+    }
+
+    return self::$stats_document_manager;
+  }
+
+  public static function getDM() {
+    if (null === self::$document_manager) {
+      self::$document_manager = self::instantiateDocumentManager();
+    }
+
+    return self::$document_manager;
+  }
+
+  private function instantiateDocumentManager($db = null) {
+    $config = new Configuration();
+
+    $config->setProxyDir(sfConfig::get('sf_cache_dir').'/Proxies');
+    $config->setProxyNamespace('Proxies');
+
+    $config->setHydratorDir(sfConfig::get('sf_cache_dir').'/Hydrators');
+    $config->setHydratorNamespace('Hydrators');
+
+    $config->setDefaultDB($db);
+
+    $reader = new AnnotationReader();
+    $reader->setDefaultAnnotationNamespace('Doctrine\ODM\MongoDB\Mapping\\');
+    $config->setMetadataDriverImpl(new AnnotationDriver($reader, __DIR__ . '/Documents'));
+
+    if ($db = "stats") {
+      $db = sfConfig::get("database_name_stats");
+    } else {
+      $db = sfConfig::get("database_name");
+    }
+
+    return DocumentManager::create(new Connection(), $this->config);
+  }
+
+}
