255dac034ba7197fa6918cf1dc9ca92883720950
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-01 13:24:57 +0000

    removed zeugs

--- apps/widget/modules/deals/actions/components.class.php
+++ apps/widget/modules/deals/actions/components.class.php
@@ -2,15 +2,13 @@
 class dealsComponents extends sfComponents {
 
 	public function executeCoupon_used(sfWebRequest $request) {
-		$this->pActivity = YiidActivityTable::retrieveById($this->pActivityId);
-		//var_dump($this->pActivity);die();
-		//$this->pActivity = $this->pDeal[0];
+	  $dm = MongoManager::getDM();
+		$this->pActivity = $dm->getRepository("Documents\YiidActivity")->find(intval($this->pActivityId()));
   }
 
 	public function executeCoupon_unused(sfWebRequest $request) {
-		$this->pActivity = YiidActivityTable::retrieveById($this->pActivityId);
-		//var_dump($this->pActivity);die();
-		//$this->pActivity = $this->pDeal[0];
+	  $dm = MongoManager::getDM();
+		$this->pActivity = $dm->getRepository("Documents\YiidActivity")->find(intval($this->pActivityId()));
   }
 }
 ?>
