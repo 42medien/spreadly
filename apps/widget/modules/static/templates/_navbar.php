600f257207e53b56ecacc1d7eb831d1a5b2c3fea
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-01 13:20:48 +0000

    removed static folder

--- apps/widget/modules/static/templates/_navbar.php
+++
@@ -1,8 +0,0 @@
-<div id="nav_main">
-  <div id="static_nav_main_box">
-    <h1 class="light_text big_size">
-      <a href="<?php echo url_for("@static_like"); ?>" id="like-nav-link" class="static-nav-elem"><?php echo $pDeal ? __('Coupon') : __('Like'); ?></a> |
-      <a href="<?php echo url_for("@static_settings"); ?>" id="settings-nav-link" class="static-nav-elem"><?php echo __('Settings'); ?></a>
-    </h1>
-  </div>
-</div>
