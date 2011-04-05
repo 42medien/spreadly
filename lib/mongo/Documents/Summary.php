e62e4f8965b8b367ef59f453396ad29535761773
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-05 15:02:21 +0000

    added demographics

--- lib/mongo/Documents/Summary.php
+++ lib/mongo/Documents/Summary.php
@@ -19,10 +19,48 @@ abstract class Summary extends BaseDocument {
 
   /** @Field(type="int", name="cb") */
   protected $clickbacks;
-  
+
   /** @Field(type="int", name="cbl") */
   protected $clickback_likes;
-  
+
   /** @String */
   protected $host;
+
+  /**
+   * @Field(type="hash", name="d")
+   *
+   * for example:
+   *
+   * [d] => Array
+   *     (
+   *         [age] => Array
+   *             (
+   *                 [b_18_24] => 4
+   *                 [b_25_34] => 9
+   *                 [b_35_54] => 6
+   *                 [o_55] => 1
+   *                 [u_18] => 6
+   *             )
+   *
+   *        [rel] => Array
+   *            (
+   *                [compl] => 3
+   *                [eng] => 0
+   *                [ior] => 6
+   *                [mar] => 0
+   *                [rel] => 8
+   *                [singl] => 20
+   *                [u] => 3
+   *                [wid] => 1
+   *            )
+   *
+   *        [sex] => Array
+   *            (
+   *                [f] => 5
+   *                [m] => 4
+   *                [u] => 5
+   *            )
+   *    )
+   */
+  protected $demographics;
 }
