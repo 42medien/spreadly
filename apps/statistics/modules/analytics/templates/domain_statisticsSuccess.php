a974d78d3ea0216f405f23bf022f7d4241cefc7f
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-06 16:00:52 +0000

    affenkopp

--- apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
+++ apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
@@ -1,54 +1,72 @@
 <?php slot('content') ?>
 <div id="analytics-bread">
-  <?php echo link_to(__('Dashboard'), 'analytics/index'); ?>&nbsp;
-  <?php echo link_to($host->getHost(), 'analytics/domain_statistics?host='.$host->getHost()); ?>&nbsp;
-  <?php echo __('Overview'); ?>
-</div>
-<h3><?php echo __('Overview domain %domain%', array('%domain%' => $host->getHost()));?></h3>
-<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$host->getHost(), array('class' => 'alignright'));?>
-<div class="clearfix">
-  <div class="alignleft" style="padding: 30px; border: 1px solid #ccc; margin: 10px;"><?php echo $host->getLikes(); ?> Likes</div>
-  <div class="alignleft" style="padding: 30px; border: 1px solid #ccc; margin: 10px;"><?php echo $host->getShares(); ?> Shares</div>
-  <div class="alignleft" style="padding: 30px; border: 1px solid #ccc; margin: 10px;">Media Penetration <?php echo $host->getMediaPenetration(); ?></div>
+	<?php echo link_to(__('Dashboard'), 'analytics/index'); ?>&nbsp;&gt;&nbsp;
+	<?php echo link_to($host->getUrl(), 'analytics/domain_statistics?domainid='.$host->getId()); ?>&nbsp;&gt;&nbsp;
+	<?php echo __('Overview'); ?>
 </div>
 
+<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$host->getId(), array('class' => 'alignright'));?>
+<h2><?php echo __('Overview for domain %domain%', array('%domain%' => $host->getUrl()));?></h2>
+<div class="clearfix" id="stat-infos">
+	<div class="alignleft stat-info-box">
+		<div class="alignleft stat-info-left"><strong><?php echo __("Likes"); ?></strong><br /><span class="stat-info-number">3</span></div>
+		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '2')); ?></div>
+	</div>
+	<div class="alignleft css-arrow-big">
+	</div>
+
+	<div class="alignleft stat-info-box">
+		<div class="alignleft stat-info-left"><strong><?php echo __("Spreads"); ?></strong><br /><span class="stat-info-number">847</span></div>
+		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '200')); ?></div>
+	</div>
+	<div class="alignleft css-arrow-big">
+	</div>
+
+	<div class="alignleft stat-info-box">
+		<div class="alignleft stat-info-left"><strong><?php echo __("Media Penetration"); ?></strong><br /><span class="stat-info-number">8247</span></div>
+		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '2540')); ?></div>
+	</div>
+	<div class="alignleft css-arrow-big">
+	</div>
+
+</div>
 <div id="demo-pie-charts" class="clearfix">
-  <div class="alignleft" id="gender-chart">
-    <?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
-          '{
-                "width": 290,
-                "height": 130,
-                "margin": [ 0, 0, 10, 10],
-                "plotsize": "50%",
-                "bgcolor" : "#EBEAEA",
-                "renderto":"gender-chart"
-            }'
-    )); ?>
-  </div>
-  <div class="alignleft" id="age-chart">
-    <?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
-          '{
-                "width": 290,
-                "height": 130,
-                "margin": [ 0, 0, 10, 10],
-                "plotsize": "50%",
-                "bgcolor" : "#EBEAEA",
-                "renderto":"age-chart"
-            }'
-    )); ?>
-  </div>
-  <div class="alignleft" id="relation-chart">
-    <?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
-          '{
-                "width": 290,
-                "height": 130,
-                "margin": [ 0, 0, 10, 10],
-                "plotsize": "50%",
-                "bgcolor" : "#EBEAEA",
-                "renderto":"relation-chart"
-            }'
-    )); ?>
-  </div>
+	<div class="alignleft" id="gender-chart">
+		<?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
+					'{
+								"width": 300,
+								"height": 130,
+								"margin": [ 0, 0, 10, 10],
+								"plotsize": "50%",
+								"bgcolor" : "#EBEAEA",
+								"renderto":"gender-chart"
+						}'
+		)); ?>
+	</div>
+	<div class="alignleft" id="age-chart">
+		<?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
+					'{
+								"width": 300,
+								"height": 130,
+								"margin": [ 0, 0, 10, 10],
+								"plotsize": "50%",
+								"bgcolor" : "#EBEAEA",
+								"renderto":"age-chart"
+						}'
+		)); ?>
+	</div>
+	<div class="alignleft" id="relation-chart">
+		<?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
+					'{
+								"width": 300,
+								"height": 130,
+								"margin": [ 0, 0, 10, 10],
+								"plotsize": "50%",
+								"bgcolor" : "#EBEAEA",
+								"renderto":"relation-chart"
+						}'
+		)); ?>
+	</div>
 </div>
 
 <?php echo link_to('Show Details', 'analytics/domain_detail?host='.$host->getHost(), array('class' => 'alignright'));?>
