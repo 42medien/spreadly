236a86aa61dd30c0adaee32c9fb8cbd78af85151
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-07 07:30:04 +0000

    added values

--- apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
+++ apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
@@ -1,30 +1,30 @@
 <?php slot('content') ?>
 <div id="analytics-bread">
 	<?php echo link_to(__('Dashboard'), 'analytics/index'); ?>&nbsp;&gt;&nbsp;
-	<?php echo link_to($host->getUrl(), 'analytics/domain_statistics?domainid='.$host->getId()); ?>&nbsp;&gt;&nbsp;
+	<?php echo link_to($host->getHost(), 'analytics/domain_statistics?domainid='.$host->getId()); ?>&nbsp;&gt;&nbsp;
 	<?php echo __('Overview'); ?>
 </div>
 
 <?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$host->getId(), array('class' => 'alignright'));?>
-<h2><?php echo __('Overview for domain %domain%', array('%domain%' => $host->getUrl()));?></h2>
+<h2><?php echo __('Overview for domain %domain%', array('%domain%' => $host->getHost()));?></h2>
 <div class="clearfix" id="stat-infos">
 	<div class="alignleft stat-info-box">
-		<div class="alignleft stat-info-left"><strong><?php echo __("Likes"); ?></strong><br /><span class="stat-info-number">3</span></div>
-		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '2')); ?></div>
+		<div class="alignleft stat-info-left"><strong><?php echo __("Likes"); ?></strong><br /><span class="stat-info-number"><?php echo $host->getLikes(); ?></span></div>
+		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => $host->getClickbackLikes())); ?></div>
 	</div>
 	<div class="alignleft css-arrow-big">
 	</div>
 
 	<div class="alignleft stat-info-box">
-		<div class="alignleft stat-info-left"><strong><?php echo __("Spreads"); ?></strong><br /><span class="stat-info-number">847</span></div>
+		<div class="alignleft stat-info-left"><strong><?php echo __("Spreads"); ?></strong><br /><span class="stat-info-number"><?php echo $host->getShares(); ?></span></div>
 		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '200')); ?></div>
 	</div>
 	<div class="alignleft css-arrow-big">
 	</div>
 
 	<div class="alignleft stat-info-box">
-		<div class="alignleft stat-info-left"><strong><?php echo __("Media Penetration"); ?></strong><br /><span class="stat-info-number">8247</span></div>
-		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => '2540')); ?></div>
+		<div class="alignleft stat-info-left"><strong><?php echo __("Media Penetration"); ?></strong><br /><span class="stat-info-number"><?php echo $host->getMediaPenetration(); ?></span></div>
+		<div class="alignleft stat-info-right"><?php echo __('%count% coming from ClickBacks', array('%count%' => $host->getClickbacks())); ?></div>
 	</div>
 	<div class="alignleft css-arrow-big">
 	</div>
