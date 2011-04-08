599f4dc48276435f4d7f04d46b6bfb7c19034880
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-08 14:44:14 +0000

    hübscher gemacht

--- apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
+++ apps/statistics/modules/analytics/templates/domain_statisticsSuccess.php
@@ -1,14 +1,11 @@
 <?php slot('content') ?>
+<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$pDomainProfileId, array('class' => 'alignright'));?>
 <div id="analytics-bread">
 	<?php echo link_to(__('Dashboard'), 'analytics/index'); ?>&nbsp;&gt;&nbsp;
 	<?php echo link_to($pHost->getHost(), 'analytics/domain_statistics?domainid='.$pDomainProfileId); ?>&nbsp;&gt;&nbsp;
 	<?php echo __('Overview'); ?>
 </div>
-
-<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$pDomainProfileId, array('class' => 'alignright'));?>
-
 <h2><?php echo __('Overview for domain %domain%', array('%domain%' => $pHost->getHost()));?></h2>
-
   <div id="navi" class="clearfix">
     <div class="stepBox alignleft">
       <div class="box_container alignleft">
@@ -47,19 +44,23 @@
       </ul>
     </div>
   </div>
+<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$pDomainProfileId, array('class' => 'alignright'));?>
+
 <?php end_slot(); ?>
 <?php include_partial('global/graybox'); ?>
 
 <?php slot('content') ?>
-<div id="demo-pie-charts" class="clearfix">
+<?php echo link_to('Show Details', 'analytics/domain_detail?domainid='.$pDomainProfileId, array('class' => 'alignright clearfix'));?>
+
+<div id="pie-charts" class="clearfix">
 	<div class="alignleft" id="gender-chart">
 		<?php include_partial('analytics/chart_pie_gender', array('pChartsettings' =>
 					'{
-								"width": 290,
+								"width": 305,
 								"height": 170,
 								"margin": [ 30, 0, 10, 10],
 								"plotsize": "50%",
-								"bgcolor" : "#EBEAEA",
+								"bgcolor" : "#e1e1e1",
 								"renderto":"gender-chart"
 						}', 'pData' => $pHost->getDemographics()
 		)); ?>
@@ -67,11 +68,11 @@
 	<div class="alignleft" id="age-chart">
 		<?php include_partial('analytics/chart_pie_age', array('pChartsettings' =>
 					'{
-								"width": 290,
+								"width": 305,
 								"height": 170,
-								"margin": [ 30, 0, 10, 10],
+								"margin": [ 30, 20, 10, 20],
 								"plotsize": "50%",
-								"bgcolor" : "#EBEAEA",
+								"bgcolor" : "#e1e1e1",
 								"renderto":"age-chart"
 						}', 'pData' => $pHost->getDemographics()
 		)); ?>
@@ -79,11 +80,11 @@
 	<div class="alignleft" id="relation-chart">
 		<?php include_partial('analytics/chart_pie_relationship', array('pChartsettings' =>
 					'{
-								"width": 290,
+								"width": 305,
 								"height": 170,
 								"margin": [ 30, 0, 10, 10],
 								"plotsize": "50%",
-								"bgcolor" : "#EBEAEA",
+								"bgcolor" : "#e1e1e1",
 								"renderto":"relation-chart"
 						}', 'pData' => $pHost->getDemographics()
 		)); ?>
