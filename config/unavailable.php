b7ed9a5c35a782b2e1cc2dd668950941f8f9a28c
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-13 11:46:11 +0000

    better maintenance site

--- config/unavailable.php
+++ config/unavailable.php
@@ -1,53 +1,68 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#">
   <head>
-	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	  <meta name="title" content="Yiid.com" />
-		<meta name="robots" content="index, follow" />
-		<meta name="description" content="Yiid helps you sharing your interesting websites" />
-		<meta name="keywords" content="yiid, social, network, sharing" />
-		<meta name="language" content="en" />
-		<title>yiid is offline</title>
-		
-		<link rel="stylesheet" type="text/css" media="screen" href="/css/001_layout.css" />
-		<link rel="stylesheet" type="text/css" media="screen" href="/css/002_color.css" />
-		<link rel="stylesheet" type="text/css" media="screen" href="/css/003_typo.css" />
-	  <link rel="shortcut icon" href="/img/global/favicon_16x16.ico" />
+    <title>Spread.ly - first choice for social sharing</title>
+    <meta name="description" content="Spread.ly consists of sharing button widgets that enable users to share content into a wide range of social networks simultanously. Besides that, Spread.ly offers a sophisticated functionality to reward users for their likes." />
+<meta name="keywords" content="sharing,sharebutton,like,likebutton,deal,dealbutton,facebook,linkedin,twitter,buzz" />
+<meta name="language" content="en" />
+<meta name="robots" content="index, follow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
+    <meta property="og:site_name" content="Spread.ly" />
+
+    <meta property="og:image" content="http://spreadly.com/img/spreadlyicon.jpg" />
+    <meta property="og:url" content="http://spreadly.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:email" content="info@spreadly.com" />
+    <meta property="og:phone_number" content="+49-6201-845200" />
+    <meta name="google-site-verification" content="0nH-7N_-Vix-3dGyWtqvJUn9IESoCtdi9wLN_Wp5MQs" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="/css/styles.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/css/yiid-styles.css" media="screen" />
+
+    <link rel="stylesheet" type="text/css" href="/css/print.css" media="print" />
+
+    <!--[if IE]>
+    <link rel="stylesheet" type="text/css" href="/css/ie.css" />
+    <![endif]-->
+    <!--[if IE 6]>
+    <script src="/js/DD_belatedPNG.js"> </script>
+    <script>
+      DD_belatedPNG.fix('*');
+    </script>
+    <![endif]-->
+    <link rel="shortcut icon" href="/favicon.ico" />
   </head>
-  <body class="bg_light clearfix">
-    <div id="container" class="bd_round clearfix">
-      <div id="error-msg-box">
-
-      </div>
-      <div id="header" class="clearfix">
-        <div id="header_sub" class="left">
-          <a href="http://www.yiid.com" id="yiid_logo">&nbsp;</a>
+  <body class="innerpage">
+    <div id="wrapper">
+      <!--feedback box start-->
+      <div class="feedback"><a href="http://getsatisfaction.com/spreadly" target="_blank"><img src="/img/feedback.png" alt="Feedback" title="Feedback" width="155" height="137" /></a></div>
+      <!--feedback box end-->
+      <div id="header">
+        <div class="clearfix">
+          <div id="logo-spread" class="alignleft"><a href="/"><img src="/img/spread-logo-domain.png" width="306" height="128" alt="" /></a></div>
+          <span class="alignleft easy-tagline">first choice for social sharing</span>
         </div>
-        <div id="header_main" class="left"></div>
-        <div id="header_supp" class="left"></div>
       </div>
 
-      <div id="content" class="clearfix">
-        <div id="error_box_404_unavailable" class="center_area container_border rounded_corners">
-	        <h1>Yiid.com is down for maintenance</h1>
-	        <p>yiid.com is currently being revised. Do please take a moment of patience. yiid.com is accessible again in a few minutes.</p>
-	        <p>yiid.com wird gerade überarbeitet. Habt bitte einen Moment Geduld. Gleich ist yiid.com wieder erreichbar.</p>
-	      </div>
+      <div>
+        <div class="grboxtop"><span></span></div>
+        <div class="grboxmid">
+          <div class="grboxmid-content">
+            <div class="panel_width">
+              <div class="graybox buttonbox">
+                <h2>Spread.ly is down for maintenance</h2>
+                <div style="padding: 30px 10px; font-size: 20px;">
+                  <p>Spread.ly is currently being revised. Do please take a moment of patience. Spread.ly is accessible again in a few minutes.</p>
+                  <p>Spread.ly wird gerade überarbeitet. Habt bitte einen Moment Geduld. Gleich ist Spread.ly wieder erreichbar.</p>
+                </div>
+              </div>
+            </div>
+            <div class="alignnone"></div>
+          </div>
+        </div>
+        <div class="grboxbot"><span></span></div>
       </div>
-
-      <div id="footer" class="clearfix">
-			  <ul class="normal_list clearfix">
-			    <li><a href="http://www.yiid.com/imprint">Imprint</a></li>
-			    <li>-</li>
-			    <li><a href="http://www.yiid.com/tos">Tos</a></li>
-			    <li>-</li>
-			    <li><a href="http://www.yiid.com/privacy">Privacy</a></li>
-			    <li>-</li>
-			    <li><a href="mailto:info@yiid.com">Contact</a></li>
-			  </ul>
-			</div>
 
     </div>
   </body>
