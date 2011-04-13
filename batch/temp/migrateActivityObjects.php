d68fc0770f78ff7564ce632bffc105d7f1e7545a
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-13 08:22:58 +0000

    removed some lines

--- batch/temp/migrateActivityObjects.php
+++ batch/temp/migrateActivityObjects.php
@@ -5,9 +5,7 @@ $configuration = ProjectConfiguration::getApplicationConfiguration('statistics',
 sfContext::createInstance($configuration);
 
 $logger = sfContext::getInstance()->getLogger();
-
 $dm = MongoManager::getDM();
-
 $yas = $dm->getRepository('Documents\YiidActivity')->findBy(array("social_object" => array('$exists' => false)))->limit(200);
 
 foreach ($yas as $ya) {
