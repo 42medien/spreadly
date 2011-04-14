b8a0210e95fde6148902ea32c644970a3ef8f1b3
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-14 17:11:42 +0000

    new frontend

--- apps/statistics/modules/analytics/templates/_domain_detail_content_by_day.php
+++ apps/statistics/modules/analytics/templates/_domain_detail_content_by_day.php
@@ -45,28 +45,55 @@
 <?php end_slot(); ?>
 <?php include_partial('global/graybox'); ?>
 
-
-<?php slot('content') ?>
-<div id="demo-pie-charts" class="clearfix">
-	<div class="alignleft">
-		<?php include_partial('analytics/chart_pie_example', array('pChartsettings' =>
-					'{
-								"width": 300,
-								"height": 130,
-								"margin": [ 0, 0, 10, 40],
-								"plotsize": "40%",
-								"bgcolor" : "#e1e1e1",
-								"renderto":"demo-pie-charts"
-						}'
-		)); ?>
-	</div>
+<?php
+if ($pHostSummary) {
+  slot('content');
+?>
+<div id="pie-charts" class="clearfix">
+  <div class="alignleft" id="gender-chart">
+    <?php include_partial('analytics/chart_pie_gender', array('pChartsettings' =>
+          '{
+                "width": 305,
+                "height": 180,
+                "margin": [ 40, 0, 10, 10],
+                "plotsize": "50%",
+                "bgcolor" : "#e1e1e1",
+                "renderto":"gender-chart"
+            }', 'pData' => $pHostSummary->getDemographics()
+    )); ?>
+  </div>
+  <div class="alignleft" id="age-chart">
+    <?php include_partial('analytics/chart_pie_age', array('pChartsettings' =>
+          '{
+                "width": 305,
+                "height": 180,
+                "margin": [ 40, 20, 10, 20],
+                "plotsize": "50%",
+                "bgcolor" : "#e1e1e1",
+                "renderto":"age-chart"
+            }', 'pData' => $pHostSummary->getDemographics()
+    )); ?>
+  </div>
+  <div class="alignleft" id="relation-chart">
+    <?php include_partial('analytics/chart_pie_relationship', array('pChartsettings' =>
+          '{
+                "width": 305,
+                "height": 180,
+                "margin": [ 40, 0, 10, 10],
+                "plotsize": "50%",
+                "bgcolor" : "#e1e1e1",
+                "renderto":"relation-chart"
+            }', 'pData' => $pHostSummary->getDemographics()
+    )); ?>
+  </div>
 </div>
-<?php end_slot(); ?>
-<?php include_partial('global/graybox'); ?>
+<?php
+  end_slot();
+  include_partial('global/graybox');
+}
 
-<?php if(count($pDeals) > 0) { ?>
-<?php slot('content') ?>
-	<?php include_partial('analytics/deal_table', array('pDeals' => $pDeals)); ?>
-<?php end_slot(); ?>
-<?php include_partial('global/graybox'); ?>
-<?php } ?>
+slot('content');
+  include_component('analytics', 'active_deal_table', array("host" => $pHostSummary->getHost()));
+end_slot();
+include_partial('global/graybox');
+?>
