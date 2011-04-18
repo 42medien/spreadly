cf9ec97e28428255d51d790d5f35e55fc8ed80c4
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-18 15:57:20 +0000

    done

---
+++ lib/mongo/Repositories/AnalyticsActivityRepository.php
@@ -0,0 +1,11 @@
+<?php
+namespace Repositories;
+
+use \MongoDate,
+    Documents\ActivityStats,
+    Documents\ActivityUrlStats,
+    Documents\Stats;
+
+class AnalyticsActivityRepository extends StatsRepository
+{
+}
