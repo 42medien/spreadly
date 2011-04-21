0e760a20bd0370b98c01d0b8e7effbf59cbdfef6
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-21 08:06:22 +0000

    migration file

--- batch/temp/migrateActivityObjects.php
+++ batch/temp/migrateActivityObjects.php
@@ -6,10 +6,27 @@ sfContext::createInstance($configuration);
 
 $logger = sfContext::getInstance()->getLogger();
 $dm = MongoManager::getDM();
-$yas = $dm->getRepository('Documents\YiidActivity')->findBy(array("social_object" => array('$exists' => false)))->limit(200);
 
-foreach ($yas as $ya) {
-  $ya->setNewChecksum(time());
-  $ya->save();
+for ($i = 0; $i < 15; $i++) {
+  $yas = $dm->getRepository('Documents\YiidActivity')->findBy(array("social_object" => array('$exists' => false)))->limit(10);
+
+  print_r("yiid-activity migration\n============================================================\n\n");
+
+  foreach ($yas as $ya) {
+    try {
+      @StatsFeeder::feed($ya);
+
+      if (@$so = MongoManager::getDM()->getRepository("Documents\SocialObject")->find($ya->getSoId())) {
+        @$ya->setSocialObject($so);
+        @$ya->save();
+        unset($so);
+      }
+
+      print_r("yiid-activity: ".$ya->getId()."\n");
+      unset($ya);
+    } catch (Exception $e) {
+      print_r($e->getMessage());
+    }
+  }
 }
 ?>
