76d9d17444d1c3f7d01618f6c6118c2beefabc9f
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-21 13:45:09 +0000

    added prod environment

--- batch/temp/migrateActivityObjects.php
+++ batch/temp/migrateActivityObjects.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('statistics', 'dev', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('statistics', 'prod', false);
 sfContext::createInstance($configuration);
 
 $logger = sfContext::getInstance()->getLogger();
