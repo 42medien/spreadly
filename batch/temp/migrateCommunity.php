6df861559912ff2abcf8303a6887af3d1e429805
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-27 15:32:23 +0000

    zeugs

--- batch/temp/migrateCommunity.php
+++ batch/temp/migrateCommunity.php
@@ -12,11 +12,18 @@ $repos = array('Documents\AnalyticsActivity', 'Documents\ActivityStats', 'Docume
 foreach ($repos as $repo) {
   $services = array('Facebook', 'Twitter', 'Google', 'LinkedIn', 'Linkedin');
   echo "Fixing Repository: $repo …\n";
-  
+  $psid_count = 0;
   foreach ($services as $service) {
     $muh = $dm->createQueryBuilder($repo)
-              ->where("function() { return this.s.$service != undefined; }")
+              ->where("function() { return this.s.$service != undefined; }")->limit(10)
               ->getQuery()->execute();
+
+    if(!$muh->hasNext()) {
+      $psid_count++;
+      break;
+    }
+
+    echo "=============================== Query done for: $service\n";
     $i = 0;
     foreach ($muh as $el) {
       $s = $el->getServices();
@@ -25,14 +32,21 @@ foreach ($repos as $repo) {
         $s[$low][$key] = (array_key_exists($low, $s) && array_key_exists($key, $s[$low])) ? $s[$low][$key]+$value : $value;
       }
       unset($s[$service]);
-      
+
       $el->setServices($s);
       $dm->persist($el);
       $dm->flush();
       $i++;
+
+      echo "Element $i done\n";
     }
     echo "Fixed $i entries for service: $service\n";
   }
+
+  if ($psid_count>=count($services)) {
+    echo "PFEFFI_SAYS_ITS_DONE";
+  }
+
 }
 
 ?>
--- batch/temp/scripterle.rb
+++ batch/temp/scripterle.rb
@@ -2,7 +2,7 @@
 i=0
 while true
  puts "Starting batch No. #{i+=1}\n--------------------------------------------------\n"
- result = `php migrateActivityObjects.php`
+ result = `php migrateCommunity.php`
  puts result
  puts "\n"
  break if result.match(/PFEFFI_SAYS_ITS_DONE/) || i>1000
