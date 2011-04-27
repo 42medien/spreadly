f0a69ed4f28f3612ad53024e153f713b9c69387e
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-27 14:25:20 +0000

    Migration for wrong service keys

--- batch/temp/migrateCommunity.php
+++ batch/temp/migrateCommunity.php
@@ -7,11 +7,32 @@ sfContext::createInstance($configuration);
 $logger = sfContext::getInstance()->getLogger();
 $dm = MongoManager::getStatsDM();
 
-$muh = $dm->createQueryBuilder('Documents\DealUrlStats')
-          ->where("function() { return this.s.Facebook != undefined || this.s.Twitter != undefined || this.s.Google != undefined || this.s.Linkedin != undefined || this.s.LinkedIn != undefined; }")
-          ->getQuery()->execute();
+$repos = array('Documents\AnalyticsActivity', 'Documents\ActivityStats', 'Documents\ActivityUrlStats', 'Documents\DealStats', 'Documents\DealUrlStats', 'Documents\DealSummary', 'Documents\DealUrlSummary', 'Documents\HostSummary', 'Documents\UrlSummary');
 
-foreach ($muh as $argh) {
-  var_dump($argh->getServices());
+foreach ($repos as $repo) {
+  $services = array('Facebook', 'Twitter', 'Google', 'LinkedIn', 'Linkedin');
+  echo "Fixing Repository: $repo …\n";
+  
+  foreach ($services as $service) {
+    $muh = $dm->createQueryBuilder($repo)
+              ->where("function() { return this.s.$service != undefined; }")
+              ->getQuery()->execute();
+    $i = 0;
+    foreach ($muh as $el) {
+      $s = $el->getServices();
+      foreach ($s[$service] as $key => $value) {
+        $low = strtolower($service);
+        $s[$low][$key] = (array_key_exists($low, $s) && array_key_exists($key, $s[$low])) ? $s[$low][$key]+$value : $value;
+      }
+      unset($s[$service]);
+      
+      $el->setServices($s);
+      $dm->persist($el);
+      $dm->flush();
+      $i++;
+    }
+    echo "Fixed $i entries for service: $service\n";
+  }
 }
+
 ?>
