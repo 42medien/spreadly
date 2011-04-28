02be1baa1458b874e41bd90c967509d95830cf05
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-04-28 10:22:25 +0000

    del

--- apps/backend/modules/domain/templates/listExportCsvSuccess.php
+++
@@ -1,6 +0,0 @@
-<?php use_helper('Date'); ?>
-E-Mail,Username,Vorname,Nachname,Registrierung,Letzter Login,Domain,Verifiziert
-<?php foreach($pDomainProfiles as $dp): ?>
-<?php echo $dp->getSfGuardUser()->getEmailAddress() ?>,<?php echo $dp->getSfGuardUser()->getUsername() ?>,<?php echo $dp->getSfGuardUser()->getProfile()->getFirstname() ?>,<?php echo $dp->getSfGuardUser()->getProfile()->getLastname() ?>,<?php echo format_date($dp->getSfGuardUser()->getCreatedAt(), "d.M.y") ?>,<?php echo format_date($dp->getSfGuardUser()->getLastLogin(), "d.M.y") ?>,<?php echo $dp->getDomain() ?>,<?php echo $dp->getState() ?>
-
-<?php endforeach; ?>
