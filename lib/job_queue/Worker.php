3d4f3834262e4f6d5e3d9cfa0432a854f5f5d7c0
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-05-02 16:22:15 +0000

    Added first rough untested implementation of new job queue

---
+++ lib/job_queue/Worker.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Worker
+ *
+ * @author Hannes Schippmann
+ */
+class Worker {
+  // Basically the YiidDaemon, without the SQS specific stuff.
+  // Runs in an infinite loop
+  // Gets Jobs from JobQueue and calls execute on them
+}
