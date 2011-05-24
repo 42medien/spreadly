eab4a6e8f0d36b8a88a2ffe6f76fd28be3cd0884
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-05-24 08:36:51 +0000

    DISCH

--- apps/widget/modules/deals/templates/_coupon_used.php
+++ apps/widget/modules/deals/templates/_coupon_used.php
@@ -11,7 +11,15 @@
     	<h2 class="graytitle txtcenter"><?php echo $lDeal->getDescription(); ?></h2>
       <div class="whtrow codebox">
     		<div class="rcor">
-        	<span class="fs13"><?php echo __('Your coupon code:'); ?></span><br /><span class="code"><?php echo auto_link_to($pActivity->getCCode(), 30, array('target' => '_blank'));?></span>
+        	<span class="fs13">
+        	  <?php
+            if (UrlUtils::isUrlValid($pActivity->getCCode())) {
+              echo __('Get your download here:');
+            } else {
+              echo __('Your Coupon Code:');
+            }
+            ?>
+          </span><br /><span class="code"><?php echo auto_link_to($pActivity->getCCode(), 30, array('target' => '_blank'));?></span>
         </div>
       </div>
     </div>
