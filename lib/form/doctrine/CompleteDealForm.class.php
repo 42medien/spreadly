38bee69a52f4e89af7fe2b1af6737d552a6b78df
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-05-25 15:43:22 +0000

    Added url to deal schema and embedded coupon form in backend new deal

--- lib/form/doctrine/CompleteDealForm.class.php
+++ lib/form/doctrine/CompleteDealForm.class.php
@@ -5,9 +5,14 @@
  *
  * @package    yiid_stats
  * @subpackage form
- * @author     Your name here
+ * @author     hannes
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class CompleteDealForm extends BaseDealForm
 {
+  public function configure()
+  {
+    $couponForm = new CouponForm();
+    $this->embedForm('Coupon', $couponForm);
+  }
 }
