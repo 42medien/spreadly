91661b92373218443cf9f21c705554f8ee7a8347
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-15 11:53:22 +0000

    pfffff

--- apps/backend/modules/dashboard/templates/_widget_single.php
+++ apps/backend/modules/dashboard/templates/_widget_single.php
@@ -2,7 +2,10 @@
 	<header><section><?php echo $title ?></section></header>
 	<section class="widget-data-section">
 		<div class="large"><?php echo $data['current_'.$type.'_count'] ?></div>
-		<div class="bigger <?php echo upDownClass($data[$type.'_count_delta']) ?>"><div class="<?php echo upDownClass($data[$type.'_count_delta']) ?>-arrow-small left">&nbsp;</div><?php echo absOrInfinity($data[$type.'_count_delta']) ?><small>&nbsp;%</small></div>
+		<div class="bigger <?php echo upDownClass($data[$type.'_count_delta']) ?>">
+		  <?php echo upDownArrow($data[$type.'_count_delta']) ?>&nbsp;
+		  <?php echo absOrInfinity($data[$type.'_count_delta']) ?><small>&nbsp;%</small>
+		</div>
 	</section>
 	<footer>
 		Powered by @Spreadly
--- lib/helper/DashboardHelper.php
+++ lib/helper/DashboardHelper.php
@@ -4,6 +4,10 @@ function upDownClass($value) {
   return $value === 0 ? 'neutral' : (($value > 0 || $value=='∞') ? 'positive' : 'negative');
 }
 
+function upDownArrow($value) {
+  return $value === 0 ? '&#9658' : (($value > 0 || $value=='∞') ? '&#9650' : '&#9660');
+}
+
 function absOrInfinity($value) {
   return $value == '∞' ? $value : abs($value);
 }
