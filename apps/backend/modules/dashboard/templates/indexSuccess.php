38bd7b52d941d80a8aa208aff9e679dc8d808ae0
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-16 09:49:33 +0000

    layout

--- apps/backend/modules/dashboard/templates/indexSuccess.php
+++ apps/backend/modules/dashboard/templates/indexSuccess.php
@@ -17,34 +17,18 @@
 			Powered by @Spreadly
 		</footer>
 	</div>
-
   <?php include_partial('widget_single', array('title' => 'spread.ly User', 'data' => $data, 'type' => 'user')) ?>
   <?php include_partial('widget_single', array('title' => 'spreadly.com User', 'data' => $data, 'type' => 'stats_user')) ?>
-  <?php include_partial('widget_single', array('title' => 'Likes & Deals', 'data' => $data, 'type' => 'likes')) ?>
+  <?php include_partial('widget_single', array('title' => 'All Likes', 'data' => $data, 'type' => 'likes')) ?>
   <?php include_partial('widget_single', array('title' => 'Domains claimed', 'data' => $data, 'type' => 'domain')) ?>
+
   <?php include_partial('widget_single', array('title' => 'Domains verified', 'data' => $data, 'type' => 'verified_domain')) ?>
 	<?php include_partial('widget_single', array('title' => 'Media penetration', 'data' => $data, 'type' => 'media_penetration')) ?>
 	<?php include_partial('widget_single', array('title' => 'Clickbacks', 'data' => $data, 'type' => 'clickback')) ?>
-	<?php include_partial('widget_single', array('title' => 'Clickback-Likes', 'data' => $data, 'type' => 'cbl')) ?>
-
   <?php include_partial('widget_piechart', array('title' => 'Services', 'data' => $data, 'type' => 'services')) ?>
-	<?php include_partial('widget_linechart', array('title' => 'Likes', 'data' => $data, 'type' => 'likes', 'range' => $range)) ?>
-
 </section>
-
 <section class="main-section chart-section clearfix">
-	<div class="widget widget-single large" style="text-align: center;"><h3>Spreadly</h3><div class="large">1350</div><div class="big">5%</div></div>
-	<div class="widget widget-single">2</div>
-	<div class="widget widget-single">3</div>
-	<div class="widget widget-single">4</div>
-	<div class="widget widget-double">5</div>
-	<div class="widget widget-single">6</div>
+	<?php include_partial('widget_linechart', array('title' => 'Domains verified', 'data' => $data, 'type' => 'verified_domain')) ?>
 </section>
-<section class="main-section table-section clearfix">
-	<div class="widget widget-single large" style="text-align: center;"><h3>Spreadly</h3><div class="large">1350</div><div class="big">5%</div></div>
-	<div class="widget widget-single">2</div>
-	<div class="widget widget-single">3</div>
-	<div class="widget widget-single">4</div>
-	<div class="widget widget-double">5</div>
-	<div class="widget widget-single">6</div>
-</section>
+
+
