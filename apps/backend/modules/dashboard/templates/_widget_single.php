60cffb3651de573e735d986207b7185509efe730
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-21 15:49:05 +0000

    truncate number

--- apps/backend/modules/dashboard/templates/_widget_single.php
+++ apps/backend/modules/dashboard/templates/_widget_single.php
@@ -1,7 +1,9 @@
+<?php use_helper('YiidNumber'); ?>
+
 <div class="widget widget-single widget-one-section">
 	<header><section><?php echo $title ?></section></header>
 	<section class="widget-data-section">
-		<div class="large"><?php echo $data['current_'.$type.'_count'] ?></div>
+		<div class="large"><?php echo truncate_number($data['current_'.$type.'_count']) ?></div>
 		<div class="bigger <?php echo upDownClass($data[$type.'_count_delta']) ?>">
 		  <?php echo upDownArrow($data[$type.'_count_delta']) ?>&nbsp;
 		  <?php echo absOrInfinity($data[$type.'_count_delta']) ?><small>&nbsp;%</small>
