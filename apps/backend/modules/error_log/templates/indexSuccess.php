aa273c7533ffff7eb1ee011f3b7723ad8db3c0ce
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-30 16:15:32 +0000

    changed some text

--- apps/backend/modules/error_log/templates/indexSuccess.php
+++ apps/backend/modules/error_log/templates/indexSuccess.php
@@ -3,8 +3,8 @@
   <li>
     <small class="meta"><?php echo $e->getC()->format("d.m.Y @ H:i:s"); ?><br /><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getProfileUri(); ?></small>
     <h3>
-      <strong><em><?php echo UserTable::getInstance()->find($e->getUId())->getFullname(); ?></em></strong> made a mistake on
-      <strong><em><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getCommunity()->getName(); ?></em></strong>
+      <strong><em><?php echo UserTable::getInstance()->find($e->getUId())->getFullname(); ?></em></strong>s
+      <strong><em><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getCommunity()->getName(); ?></em></strong>-account has hiccups
     </h3>
     <p>Error: {<?php echo $e->getCode(); ?>} <?php echo $e->getMessage(); ?></p>
   </li>
