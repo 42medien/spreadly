241945894ca91851f10163c6643ae8abc300706d
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-30 08:32:47 +0000

    added error_log

---
+++ apps/backend/modules/error_log/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * error_log actions.
+ *
+ * @package    yiid
+ * @subpackage error_log
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class error_logActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request) {
+    $lDm = MongoManager::getDM();
+    $this->pErrors = $lDm->createQueryBuilder("Documents\ApiErrorLog")->find()->getQuery()->execute();
+  }
+}
