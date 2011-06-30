712583eae4f946a3168c741df6bb356449ad0230
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-06-30 09:01:25 +0000

    error log

--- apps/backend/modules/error_log/templates/indexSuccess.php
+++ apps/backend/modules/error_log/templates/indexSuccess.php
@@ -1,13 +1,12 @@
 <ul class="errors">
 <?php foreach ($pErrors as $e) { ?>
   <li>
-    <small>@ 14.10.1978</small>
+    <small class="meta"><?php echo $e->getC()->format("d.m.Y @ H:i:s"); ?><br /><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getProfileUri(); ?></small>
     <h3>
       <strong><em><?php echo UserTable::getInstance()->find($e->getUId())->getFullname(); ?></em></strong> made a mistake on
       <strong><em><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getCommunity()->getName(); ?></em></strong>
-      (<?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getProfileUri(); ?>)
     </h3>
-    Error: {<?php echo $e->getCode(); ?>} <?php echo $e->getMessage(); ?>
+    <p>Error: {<?php echo $e->getCode(); ?>} <?php echo $e->getMessage(); ?></p>
   </li>
 <?php } ?>
 </ul>
