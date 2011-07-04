e10e04fb7343e8fbf48694b20ad554d2734d3629
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-04 12:26:24 +0000

    bezahlformular

--- apps/statistics/modules/system/templates/credentialsSuccess.php
+++ apps/statistics/modules/system/templates/credentialsSuccess.php
@@ -1,6 +1,6 @@
 <?php slot('content') ?>
 
-<iframe src="https://spreadsheets.google.com/a/ekaabo.com/spreadsheet/embeddedform?formkey=dEdCcnJhUElhS2dpNjQ1NHk3Nl9wTmc6MQ&entry_13=<?php echo $domainProfile->getDomain(); ?>&entry_20=<?php echo $sf_user->getGuardUser()->getEmailAddress(); ?>" width="760" height="1358" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+<iframe src="https://spreadsheets.google.com/a/ekaabo.com/spreadsheet/embeddedform?formkey=dEdCcnJhUElhS2dpNjQ1NHk3Nl9wTmc6MQ&entry_13=<?php echo $domainProfile->getDomain(); ?>&entry_20=<?php echo $sf_user->getGuardUser()->getEmailAddress(); ?>" width="760" height="1018" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 
 <?php end_slot(); ?>
 <?php include_partial('global/graybox'); ?>
