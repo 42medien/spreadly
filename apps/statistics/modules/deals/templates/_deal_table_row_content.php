03341f8bfe4a6a5822758247b59de0b446eb08ff
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-05 13:56:20 +0000

    stricherle

--- apps/statistics/modules/deals/templates/_deal_table_row_content.php
+++ apps/statistics/modules/deals/templates/_deal_table_row_content.php
@@ -83,17 +83,17 @@
 <?php } ?>
 
 <?php if($pDeal->getState() == 'paused') { ?>
-			<td align="center" class="first"><?php echo image_tag('/img/global/24x24/clock.png', array('title' => __('Deal stopped')))?></td>
+			<td align="center" class="first"><div><?php echo image_tag('/img/global/24x24/clock.png', array('title' => __('Deal stopped')))?></div></td>
 		  <td align="left"><div class="marketbox" title="<?php echo __('Go to analytics for %deal%', array('%deal%' => $pDeal->getSummary())); ?>"><?php echo link_to(truncate_text($pDeal->getSummary(), 20), 'deal_analytics/deal_statistics?deal_id='.$pDeal->getId(), array('class' => 'paused-link')); ?></div></td>
 		  <td align="left" valign="middle"><div class="marketbox"><?php echo $pDeal->getDomainProfile()->getUrl(); ?></div></td>
 		  <td align="left" valign="middle"><div class="marketbox"><?php echo $pDeal->getStartDate(); ?></div></td>
-		  <td align="left" valign="middle" title="<?php echo __('Edit end date of deal'); ?>" class="editinplaceclick edit-col" id="deal-date-<?php echo $pDeal->getId(); ?>" data-obj='{"action":"/deals/edit_enddate", "type":"text", "callback":"DealTable.editDate",  "params": "{\"id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-date-<?php echo $pDeal->getId(); ?>\"}"}'><div  class="marketbox"><?php echo $pDeal->getEndDate(); ?></div></td>
-		  <td align="center" valign="middle"><?php echo $pDeal->getCouponClaimedQuantity(); ?></td>
+		  <td align="left" valign="middle" title="<?php echo __('Edit end date of deal'); ?>" class="editinplaceclick edit-col" id="deal-date-<?php echo $pDeal->getId(); ?>" data-obj='{"action":"/deals/edit_enddate", "type":"text", "callback":"DealTable.editDate",  "params": "{\"id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-date-<?php echo $pDeal->getId(); ?>\"}"}'><div class="marketbox"><?php echo $pDeal->getEndDate(); ?></div></td>
+		  <td align="center" valign="middle"><div><?php echo $pDeal->getCouponClaimedQuantity(); ?></div></td>
 		  <td align="center" title="<?php echo __('Edit Coupon Quantity'); ?>" class="<?php if(!$pDeal->isUnlimited()) { ?>editinplaceclick <?php } ?> edit-col" id="deal-quantity-<?php echo $pDeal->getId(); ?>"
       	<?php if($pDeal->getCouponType() == "single"){ ?>data-obj='{"action":"/deals/save_quantity", "type":"text",  "params": "{\"deal_id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
       	<?php } else { ?> data-obj='{"action":"/deals/paste_codes?deal_id=<?php echo $pDeal->getId(); ?>", "type":"layer", "params": "{\"deal_id\":\"<?php echo $pDeal->getId(); ?>\", \"cssid\":\"deal-quantity-<?php echo $pDeal->getId(); ?>\"}"}'
-      	<?php } ?>><?php echo __($pDeal->getHumanCouponQuantity()); ?></td>
-		  <td align="center"><?php echo link_to(image_tag('/img/global/24x24/tools.png', array('title' => __('Edit Deal'))), '/deals/get_create_form?deal_id='.$pDeal->getId(), array('class' => 'link-deal-content')); ?></td>
+      	<?php } ?>><div><?php echo __($pDeal->getHumanCouponQuantity()); ?></div></td>
+		  <td align="center"><div><?php echo link_to(image_tag('/img/global/24x24/tools.png', array('title' => __('Edit Deal'))), '/deals/get_create_form?deal_id='.$pDeal->getId(), array('class' => 'link-deal-content')); ?></div></td>
 		  <td align="center" class="last">
       	<?php echo link_to(image_tag('/img/global/24x24/trash.png', array('title' => __('Delete Deal'))), '/deals/set_state?deal_id='.$pDeal->getId().'&state=trashed', array('class' => 'edit-state')); ?>
         <?php echo link_to(image_tag('/img/global/24x24/next.png', array('title' => __('Proceed Deal'))), '/deals/set_state?deal_id='.$pDeal->getId().'&state=approved', array('class' => 'edit-state')); ?>
