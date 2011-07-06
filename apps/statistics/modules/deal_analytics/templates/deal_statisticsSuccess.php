28463da9ca7ec0fd6021ee2a033e1c2846aec1ef
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-06 16:20:39 +0000

    wildcards

--- apps/statistics/modules/deal_analytics/templates/deal_statisticsSuccess.php
+++ apps/statistics/modules/deal_analytics/templates/deal_statisticsSuccess.php
@@ -13,7 +13,7 @@ use_helper('YiidNumber');
 		<li class="bc-last"></li>
 	</ul>
 </div>
-<h2 class="sub_title"><?php echo __('Overview for deal %deal%', array('%deal%' => '"'.$pDeal->getSummary().'" from '.date("d.m.Y", strtotime($pDeal->getStartDate())).' to '.date("d.m.Y", strtotime($pEndDate))));?></h2>
+<h2 class="sub_title"><?php echo __('Overview for deal "%deal%', array('%deal%' => $pDeal->getSummary().'"'.'&nbsp;from&nbsp;'.date("d.m.Y", strtotime($pDeal->getStartDate())).'&nbsp;to&nbsp;'.date("d.m.Y", strtotime($pEndDate))));?></h2>
 <div id="navi" class="clearfix">
   <div class="stepBox alignleft">
     <div class="box_container alignleft">
