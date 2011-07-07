8b6246ddbec938ff4bf074ed5baa30867edcc8c0
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-07 11:59:39 +0000

    added email address

--- apps/backend/modules/error_log/templates/indexSuccess.php
+++ apps/backend/modules/error_log/templates/indexSuccess.php
@@ -1,9 +1,15 @@
 <ul class="errors">
-<?php foreach ($pErrors as $e) { ?>
+<?php
+foreach ($pErrors as $e) {
+  $user = UserTable::getInstance()->find($e->getUId());
+?>
   <li>
-    <small class="meta"><?php echo $e->getC()->format("d.m.Y @ H:i:s"); ?><br /><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getProfileUri(); ?></small>
+    <small class="meta"><?php echo $e->getC()->format("d.m.Y @ H:i:s"); ?><br />
+    <?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getProfileUri(); ?>
+    <?php if ($user->getEmail()) { echo "<br />".$user->getEmail(); } ?>
+    </small>
     <h3>
-      <strong><em><?php echo UserTable::getInstance()->find($e->getUId())->getFullname(); ?></em></strong>s
+      <strong><em><?php echo $user->getFullname(); ?></em></strong>s
       <strong><em><?php echo OnlineIdentityTable::getInstance()->find($e->getOiId())->getCommunity()->getName(); ?></em></strong>-account has hiccups
     </h3>
     <p>Error: {<?php echo $e->getCode(); ?>} <?php echo $e->getMessage(); ?></p>
