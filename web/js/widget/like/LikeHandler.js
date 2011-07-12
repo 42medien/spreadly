179341dc42af3243cace7e456a184337771784fd
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-12 14:58:34 +0000

    fixed js

--- web/js/widget/like/LikeHandler.js
+++ web/js/widget/like/LikeHandler.js
@@ -17,24 +17,24 @@ var WidgetDealForm = {
       // reset the form after side-reload (fix for ff)
       if (typeof (document.popupdealform) != "undefined") {
         document.popupdealform.reset();
-      }         
-      
+      }
+
       WidgetDealForm.doSend();
     },
-    
+
     /**
      * sends the like to the backend
      * @author KM
      */
     doSend: function() {
-      debug.log('[WidgetDealForm][doSend]');      
-      
+      debug.log('[WidgetDealForm][doSend]');
+
       jQuery('#popup-send-deal-button').bind('click', function() {
-        var lAction = jQuery('#popupdealform').attr('action');         
+        var lAction = jQuery('#popupdealform').attr('action');
         OnLoadGrafic.showGrafic();
         WidgetDealForm.hideButton();
         var options = {
-          url : lAction,    
+          url : lAction,
           data : {
             ei_kcuf : new Date().getTime()
           },
@@ -45,7 +45,7 @@ var WidgetDealForm = {
               debug.log(pResponse.html);
               jQuery('#coupon-unused-container').empty();
               jQuery('#coupon-unused-container').append(pResponse.html);
-              
+
             } else {
               WidgetDealForm.showButton();
               WidgetDealForm.showErrorMsg();
@@ -53,20 +53,20 @@ var WidgetDealForm = {
             OnLoadGrafic.hideGrafic();
           }
         };
-        
+
          jQuery('#popupdealform').ajaxSubmit(options);
          return false;
-      });      
+      });
     },
-    
+
     hideButton: function() {
       jQuery('#popup-send-deal-box').hide();
     },
-    
+
     showButton: function(){
-      jQuery('#popup-send-deal-box').show();      
+      jQuery('#popup-send-deal-box').show();
     },
-    
+
     showErrorMsg: function() {
       debug.log('[showErrorMsg]');
       jQuery('.error').show();
@@ -74,14 +74,14 @@ var WidgetDealForm = {
         lTimeout = setTimeout(function() {
           jQuery('.error').hide();
         }, 3000);
-    }    
+    }
 };
 
 
 var WidgetLikeForm = {
-    
+
     aComment: " ",
-    
+
     /**
      * inits the form-functions
      */
@@ -91,28 +91,28 @@ var WidgetLikeForm = {
       jQuery('#area-like-comment').toggleValue();
       jQuery('.mirror-value').mirrorValue();
       WidgetLikeForm.aComment = jQuery('#area-like-comment').val();
-      
+
       /* reset the form after side-reload (fix for ff)
       if (typeof (document.popup-like-form) != "undefined") {
         document.popup-like-form.reset();
-      } */     
-      
+      } */
+
     },
-    
-    
+
+
     /**
      * sends the like to the backend
      * @author KM
      */
     doSend: function() {
-      debug.log('[WidgetLikeForm][doSend]');      
-      
+      debug.log('[WidgetLikeForm][doSend]');
+
       jQuery('#popup-send-like-button').bind('click', function() {
-        var lAction = jQuery('#popup-like-form').attr('action');         
+        var lAction = jQuery('#popup-like-form').attr('action');
         OnLoadGrafic.showGrafic();
         WidgetLikeForm.hideButton();
         //WidgetLikeForm.hideTextarea();
-        
+
         var options = {
           beforeSerialize : WidgetLikeForm.checkComment,
           url : lAction,
@@ -123,25 +123,25 @@ var WidgetLikeForm = {
           dataType : 'json',
           success : function(pResponse) {
             if(pResponse.success == true) {
-              WidgetLikeForm.removeButton(); 
-              //WidgetLikeForm.removeTextarea(); 
+              WidgetLikeForm.removeButton();
+              //WidgetLikeForm.removeTextarea();
               WidgetLikeForm.removeShares();
               WidgetLikeForm.showSuccessMsg();
             } else {
-              WidgetLikeForm.showButton(); 
+              WidgetLikeForm.showButton();
               //WidgetLikeForm.showTextarea();
               WidgetLikeForm.showErrorMsg();
             }
-            
+
             OnLoadGrafic.hideGrafic();
           }
         };
-        
+
          jQuery('#popup-like-form').ajaxSubmit(options);
          return false;
-      });      
+      });
     },
-    
+
     checkComment: function(form, options) {
       var lComment = jQuery(form[0]["like[comment]"]).val();
       if(Utils.trim(lComment) == Utils.trim(WidgetLikeForm.aComment)){
@@ -151,8 +151,8 @@ var WidgetLikeForm = {
         form[0]["like[comment]"].defaultValue = " ";
       }
       return true;
-    },    
-    
+    },
+
     /**
      * sets the hidden img-value
      * @author KM
@@ -161,51 +161,51 @@ var WidgetLikeForm = {
     setImageValue: function(pPath){
       jQuery('#like-img-value').val(pPath);
     },
-    
+
     hideButton: function() {
       jQuery('#popup-send-like-button').hide();
     },
-    
+
     removeButton: function() {
-      jQuery('#popup-send-like-button').remove();      
+      jQuery('#popup-send-like-button').remove();
     },
-    
+
     showButton: function(){
-      jQuery('#popup-send-like-button').show();      
+      jQuery('#popup-send-like-button').show();
     },
-    
+
     removeShares: function(){
-      jQuery('#like-oi-list').remove();   
+      jQuery('#like-oi-list').remove();
     },
-    
+
     /*
     hideTextarea: function() {
       jQuery('#comment-area').hide();
     },
-    
+
     removeTextarea: function() {
-      jQuery('#comment-area').remove();      
+      jQuery('#comment-area').remove();
     },
-    
+
     showTextarea: function() {
-      jQuery('#comment-area').show();            
+      jQuery('#comment-area').show();
     },*/
-    
+
     showErrorMsg: function() {
       debug.log('[showErrorMsg]');
-      jQuery('#like-response').empty();      
-      jQuery('#like-response').append('<span class="error">'+i18n.get('like_error_message')+"</span>");      
-      
+      jQuery('#like-response').empty();
+      jQuery('#like-response').append('<span class="error">'+i18n.get('like_error_message')+"</span>");
+
       var lTimeout;
         lTimeout = setTimeout(function() {
           jQuery('.error').hide('slow');
           jQuery('.error').remove();
-        }, 5000);      
+        }, 5000);
     },
-    
+
     showSuccessMsg: function() {
       debug.log('[showSuccessMsg]');
-      
+
       jQuery('#comment-area').empty();
       jQuery('#comment-area').append('<span class="success">'+i18n.get('like_success_message')+"</span><a href='/' id='close-popup-link'>"+i18n.get('close_popup')+"</a>");
       WidgetLikeForm.closePopup();
@@ -216,7 +216,7 @@ var WidgetLikeForm = {
           jQuery('#comment-area').remove();
         }, 5000);*/
     },
-    
+
     closePopup: function(){
       jQuery('#close-popup-link').live('click', function() {
         window.close();
@@ -230,12 +230,12 @@ var WidgetLikeForm = {
  * @author KM
  */
 var LikeImage = {
-    
+
     init: function(pImgCount, pUrl) {
      debug.log("[LikeImage][init]");
      if(pImgCount == 0) {
         LikeImage.get(pUrl);
-     } else if (pImgCount == 1) { 
+     } else if (pImgCount == 1) {
        WidgetLikeForm.setImageValue(LikeImage.getImgPath(0));
        LikeImageCounter.hide();
      } else {
@@ -245,21 +245,21 @@ var LikeImage = {
        LikeImageScroller.onScroll();
      }
     },
-      
+
     /**
      * if there is no image given by the meta-tag-parser, try to get some from the html-content from the given url
-     * @author KM 
+     * @author KM
      * @param string pUrl
      */
     get: function(pUrl) {
-      debug.log("[LikeImage][get]");      
+      debug.log("[LikeImage][get]");
       OnLoadGrafic.insertGraficToElem(jQuery('#myscroll'));
       var lAction = '/like/get_images';
       var lData = {
         ei_kcuf : new Date().getTime(),
         url: pUrl
       };
-      
+
       jQuery.ajax({
         //beforeSubmit : OnLoadGrafic.showGrafic,
         type : "GET",
@@ -269,9 +269,9 @@ var LikeImage = {
         success : function(pResponse) {
           LikeImage.handleResponse(pResponse);
         }
-      });        
+      });
     },
-    
+
     /**
      * handles the response after the get-images
      * @author KM
@@ -285,7 +285,7 @@ var LikeImage = {
         LikeImageCounter.hide();
         LikeImageScroller.hideContainer();
       } else {
-        // if there are more than 1 images: 
+        // if there are more than 1 images:
         // init the scroller
         LikeImageScroller.init(true);
         //init the counter
@@ -293,14 +293,14 @@ var LikeImage = {
         //show the slide-arrows and the counter
         LikeImageCounter.show();
         //and init the onscroll-functionalities (e.g. update counter & update hidden-img-value onscroll)
-        LikeImageScroller.onScroll();          
+        LikeImageScroller.onScroll();
       }
-      
+
       //fill the hidden-img-value with the path of the first image
-      WidgetLikeForm.setImageValue(LikeImage.getImgPath(0));      
+      WidgetLikeForm.setImageValue(LikeImage.getImgPath(0));
       OnLoadGrafic.removeGraficFromElem(jQuery('#myscroll'));
     },
-    
+
     /**
      * inserts the images into the scroll-area
      * @author KM
@@ -310,7 +310,7 @@ var LikeImage = {
       jQuery('#scroll-meta-images').empty();
       jQuery('#scroll-meta-images').append(pHtml);
     },
-    
+
 
     /**
      * returns the image-path of a given index(position of the image into the slider)
@@ -320,7 +320,7 @@ var LikeImage = {
      */
     getImgPath: function(pIndex) {
       return jQuery('#meta-img-'+pIndex).attr('src');
-    } 
+    }
 };
 
 
@@ -330,7 +330,7 @@ var LikeImage = {
  */
 var LikeImageScroller = {
   aApiObj: {},
-  
+
   /**
    * init the scrollable-plugin and set global vars
    * @author KM
@@ -338,20 +338,20 @@ var LikeImageScroller = {
    */
   init: function(pCircular){
     //check, if we would an endless slide-show
-    var lCircular = false; 
+    var lCircular = false;
     if(pCircular !== undefined) {
       lCircular = true;
     }
-    
+
     //init the plugin
     jQuery("#myscroll").scrollable({
       //circular: lCircular
     });
-   
+
     //init the global apiobject with the plugins-api-object
     LikeImageScroller.aApiObj = jQuery('#myscroll').data("scrollable");
   },
-  
+
   /**
    * calls the scrollable api on scrolling thru the images
    * http://flowplayer.org/tools/documentation/scripting.html
@@ -366,12 +366,12 @@ var LikeImageScroller = {
       WidgetLikeForm.setImageValue(LikeImage.getImgPath(this.getIndex()));
     });
   },
-  
+
   hideContainer: function() {
     jQuery('.subdetail_img').hide();
   }
-  
-  
+
+
 };
 
 
@@ -383,14 +383,14 @@ var LikeImageCounter = {
 
   /**
    * sets the total number
-   * @author KM 
+   * @author KM
    * @param number pCount
    */
   init: function(pCount) {
     jQuery('#img-number').empty();
     jQuery('#img-number').append(pCount);
   },
-  
+
   /**
    * updates the counter, e.g. after scrolling
    * @author KM
@@ -400,28 +400,28 @@ var LikeImageCounter = {
     jQuery('#img-counter').empty();
     jQuery('#img-counter').append(pCount+1);
   },
-  
+
   /**
    * shows the counter-area (including the slide-arrows)
    * @author KM
    */
   show: function() {
-    jQuery('#scroll-button-area').show();        
+    jQuery('#scroll-button-area').show();
   },
-  
+
   /**
    * hides the counter-area (including the slide-arrows)
    * @author KM
    */
   hide: function() {
-    jQuery('#scroll-button-area').hide();        
+    jQuery('#scroll-button-area').hide();
   }
 };
 
 var WidgetLikeContent = {
-    
+
   aIsContent: false,
-  
+
   get: function(){
     OnLoadGrafic.showGrafic();
     jQuery.ajax({
@@ -429,7 +429,7 @@ var WidgetLikeContent = {
       type :     "GET",
       url:       '/like/get_like_content',
       dataType : "json",
-      data: { 
+      data: {
         ei_kcuf: new Date().getTime(),
         url: jQuery('#man-url-input').val()
       },
@@ -438,26 +438,26 @@ var WidgetLikeContent = {
           WidgetLikeContent.show(pResponse.html);
           WidgetLikeContent.aIsContent = true;
           LikeImage.init(pResponse.imgcount, pResponse.url);
-          WidgetLikeForm.init();          
+          WidgetLikeForm.init();
         } else {
           if(WidgetLikeContent.aIsContent === false) {
             WidgetLikeContent.showError(pResponse.msg);
           }
-          OnLoadGrafic.hideGrafic();          
+          OnLoadGrafic.hideGrafic();
         }
       }
-    });      
+    });
   },
-    
+
   show: function(pHtml) {
     jQuery('#like-content-box').empty();
     jQuery('#like-content-box').append(pHtml);
     OnLoadGrafic.hideGrafic();
   },
-  
+
   showError: function(pMsg){
     jQuery('#like-content-box').empty();
-    jQuery('#like-content-box').prepend("<div class='error'>"+pMsg+"</div>");      
+    jQuery('#like-content-box').prepend("<div class='error'>"+pMsg+"</div>");
     OnLoadGrafic.hideGrafic();
   }
 };
@@ -465,9 +465,9 @@ var WidgetLikeContent = {
 var WidgetAddService = {
   init:function(){
     WidgetAddService.bindClick();
-    
+
   },
-  
+
   bindClick: function() {
     jQuery('#like-oi-list .add-service-checkbox').live('click', function() {
       OnLoadGrafic.showGrafic();
@@ -475,9 +475,9 @@ var WidgetAddService = {
       WidgetAddService.redirect(lService);
     });
   },
-  
+
   redirect: function(pService){
-    window.location = '/auth/signinto?service='+pService+'&r=s';
+    window.location = '/auth/signinto?service='+pService;
   }
-    
+
 };
