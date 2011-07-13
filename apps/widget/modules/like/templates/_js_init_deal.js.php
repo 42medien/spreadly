da7380eead902b975936e2b1789ea99ffd3efb5e
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-13 09:54:44 +0000

    init signup

--- apps/widget/modules/like/templates/_js_init_deal.js.php
+++ apps/widget/modules/like/templates/_js_init_deal.js.php
@@ -1,3 +1,6 @@
 //jQuery("input.dealcheckbox").custCheckBox();
-jQuery("input[type='checkbox']").custCheckBox();
+//jQuery("input[type='checkbox']").custCheckBox();
 WidgetDealForm.init();
+<?php if(!$sf_user->isAuthenticated()) { ?>
+	WidgetAddService.init();
+<?php } ?>
