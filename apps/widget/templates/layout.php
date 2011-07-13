873d18eb16587c2c7e79c8e0a75708af307752aa
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-13 09:16:19 +0000

    changed to html5

--- apps/widget/templates/layout.php
+++ apps/widget/templates/layout.php
@@ -1,12 +1,11 @@
 <?php use_helper('YiidUrl', 'Avatar', 'Text') ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
   <head>
+    <meta charset="utf-8">
     <title><?php echo __('Spread.ly - first choice for social sharing'); ?></title>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 
 		<link rel="stylesheet" type="text/css" href="/css/popup.css" media="screen" />
 		<!--[if IE]>
