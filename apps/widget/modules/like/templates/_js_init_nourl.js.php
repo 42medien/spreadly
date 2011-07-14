20be466e97ece3162f750bc343f9a90df45f1d56
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-14 11:11:55 +0000

    init auth

--- apps/widget/modules/like/templates/_js_init_nourl.js.php
+++ apps/widget/modules/like/templates/_js_init_nourl.js.php
@@ -1,6 +1,10 @@
 NoUrlForm.init();
-jQuery(".likecheckbox").custCheckBox();
+//jQuery(".likecheckbox").custCheckBox();
 i18n.init({
 	"like_success_message": "<?php echo __("Like successfully sent!");?>",
 	"like_error_message": "<?php echo __("Something went wrong! Check your selected services and try again!");?>"
 });
+
+<?php if(!$sf_user->isAuthenticated()) { ?>
+	WidgetAddService.init();
+<?php } ?>
