242eb38e9e17687a138b648039a2a65d687e93a6
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-20 08:07:57 +0000

    remove comments

--- apps/widget/modules/like/templates/_js_init_nourl.js.php
+++ apps/widget/modules/like/templates/_js_init_nourl.js.php
@@ -1,5 +1,4 @@
 NoUrlForm.init();
-//jQuery(".likecheckbox").custCheckBox();
 i18n.init({
 	"like_success_message": "<?php echo __("Like successfully sent!");?>",
 	"like_error_message": "<?php echo __("Something went wrong! Check your selected services and try again!");?>"
