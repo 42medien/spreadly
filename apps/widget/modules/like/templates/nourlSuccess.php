424bff529b43b31f21e1b1c9f22af3bd6ac61b71
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-07-21 09:27:55 +0000

    abstand

--- apps/widget/modules/like/templates/nourlSuccess.php
+++ apps/widget/modules/like/templates/nourlSuccess.php
@@ -1,8 +1,11 @@
 <?php use_helper('Avatar', 'Text'); ?>
 <form action="<?php echo url_for('@save_like'); ?> " name="popup-like-form" id="popup-like-form" method="post">
-	<input type="text" id="man-url-input" name="manurl" value="<?php echo __('Insert URL you wanna like'); ?>" />
-	<label class="alignright" id="nourl-label"><?php echo __("Please paste the URL you like to spread to your friends."); ?></label>
-	<input type="hidden" name="like[ignore_deal]" id="like-ignore-deal" value="1" />
+	<div id="man-url-input-area" class="clearfix">
+		<input type="text" id="man-url-input" name="manurl" value="<?php echo __('Insert URL you wanna like'); ?>" />
+		<label class="alignright" id="nourl-label"><?php echo __("Please paste the URL you like to spread to your friends."); ?></label>
+		<input type="hidden" name="like[ignore_deal]" id="like-ignore-deal" value="1" />
+	</div>
+
 	<div id="man-url-content">
 	</div>
 </form>
