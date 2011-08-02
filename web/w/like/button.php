4e2654811b17b9221182f2fadcd5c4c782f35f27
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-08-02 11:54:53 +0000

    new button

--- web/w/like/button.php
+++ web/w/like/button.php
@@ -16,95 +16,209 @@ $wu->trackUser();
 <head>
   <meta charset="utf-8">
   <title>spread.ly</title>
-  <!--[if lte IE 6]>
-    <style type="text/css">a { behavior: url(./../js/widget/iepngfix.htc); }</style>
-  <![endif]-->
 
   <style type="text/css">
-  * { margin: 0; padding: 0; }
-  a.button {
-    cursor: pointer;
-    display: block;
-    height: 22px;
-    line-height: 22px;
-    position: relative;
-    overflow: hidden;
-    color: #782a4f;
-    font-size: 10px;
-    text-decoration: none;
-  }
-  a.button span {
-    position: absolute;
-    display: block;
-    height: 22px;
-    background: transparent url('/img/sprites/spreadly_button_sprite.png') no-repeat scroll 0 0;
-    font-family: Tahoma, Verdana, Arial, sans-serif;
-  }
-  a.button span.like {
-    width: 60px;
-    left: 0;
-  }
-  a.button span.count {
-    width: 41px;
-    left: 60px;
-    text-align: center;
-    background-position: 0 -600px;
-  }
-  a.button span.like:hover {
-    background-position: 0 -100px;
-  }
-  a.button.disabled span.like {
-    background-position: 0 -200px;
-  }
-  a.deal span.like {
-    background-position: 0 -300px;
-  }
-  a.deal span.like:hover {
-    background-position: 0 -400px;
-  }
-  a.deal.disabled span.like {
-    background-position: 0 -500px;
-  }
-  div.text {
-    position: absolute;
-    top: 0;
-    font-family: Tahoma, Verdana, Arial, sans-serif;
-	  line-height: 11px;
-	  color: #444;
-    font-size: 10px;
-<?php if ($wu->showCounter()) { ?>
-    left: 110px;
-<?php } else { ?>
-    left: 70px;
-<?php } ?>
-  }
+    *{
+      margin: 0;
+      padding: 0;
+      font-family: Corbel, 'Helvetica Neue', Arial, sans-serif;
+      font-size: 12px;
+      -moz-user-select:none;
+      -webkit-user-select:none;
+    }
+    /* default skin*/
+    .A {
+      display: inline-block;
+      margin: 5px;
+    }
+    .B {
+      display: inline-block;
+      position: relative;
+      color: hsl(333,42%,50%);  /* #B44A7A */
+      line-height: 18px;
+      font-weight: bold;
+      text-rendering: optimizeLegibility;
+      -webkit-font-smoothing: antialiased;
+      padding: 0 4px 0 21px;
+      height: 18px;
+      border: 1px solid hsl(333,42%,73%);
+      border-bottom-color: hsl(333,42%,50%);
+      border-top-color: hsl(333,42%,81%);
+      border-radius: 3px;
+      background: #fff url('./img/button/s.png') no-repeat 130% -20px;
+      text-decoration: none;
+      text-transform: uppercase;
+      box-shadow: 0 -1px 1px 1px hsla(333,42%,100%,.7) inset, 0 -4px 8px 0 hsla(333,42%,50%,.5) inset, 0 0 2px 8px hsla(333,42%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(333,42%,50%,.25);
+      -webkit-transition: box-shadow .3s ease-out, text-shadow .3s ease-out, background-position .6s ease-out;
+      -moz-transition: box-shadow .3s ease-out, text-shadow .3s ease-out, background-position .6s ease-out;
+    }
+    .B:before{
+      background: hsla(333,42%,50%,1) url('./img/button/l.png') no-repeat 0px 0px;
+      box-shadow: 0 1px 1px 0px hsla(333,42%,0%,.8) inset;
+      content:'';
+      width: 16px;
+      height: 16px;
+      display: block;
+      position: absolute;
+      top: 1px;
+      left: 1px;
+      border-radius: 2px;
+      opacity:1;
 
-  ul.icons {
-    list-style-type: none;
-    margin-top: 3px;
-    height: 30px;
-    width: 100px;
-    overflow: hidden;
-  }
-  ul.icons li {
-    float: left;
-    margin-right: 3px;
-  }
-  ul.icons li.last {
-    margin-right: 0px;
-  }
-  ul.icons a img {
-    height: 30px;
-    width: 30px;
-  }
+    }
+    .B:focus, .B:hover{
+      outline: none;
+      background-position: -30% 20px;
+      border: 1px solid hsl(333,42%,65%);
+      border-top-color: hsl(333,42%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(333,42%,50%,.3) inset, 0 0 4px 0 hsla(333,42%,30%,.5);
+      text-shadow: 0 1px 1px hsla(333,42%,50%,.25), 0 -1px 1px #fff;
+      -webkit-transition: box-shadow .3s ease-in, text-shadow .3s ease-in, background-position .6s ease-out;
+      -moz-transition: box-shadow .3s ease-in, text-shadow .3s ease-in, background-position .6s ease-out;
+    }
+
+    .B:active {
+      background-position: -30% 20px;
+      background-color: hsla(333,42%,70%,1);
+      color: #fff;
+      border: 1px solid hsl(333,42%,30%);
+      border-top-color: hsl(333,42%,30%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(333,42%,50%,.3) inset, 0 0 4px 0 hsla(333,42%,30%,.5);
+      text-shadow: 0 0px 0px hsla(333,42%,50%,0), 0 0px 0px #fff;
+    }
+
+    .B:focus:before, .B:hover:before{
+      opacity:.85;
+      width: 14px;
+      height: 14px;
+      top:2px;
+      left:2px;
+      background-position: -1px -1px;*/
+    }
+
+
+    /* skin .ca*/
+
+    .ca .B {
+      color: hsl(199,81%,50%);
+      border-color: hsl(199,81%,73%);
+      border-bottom-color: hsl(199,81%,50%);
+      border-top-color: hsl(199,81%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(199,81%,100%,.7) inset, 0 -4px 8px 0 hsla(199,81%,50%,.5) inset, 0 0 2px 8px hsla(199,81%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(199,81%,50%,.25);
+    }
+    .ca .B:before{
+      background-color: hsla(199,81%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(199,81%,0%,.5) inset;
+    }
+    .ca .B:focus, .ca .B:hover{
+      border-color: hsl(199,81%,65%);
+      border-top-color: hsl(199,81%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(199,81%,50%,.3) inset, 0 0 4px 0 hsla(199,81%,30%,.5);
+      text-shadow: 0 1px 1px hsla(199,81%,50%,.25), 0 -1px 1px #fff;
+    }
+    /* skin .cb*/
+    .cb .B {
+      color: hsl(111,24%,50%);
+      border-color: hsl(111,24%,73%);
+      border-bottom-color: hsl(111,24%,50%);
+      border-top-color: hsl(111,24%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(111,24%,100%,.7) inset, 0 -4px 8px 0 hsla(111,24%,50%,.5) inset, 0 0 2px 8px hsla(111,24%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(111,24%,50%,.25);
+    }
+    .cb .B:before{
+      background-color: hsla(111,24%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(111,24%,0%,.5) inset;
+    }
+    .cb .B:focus, .cb .B:hover{
+      border-color: hsl(111,24%,65%);
+      border-top-color: hsl(111,24%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(111,24%,50%,.3) inset, 0 0 4px 0 hsla(111,24%,30%,.5);
+      text-shadow: 0 1px 1px hsla(111,24%,50%,.25), 0 -1px 1px #fff;
+    }
+    /* skin .cc*/
+    .cc .B {
+      color: hsl(302,54%,50%);
+      border-color: hsl(302,54%,73%);
+      border-bottom-color: hsl(302,54%,50%);
+      border-top-color: hsl(302,54%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(302,54%,100%,.7) inset, 0 -4px 8px 0 hsla(302,54%,50%,.5) inset, 0 0 2px 8px hsla(302,54%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(302,54%,50%,.25);
+    }
+    .cc .B:before{
+      background-color: hsla(302,54%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(302,54%,0%,.5) inset;
+    }
+    .cc .B:focus, .cc .B:hover{
+      border-color: hsl(302,54%,65%);
+      border-top-color: hsl(302,54%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(302,54%,50%,.3) inset, 0 0 4px 0 hsla(302,54%,30%,.5);
+      text-shadow: 0 1px 1px hsla(302,54%,50%,.25), 0 -1px 1px #fff;
+    }
+    /* skin .cd*/
+    .cd .B {
+      color: hsl(22,90%,50%);
+      border-color: hsl(22,90%,73%);
+      border-bottom-color: hsl(22,90%,50%);
+      border-top-color: hsl(22,90%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(22,90%,100%,.7) inset, 0 -4px 8px 0 hsla(22,90%,50%,.5) inset, 0 0 2px 8px hsla(22,90%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(22,90%,50%,.25);
+    }
+    .cd .B:before{
+      background-color: hsla(22,90%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(22,90%,0%,.5) inset;
+    }
+    .cd .B:focus, .cd .B:hover{
+      border-color: hsl(22,90%,65%);
+      border-top-color: hsl(22,90%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(22,90%,50%,.3) inset, 0 0 4px 0 hsla(22,90%,30%,.5);
+      text-shadow: 0 1px 1px hsla(22,90%,50%,.25), 0 -1px 1px #fff;
+    }
+    /* skin .ce*/
+    .ce .B {
+      color: hsl(349,79%,50%);
+      border-color: hsl(349,79%,73%);
+      border-bottom-color: hsl(349,79%,50%);
+      border-top-color: hsl(349,79%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(349,79%,100%,.7) inset, 0 -4px 8px 0 hsla(349,79%,50%,.5) inset, 0 0 2px 8px hsla(349,79%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(349,79%,50%,.25);
+    }
+    .ce .B:before{
+      background-color: hsla(349,79%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(349,79%,0%,.5) inset;
+    }
+    .ce .B:focus, .ce .B:hover{
+      border-color: hsl(349,79%,65%);
+      border-top-color: hsl(349,79%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(349,79%,50%,.3) inset, 0 0 4px 0 hsla(349,79%,30%,.5);
+      text-shadow: 0 1px 1px hsla(349,79%,50%,.25), 0 -1px 1px #fff;
+    }
+    /* skin .cf*/
+    .cf .B {
+      color: hsl(0,0%,50%);
+      border-color: hsl(0,0%,73%);
+      border-bottom-color: hsl(0,0%,50%);
+      border-top-color: hsl(0,0%,81%);
+      box-shadow: 0 -1px 1px 1px hsla(0,0%,100%,.7) inset, 0 -4px 8px 0 hsla(0,0%,50%,.5) inset, 0 0 2px 8px hsla(0,0%,0%,0);
+      text-shadow: 0 1px 1px #fff, 0 -1px 1px hsla(0,0%,50%,.25);
+    }
+    .cf .B:before{
+      background-color: hsla(0,0%,50%,1);
+      box-shadow: 0 1px 1px 0px hsla(0,0%,0%,.5) inset;
+    }
+    .cf .B:focus, .cf .B:hover{
+      border-color: hsl(0,0%,65%);
+      border-top-color: hsl(0,0%,46%);
+      box-shadow: 0 0 0 1px #fff inset, 0 6px 12px 0 hsla(0,0%,50%,.3) inset, 0 0 4px 0 hsla(0,0%,30%,.5);
+      text-shadow: 0 1px 1px hsla(0,0%,50%,.25), 0 -1px 1px #fff;
+    }
   </style>
 </head>
 <body>
-  <a href="<?php echo $wu->getPopupUrl() ?>" rel="like" onclick="window.open(this.href, 'popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;" target="_blank" class="button <?php echo $wu->getButtonClass(); ?>"><span class="like">&nbsp;</span><?php if ($wu->showCounter()) { ?><span class="count"><?php echo $wu->getActivityCount() ?></span><?php } ?></a>
-  <?php if ($wu->getDeal()): ?>
-  <?php $lActiveDeal = $wu->getDeal(); ?>
-    <div class="text"><?php echo $lActiveDeal['button_wording']; ?></div>
-  <?php endif; ?>
+  <div class="A">
+    <a class="B" href="<?php echo $wu->getPopupUrl() ?>" onclick="window.open(this.href, 'popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;" target="_blank">Like</a>
+  </div>
   <?php if ($wu->showFriends()): ?>
  	<script type="text/javascript" src="/js/100_main/include/button-<?php echo LikeSettings::RELEASE_NAME; ?>.js"></script>
   <script type="text/javascript">
