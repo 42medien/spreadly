8a316c1bd04bc358f80bd9a6df078d5e067c0897
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-08-04 09:57:01 +0000

    new layout

--- apps/widget/templates/layout.php
+++ apps/widget/templates/layout.php
@@ -1,80 +1,53 @@
-<?php use_helper('YiidUrl', 'Avatar', 'Text') ?>
+<?php use_helper('Text'); ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title><?php echo __('Spread.ly - first choice for social sharing'); ?></title>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
+    <title>Spread.ly - first choice for social sharing</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Spread.ly consists of sharing button widgets that enable users to share content into a wide range of social networks simultanously. Besides that, Spread.ly offers a sophisticated functionality to reward users for their likes." />
+    <meta name="keywords" content="sharing,sharebutton,like,likebutton,deal,dealbutton,facebook,linkedin,twitter,buzz" />
+    <link rel="stylesheet" type="text/css" href="/css/widget/popup.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/css/widget/Button.css" media="screen" />
 
-		<link rel="stylesheet" type="text/css" href="/css/popup.css" media="screen" />
-
-		<!--[if IE 6]>
-		<script>
-		  DD_belatedPNG.fix('*');
-		</script>
-		<![endif]-->
-		<!--[if lt IE 9]>
-			<link rel="stylesheet" type="text/css" href="/css/popup-ie.css" />
+    <!--[if lt IE 9]>
+      <link rel="stylesheet" type="text/css" href="/css/popup-ie.css" />
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-		<?php if ($sf_params->get('resize') == true) { ?>
-		<script>
-      window.resizeTo(580,600);
-    </script>
-    <?php } ?>
+
     <script type="text/javascript">var _sf_startpt=(new Date()).getTime();</script>
     <script type="text/javascript" src="/js/100_main/include/widget-<?php echo sfConfig::get('app_release_name') ?>.js"></script>
 
 
     <link rel="shortcut icon" href="/favicon.ico" />
   </head>
-  <body class="nobg">
-  	<div class="popupblock">
-  		<header>
-				<nav class="clearfix" id="main-nav">
-		      <!--Pop Navigation start -->
-		      <?php if ($sf_user->isAuthenticated() ) { ?>
-		          <ul class="nav-list alignright" role="navigation">
-		            <li><span><?php echo __('Hi'); ?> <?php echo truncate_text($sf_user->getUser()->getUsername(), 10); ?></span></li>
-		            <li><?php echo link_to(__('Logout'), '@signout'); ?></li>
-		          </ul>
-		          <ul class="nav-list" role="navigation">
-		            <li <?php if ($sf_context->getModuleName()=='likes') { echo 'class="active"'; } ?>><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
-		            <li id="nav-next-to-last" <?php if ($sf_context->getModuleName()=='deals') { echo 'class="active"'; } ?>><?php echo link_to(__('Deals'), '@widget_deals'); ?></li>
-		          </ul>
-		      <?php } else { ?>
-			       	<ul class="nav-list alignright" role="navigation">
-			          <li><?php echo link_to(__('Login'), '@signinto'); ?></li>
-		          </ul>
-		          <ul class="nav-list" role="navigation">
-		            <li <?php if ($sf_context->getModuleName()=='likes') { echo 'class="active"'; } ?>><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
-		            <li id="nav-next-to-last" <?php if ($sf_context->getModuleName()=='deals') { echo 'class="active"'; } ?>><?php echo link_to(__('Deals'), '@widget_deals'); ?></li>
-		            <li id="nav-spread" class="<?php if ($sf_context->getModuleName()=='like') { echo ''; } ?>"><?php echo link_to(__('Like it!'), '@widget_like'); ?></li>
-		          </ul>
-		      <?php } ?>
-		      <!-- Pop Navigation end -->
-				</nav>
-			</header>
-			<div id="content-outer" role="main">
-				<header>
-					<?php if (has_slot('headline')) { ?>
-						<?php include_slot('headline'); ?>
-					<?php } ?>
-				</header>
-				<div id="content-inner" class="clearfix">
-					<?php echo $sf_content; ?>
-				</div>
-			</div>
+  <body>
+    <div class="popupblock">
 
-			<footer class="clearfix">
-				<ul>
-					<li><?php echo link_to(__("Imprint"), "http://spreadly.com/imprint", array("target" => "_blank")); ?></li>
-					<li><?php echo link_to("Powered by Spreadly", sfConfig::get("app_settings_url"), array("title" => "spread.ly", "target" => "_blank")) ?></li>
-				</ul>
-			</footer>
-		</div>
-    <!--popup block end -->
+      <!-- transparent-grauer Hintergrund mit Überschrift -->
+      <div id="content-outer" role="main">
+        <?php echo $sf_content; ?>
+
+        <!-- popup footer -->
+
+        <footer>
+          <!-- Hauptnavigation -->
+          <nav>
+            <!-- Navi rechte Seite -->
+            <ul class="nav-list" role="navigation">
+              <?php if ($sf_user->isAuthenticated()) { ?>
+              <li><span title="<?php echo $sf_user->getUser()->getUsername(); ?>"><?php echo __('Hi'); ?> <?php echo truncate_text($sf_user->getUser()->getUsername(), 10); ?></span></li>
+              <li><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
+              <li><?php echo link_to(__('Deals'), '@widget_deals'); ?></li>
+              <li><?php echo link_to(__('Logout'), '@signout'); ?></li>
+              <?php } ?>
+              <li><?php echo link_to(__("Imprint"), "http://spreadly.com/imprint", array("target" => "_blank")); ?></li>
+              <li><?php echo link_to("Powered by Spreadly", sfConfig::get("app_settings_url"), array("title" => "spread.ly", "target" => "_blank")) ?></li>
+            </ul>
+          </nav>
+        </footer>
+      </div>
+    </div>
+
 		<img id="general-ajax-loader" style="display:none;" src="/img/global/ajax-loader-bar-circle.gif" />
 	  <script  type="text/javascript">
 	    jQuery(document).ready( function() {
