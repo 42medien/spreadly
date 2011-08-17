3c624f76bdd8c6213aa0bf7b6d9698f72bdef4f3
hannes <hannes@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-08-17 09:34:41 +0000

    Added mock

---
+++ web/mock/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+  <style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  
+  #background {
+    position: relative;
+  }
+  #button {
+    position: absolute;
+  }
+
+  body {
+    overflow-x: hidden;
+  }
+  
+  #background {
+    background: #fff url('./screen.png') no-repeat scroll top left;
+    width: 1478px;
+    height: 986px;
+  }
+  #background #button {
+    top: 475px;
+    left: 252px;
+  }  
+  
+  </style>
+  </head>
+  <body>
+    <div id="background">
+      <iframe id="button" scrolling="no" frameborder="0" allowtransparency="true" src="http://button.yiiddev.com/?url=http://social-media.com/?cat=1" style="overflow: hidden; width: 420px; height: 62px;" marginheight="0" marginwidth="0">
+      </iframe>
+    </div>
+  </body>
+</html>
+
