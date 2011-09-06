79b4f0da230f352612419b173985da0b3db1f58a
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-06 08:36:52 +0000

    removed pages

--- apps/widget/templates/layout.php
+++ apps/widget/templates/layout.php
@@ -42,8 +42,8 @@
             <ul class="nav-list" role="navigation">
               <?php if ($sf_user->isAuthenticated()) { ?>
               <li><span title="<?php echo $sf_user->getUser()->getUsername(); ?>"><?php echo __('Hi'); ?> <?php echo truncate_text($sf_user->getUser()->getUsername(), 10); ?></span></li>
-              <li><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
-              <li><?php echo link_to(__('Deals'), '@widget_deals'); ?></li>
+              <!-- li><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
+              <li><?php echo link_to(__('Deals'), '@widget_deals'); ?></li-->
               <li><?php echo link_to(__('Logout'), '@signout'); ?></li>
               <?php } ?>
               <li><?php echo link_to(__("Imprint"), "http://spreadly.com/imprint", array("target" => "_blank")); ?></li>
