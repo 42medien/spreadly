c1ec31f498b2a9022073d7128b045a9431e132e2
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-07 15:06:49 +0000

    disch!

--- apps/statistics/modules/domain_profiles/templates/indexSuccess.php
+++ apps/statistics/modules/domain_profiles/templates/indexSuccess.php
@@ -3,4 +3,7 @@
   <?php include_partial('domain_profiles/form', array('form'=> $form)); ?>
 </div>
 
+<?php //slot('content') ?>
 <?php include_partial('domain_profiles/domain_profiles_table', array('domain_profiles' => $domain_profiles)); ?>
+<?php //end_slot(); ?>
+<?php //include_partial('global/graybox'); ?>
