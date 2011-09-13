aa0525345fe11006001c33a9cf9a513dccb9121e
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-13 14:46:59 +0000

    load non-json friends

--- apps/widget/modules/api/actions/actions.class.php
+++ apps/widget/modules/api/actions/actions.class.php
@@ -8,23 +8,25 @@
  * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
  */
 class apiActions extends sfActions {
-  public function executeLoad_friends(sfWebRequest $request) {
-    $this->getResponse()->setContentType('application/json');
 
+	public function executeLoad_friends(sfWebRequest $request) {
+    $this->getResponse()->setContentType('text/html');
+    $this->setLayout(false);
     $lUserId = $this->getUser()->getUserId();
     $lSocialObjectId = $request->getParameter('so_id');
 
     $lReturn['success'] = false;
-
+		$lReturn['html'] = false;
+		$this->pFriends = array();
     if($lUserId && $lSocialObjectId) {
-    	$lFriends = UserTable::getFriendIdsBySocialObjectId($lSocialObjectId, $lUserId);
+    	$this->pFriends = array_slice(UserTable::getFriendIdsBySocialObjectId($lSocialObjectId, $lUserId), 0, 3);
 
-    	if($lFriends) {
+    	/*if($lFriends) {
     		$lReturn['success'] = true;
     		$lReturn['html'] =  $this->getPartial('profile/social_object_friends', array('pFriends' => array_slice($lFriends, 0, 3)));
-    	}
+    	}*/
 
     }
-    return $this->renderText(json_encode($lReturn));
+   	return $lReturn['html'];
   }
 }
