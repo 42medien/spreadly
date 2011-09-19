d7c8e6789a046223d6b4a1f4810ad76b8aeba2c6
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-19 10:03:12 +0000

    new wildcards

--- apps/statistics/modules/landing/templates/indexSuccess.php
+++ apps/statistics/modules/landing/templates/indexSuccess.php
@@ -5,8 +5,8 @@
 					<div class="panel_width">
 						<div class="graybox buttonbox">
 							<h2><?php echo __("Get your button NOW!"); ?></h2>
-							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, GoogleBuzz, LinkedIn."); ?><br />
-							<?php echo __("Like to thank for recommendations? Just create your deal!"); ?></h3>
+							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, Google, LinkedIn."); ?><br />
+							<?php echo __("Advertiser, publisher and user profit by new Adnetwork!"); ?></h3>
 							<div class="bannerimg"><?php echo image_tag("/img/button-banner-".$sf_user->getCulture().".png", array("class"=>"banner")); ?><a href="<?php echo url_for("@configurator"); ?>" class="goimg" title="Get Your Own Button Now"><img src="/img/get-yourown-img-<?php echo $sf_user->getCulture() ?>.png" alt="Get Your Own Button Now" title="Get Your Own Button Now"/></a></div>
 						</div>
 					</div>
@@ -21,8 +21,8 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><span class="bluetxt"><?php echo __("Share"); ?></span><span><?php echo __("Benefit from huge scope"); ?></span></h3>
-							<p><?php echo __("Users of your site comment on your contents and products. They share their feedback with a click on the Spread.ly-button on different social networks. Their contacts can react by clicking and commenting."); ?></p>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Button"); ?></span><span><?php echo __("Benefit from four times greater range"); ?></span></h3>
+							<p><?php echo __("Users comment on contents and products and share their feedback with a single click on the Spreadly-button on different social networks. Their contacts can react by commenting. Every commercial recommendation also profits from the great reach."); ?></p>
 						</div>
 					</div>
 				</div>
@@ -33,8 +33,8 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><span class="bluetxt"><?php echo __("Deal"); ?></span><span><?php echo __("Recommendation marketing"); ?></span></h3>
-							<p><?php echo __("Create your deal for a defined period of time. Thank your visitors and customers for their recommendations with individual vouchers. Deals ensure additional recommendations easily."); ?></p>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Publisher"); ?></span><span><?php echo __("Benefit for site operators"); ?></span></h3>
+							<p><?php echo __("Site operators receive analytic data to gain knowledge about the exact effect of their shared contents. They gain money because the advertisement appears in the Like-Popup window after sharing a content of the web site. The ad is always associated with vouchers for recommendations. Users get the vouchers directly."); ?></p>
 						</div>
 					</div>
 				</div>
@@ -45,8 +45,8 @@
 				<div class="grboxmid">
 					<div class="grboxmid-content">
 						<div class="graybox service_content">
-							<h3 class="header"><span class="bluetxt"><?php echo __("Analytics"); ?></span><span><?php echo __("Get to know your fans"); ?></span></h3>
-							<p><?php echo __("Spread.ly offers an analytic tool that measures effectiveness of recommendations. You get demographics analyzed in anonymized form and come to know the value of each recommendation."); ?></p>
+							<h3 class="header"><span class="bluetxt"><?php echo __("Advertiser"); ?></span><span><?php echo __("Benefits for advertisers"); ?></span></h3>
+							<p><?php echo __("Spreadly offers a new advertising format. Advertisers pay and book an exact range. The linking of commercial message and voucher as thanks for the recommendation makes the ad attractive to the user. They are well aware of the ad message because it appears right after the focused like."); ?></p>
 						</div>
 					</div>
 				</div>
