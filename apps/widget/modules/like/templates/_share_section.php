d74706477dc0227342d64cbe7f5df69f7e10d97b
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-22 09:28:10 +0000

    ajax-loader bei anmeldung

--- apps/widget/modules/like/templates/_share_section.php
+++ apps/widget/modules/like/templates/_share_section.php
@@ -27,19 +27,19 @@
         </ul>
         <?php } else { ?>
         <ul class="clearfix" id="like-oi-list">
-          <li class="B" id="o1" onclick="window.open('<?php echo url_for("@signinto?service=twitter"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
+          <li class="B" id="o1" onclick="WidgetLikeForm.beforeSend(); window.open('<?php echo url_for("@signinto?service=twitter"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
             <input class="add-service-checkbox" type="checkbox" name="twitter" value="twitter" />
             <label for="o1"><?php echo link_to(image_tag("/img/twitter-favicon.gif", array("alt" => 'Twitter', "title" => 'Twitter')), "@signinto?service=twitter"); ?></label>
           </li>
-          <li class="B" id="o2" onclick="window.open('<?php echo url_for("@signinto?service=facebook"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
+          <li class="B" id="o2" onclick="WidgetLikeForm.beforeSend();window.open('<?php echo url_for("@signinto?service=facebook"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
             <input class="add-service-checkbox" type="checkbox" name="facebook" value="facebook" />
             <label for="o2"><?php echo link_to(image_tag("/img/facebook-favicon.gif", array("alt" => 'facebook', "title" => 'facebook')), "@signinto?service=facebook"); ?></label>
           </li>
-          <li class="B" id="o3" onclick="window.open('<?php echo url_for("@signinto?service=linkedin"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
+          <li class="B" id="o3" onclick="WidgetLikeForm.beforeSend();window.open('<?php echo url_for("@signinto?service=linkedin"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
             <input class="add-service-checkbox" type="checkbox" name="linkedin" value="linkedin" />
             <label for="o3"><?php echo link_to(image_tag("/img/linkedin-favicon.gif", array("alt" => 'Linkedin', "title" => 'Linkedin')), "@signinto?service=linkedin"); ?></label>
           </li>
-          <li class="B" id="o4" onclick="window.open('<?php echo url_for("@signinto?service=google"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
+          <li class="B" id="o4" onclick="WidgetLikeForm.beforeSend();window.open('<?php echo url_for("@signinto?service=google"); ?>', 'auth_popup', 'width=580,height=450,scrollbars=no,toolbar=no,status=no,resizable=no,menubar=no,location=0,directories=no,top=150,left=150'); return false;">
             <input class="add-service-checkbox" type="checkbox" name="google" value="google" />
             <label for="o4"><?php echo link_to(image_tag("/img/google-favicon.gif", array("alt" => 'google', "title" => 'google')), "@signinto?service=google&r=s"); ?></label>
           </li>
