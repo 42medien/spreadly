cd71e0dc6ae9d156d54f0cdab1445a22686e7a6d
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-09-28 09:13:10 +0000

    added api app

---
+++ apps/api/modules/deals/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * deals actions.
+ *
+ * @package    yiid
+ * @subpackage deals
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class dealsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
