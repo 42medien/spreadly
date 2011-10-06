46eb37317f460b01bf42a22c4704ce81a7b25a6a
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-10-06 08:32:28 +0000

    some fixes

--- data/migration/20110930.sql
+++ data/migration/20110930.sql
@@ -1,4 +1,7 @@
-ALTER TABLE sf_guard_user ADD access_token VARCHAR(255), api_price_like FLOAT, api_price_media_penetration FLOAT;
+ALTER TABLE sf_guard_user ADD access_token VARCHAR(255);
+ALTER TABLE sf_guard_user ADD api_price_like FLOAT;
+ALTER TABLE sf_guard_user ADD api_price_media_penetration FLOAT;
+
 ALTER TABLE deal ADD coupon_webhook_url VARCHAR(255);
 ALTER TABLE payment_method ADD api_method TINYINT(1);
 
