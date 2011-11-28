596a684437ecc0e72b2b6e88d26413f55ff01b39
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-11-28 10:57:01 +0000

    removed google

--- apps/statistics/modules/landing/templates/indexSuccess.php
+++ apps/statistics/modules/landing/templates/indexSuccess.php
@@ -5,7 +5,7 @@
 					<div class="panel_width">
 						<div class="graybox buttonbox">
 							<h2><?php echo __("Get your button NOW!"); ?></h2>
-							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, Google, LinkedIn."); ?><br />
+							<h3><?php echo __("Three easy steps to get your content shared on all important social networks: Facebook, Twitter, LinkedIn."); ?><br />
 							<?php echo __("Advertiser, publisher and user profit by new Adnetwork!"); ?></h3>
 							<div class="bannerimg"><?php echo image_tag("/img/button-banner-".$sf_user->getCulture().".png", array("class"=>"banner")); ?><a href="<?php echo url_for("@configurator"); ?>" class="goimg" title="Get Your Own Button Now"><img src="/img/get-yourown-img-<?php echo $sf_user->getCulture() ?>.png" alt="Get Your Own Button Now" title="Get Your Own Button Now"/></a></div>
 						</div>
