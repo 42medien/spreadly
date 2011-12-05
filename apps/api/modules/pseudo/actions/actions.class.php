18d095589251c1b37727037ef3071b2dc4a4ce09
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-12-05 14:19:57 +0000

    added pseudo api

---
+++ apps/api/modules/pseudo/actions/actions.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * pseudo actions.
+ *
+ * @package    yiid
+ * @subpackage pseudo
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pseudoActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeCustom_code(sfWebRequest $request) {
+    $this->getResponse()->setContentType('application/json');
+    $this->setLayout(false);
+
+    $identifier = trim($request->getParameter("for", null));
+
+    // check identifier
+    if (!$identifier) {
+      $this->getResponse()->setStatusCode(405);
+      return $this->renderPartial("error");
+    }
+
+    $identifier = "custom_code_".$identifier;
+    $obj = PersistentVariableTable::getInstance()->findOneByName($identifier);
+
+    // check if there are some codes in the db
+    if (!$obj) {
+      $this->getResponse()->setStatusCode(405);
+      return $this->renderPartial("error");
+    }
+
+    $this->code = $obj->getValue();
+    $obj->delete();
+  }
+}
---
+++ apps/api/modules/pseudo/templates/_error.php
@@ -0,0 +1,1 @@
+<?php echo json_encode(array("error" => array("code" => 405, "message" => "no codes for this request params"))); ?>
