207576265a7e40174aa156116a9f7f17dbd6ef6c
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-12-14 11:32:40 +0000

    new layout

--- apps/statistics/modules/advertiser/templates/indexSuccess.php
+++ apps/statistics/modules/advertiser/templates/indexSuccess.php
@@ -1,46 +1,34 @@
-<?php slot('content') ?>
-<h2><?php echo __('Herzlich Willkommen'); ?></h2>
-<div><?php echo __('Schalten Sie Ihre Kampagne im Spreadly Ad-Network. Nach nur 5 einfachen Schritten erscheint Ihre Kampagne auf über 1.000 Seiten im Spreadly Ad-Network. Sie werden mit Ihrem speziellen Angebot das "kleine Dankeschön" für TOP-Empfehlungsgeber und Influencer.<br/><br/>Entscheiden Sie selbst ob Ihnen eine feste Anzahl von LIKES wichtiger ist, oder Sie eine große Reichweite und Sichtbarkeit für Ihre Kampagne vorziehen.<br/><br/>Das Spreadly Ad-Network macht den Einstieg in die Werbewelt zum Kinderspiel.<br/><br/>Unser faires und transparentes Abrechnungsmodell wird auch Sie überzeugen.<br/><br/>Legen Sie einfach los!'); ?></div>
-<?php end_slot(); ?>
-<?php include_partial('global/graybox'); ?>
-<div class="clearfix services_list">
-	<div class="alignleft item">
-		<div class="grboxtop"><span></span></div>
-		<div class="grboxmid">
-			<div class="grboxmid-content">
-				<div class="graybox service_content advertiser_content">
-					<h3 class="header"><a href="<?php echo url_for('@deals')?>" class="bluetxt"><?php echo __("Start campaign"); ?></a><span><?php echo __("Benefit from four times greater range"); ?></span></h3>
-					<p><?php echo __("Users comment on contents and products and share their feedback with a single click on the Spreadly-button on different social networks. Their contacts can react by commenting. Every commercial recommendation also profits from the great reach."); ?></p>
-					<p><?php echo link_to(__('Create a deal and start campaign'), '@deals'); ?></p>
+			<section class="content-publish">
+			<section class="container_12 submenue">
+				<div class="page-titlecontent">
+					<h2><?php echo __('Herzlich Willkommen'); ?></h2>
+					<p><?php echo __('Schalten Sie Ihre Kampagne im Spreadly Ad-Network. Nach nur 5 einfachen Schritten erscheint Ihre Kampagne auf über 1.000 Seiten im Spreadly Ad-Network. Sie werden mit Ihrem speziellen Angebot das "kleine Dankeschön" für TOP-Empfehlungsgeber und Influencer. Entscheiden Sie selbst ob Ihnen eine feste Anzahl von LIKES wichtiger ist, oder Sie eine große Reichweite und Sichtbarkeit für Ihre Kampagne vorziehen.Das Spreadly Ad-Network macht den Einstieg in die Werbewelt zum Kinderspiel.Unser faires und transparentes Abrechnungsmodell wird auch Sie überzeugen.Legen Sie einfach los'); ?></p>
 				</div>
-			</div>
-		</div>
-		<div class="grboxbot"><span></span></div>
-	</div>
-	<div class="alignleft item">
-		<div class="grboxtop"><span></span></div>
-		<div class="grboxmid">
-			<div class="grboxmid-content">
-				<div class="graybox service_content advertiser_content">
-					<h3 class="header"><a href="<?php echo url_for('@dealapi')?>" class="bluetxt"><?php echo __("Use the deal API"); ?></a><span><?php echo __("Benefit for site operators"); ?></span></h3>
-					<p><?php echo __("Site operators receive analytic data to gain knowledge about the exact effect of their shared contents. They gain money because the advertisement appears in the Like-Popup window after sharing a content of the web site. The ad is always associated with vouchers for recommendations. Users get the vouchers directly."); ?></p>
-					<p><?php echo link_to(__('Start to use our API'), '@dealapi'); ?></p>
-				</div>
-			</div>
-		</div>
-		<div class="grboxbot"><span></span></div>
-	</div>
-	<div class="alignright itemlast">
-		<div class="grboxtop"><span></span></div>
-		<div class="grboxmid">
-			<div class="grboxmid-content">
-				<div class="graybox service_content advertiser_content">
-					<h3 class="header"><a href="<?php echo url_for('@deal_analytics_index')?>" class="bluetxt"><?php echo __("Get Analytics"); ?></a><span><?php echo __("Benefits for advertisers"); ?></span></h3>
-					<p><?php echo __("Spreadly offers a new advertising format. Advertisers pay and book an exact range. The linking of commercial message and voucher as thanks for the recommendation makes the ad attractive to the user. They are well aware of the ad message because it appears right after the focused like."); ?></p>
-					<p><?php echo link_to(__('Watch your detailled campaign analytics'), '@deal_analytics_index'); ?></p>
-				</div>
-			</div>
-		</div>
-		<div class="grboxbot"><span></span></div>
-	</div>
-</div>
+				<section class="publisher-list">
+					<ul class="clearfix">
+						<li>
+							<span class="icon"><img src="/img/statistics/get-btn-icon.png" alt="" title=""></span>
+							<h3><?php echo __('Das Spreadly Ad-Network'); ?></h3>
+							<h4><?php echo __('Vorteil vielfacher Reichweite'); ?></h4>
+							<p><?php echo __('Besucher teilen ihre Kommentare zu Inhalten und Produkten per Klick auf den Spreadly-Button in verschiedene Social Media-Netzwerke. Kontakte der Besucher können mit Kommentaren und Klicks reagieren. Werbende Empfehlungen haben ebenfalls die große Reichweite.'); ?></p>
+							<span class="link"><a href="<?php echo url_for('deals/campaign'); ?>"><?php echo __('Neue Kampagne starten &raquo;'); ?></a></span>
+						</li>
+						<li>
+							<span class="icon"><img src="/img/statistics/domain-icon.png" alt="" title=""></span>
+							<h3><?php echo __('Spreadly Deal API'); ?></h3>
+							<h4><?php echo __('Vorteile für Seitenbetreiber'); ?></h4>
+							<p><?php echo __('Wie bieten zahlreiche APIs für Deinen speziellen Fall. Ob der Import vieler Kampagnen oder das Auslösen bestimmter Aktionen bei jedem Klick, für alle Bedürfnisse haben die passende Lösung'); ?></p>
+							<span class="link"><a href="<?php echo url_for('advertiser/deal_api'); ?>"><?php echo __('Start to use our API &raquo;'); ?></a></span>
+						</li>
+						<li class="last">
+							<span class="icon"><img src="/img/statistics/get-analytics.png" alt="" title=""></span>
+							<h3><?php echo __('Social Media Analytics'); ?></h3>
+							<h4><?php echo __('Vorteile für Werbetreibende'); ?></h4>
+							<p><?php echo __('Mit Spreadly erfährst Du alles was in Sachen Empfehlungsmarketing wichtig ist. Wir zeigen Die wie, ob und dank wem Dein Empfehlungsmarketing funktioniert.'); ?></p>
+							<span class="link"><a href="<?php echo url_for('@deal_analytics_index'); ?>"><?php echo __('Watch your detailled deal analytics &raquo;'); ?></a></span>
+						</li>
+					</ul>
+				</section>
+				<?php include_partial('global/spreadly_references');?>
+			</section>
+		</section>
--- apps/statistics/modules/publisher/templates/indexSuccess.php
+++ apps/statistics/modules/publisher/templates/indexSuccess.php
@@ -1,32 +1,31 @@
 			<section class="content-publish">
 			<section class="container_12 submenue">
 				<div class="page-titlecontent">
-					<h2>Herzlich Willkommen im Publisher-Bereich</h2>
-					<p>Holen Sie sich jetzt den Spreadly Button f&uuml;r ihre Seite.<br>Registrieren Sie ihre Webseite bei Spreadly und erhalten sie damit Analyse-Daten &uuml;ber Shares, Likes und Media Penetration ihrer User.<br>
-Verdienen Sie nach der Registrierung &uuml;ber unser Adnetwork bares Geld, indem Sie an jedem geklickten Deal beteiligt werden.</p>
+					<h2><?php echo __('Herzlich Willkommen im Publisher-Bereich'); ?></h2>
+					<p><?php echo __('Holen Sie sich jetzt den Spreadly Button für ihre Seite.Registrieren Sie ihre Webseite bei Spreadly und erhalten sie damit Analyse-Daten über Shares, Likes und Media Penetration ihrer User. Verdienen Sie nach der Registrierung über unser Adnetwork bares Geld, indem Sie an jedem geklickten Deal beteiligt werden.'); ?></p>
 				</div>
 				<section class="publisher-list">
 					<ul class="clearfix">
 						<li>
 							<span class="icon"><img src="/img/statistics/get-btn-icon.png" alt="" title=""></span>
-							<h3>Get Button</h3>
-							<h4>Benefit from four times greater range</h4>
-							<p>Users comment on contents and products and share their feedback with a single click on the Spreadly-button on different social networks. Their contacts can react by commenting. Every commercial recommendation also profits from the great reach.</p>
-							<span class="link"><a href="#">Generate your button code &raquo;</a></span>
+							<h3><?php echo __('Get Button'); ?></h3>
+							<h4><?php echo __('Vorteil vielfacher Reichweite'); ?></h4>
+							<p><?php echo __('Besucher teilen ihre Kommentare zu Inhalten und Produkten per Klick auf den Spreadly-Button in verschiedene Social Media-Netzwerke. Kontakte der Besucher können mit Kommentaren und Klicks reagieren. Werbende Empfehlungen haben ebenfalls die große Reichweite.'); ?></p>
+							<span class="link"><a href="<?php echo url_for('@configurator'); ?>"><?php echo __('Generate your button code &raquo;'); ?></a></span>
 						</li>
 						<li>
 							<span class="icon"><img src="/img/statistics/domain-icon.png" alt="" title=""></span>
-							<h3>Register your domain</h3>
-							<h4>Earn money and use other exclusive </h4>
-							<p>Site operators receive analytic data to gain knowledge about the exact effect of their shared contents. They gain money because the advertisement appears in the Like-Popup window after sharing a content of the web site. Register your domain to get the analytics and earn money! </p>
-							<span class="link"><a href="<?php echo url_for('domain_profiles/index'); ?>">Start the domain registration process &raquo;</a></span>
+							<h3><?php echo __('Domain registrieren'); ?></h3>
+							<h4><?php echo __('Earn money and use other exclusive'); ?></h4>
+							<p><?php echo __('Site operators receive analytic data to gain knowledge about the exact effect of their shared contents. They gain money because the advertisement appears in the Like-Popup window after sharing a content of the web site. Register your domain to get the analytics and earn money!'); ?></p>
+							<span class="link"><a href="<?php echo url_for('domain_profiles/index'); ?>"><?php echo __('Start the domain registration process &raquo;'); ?></a></span>
 						</li>
 						<li class="last">
 							<span class="icon"><img src="/img/statistics/get-analytics.png" alt="" title=""></span>
-							<h3>Get Analytics</h3>
-							<h4>Watch your spread analytics</h4>
-							<p>Spreadly offers a new advertising format. Advertisers pay and book an exact range. The linking of commercial message and voucher as thanks for the recommendation makes the ad attractive to the user. They are well aware of the ad message because it appears right after the focused like.</p>
-							<span class="link"><a href="<?php echo url_for('analytics/index'); ?>">Watch your detailled site analytics &raquo;</a></span>
+							<h3><?php echo __('Social Media Analytics'); ?></h3>
+							<h4><?php echo __('Vorteile für Seitenbetreiber'); ?></h4>
+							<p><?php echo __('Mit Spreadly erfährst Du alles was in Sachen Empfehlungsmarketing wichtig ist. Wir zeigen Die wie, ob und dank wem Dein Empfehlungsmarketing funktioniert.'); ?></p>
+							<span class="link"><a href="<?php echo url_for('analytics/index'); ?>"><?php echo __('Watch your detailled site analytics &raquo;'); ?></a></span>
 						</li>
 					</ul>
 				</section>
