86d85d1ddda03d1f5a6bf13e5b8b8ea03bb61da8
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2011-12-20 12:43:13 +0000

    layout

--- apps/statistics/modules/deals/templates/_js_init_deals.js.php
+++ apps/statistics/modules/deals/templates/_js_init_deals.js.php
@@ -8,7 +8,7 @@ i18n.init({
 });
 
 
-jQuery("input[type='checkbox']").custCheckBox();
+//jQuery("input[type='checkbox']").custCheckBox();
 //jQuery('.mirror-change-value').mirrorChangeValue();
 //Deal.init();
 //DealTable.init();
