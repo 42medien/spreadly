4e32d166bbb8804afe7a5d4255d2eda9177d33c2
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-01-10 10:24:13 +0000

    added links

--- apps/statistics/templates/_spreadly_references.php
+++ apps/statistics/templates/_spreadly_references.php
@@ -1,12 +1,12 @@
 <section class="spread-refrance">
 	<h3>Spreadly Referenzen</h3>
 	<ul class="clearfix">
-		<li><a href="#" title="Mailing Work"><img src="/img/statistics/ref-img1.jpg" alt="Mailing Work" title="Mailing Work"></a></li>
-		<li><a href="#" title="Abacho"><img src="/img/statistics/ref-img2.jpg" alt="Abacho" title="Abacho"></a></li>
-		<li><a href="#" title="Pressehof"><img src="/img/statistics/ref-img3.jpg" alt="Pressehof" title="Pressehof"></a></li>
-		<li><a href="#" title="Business"><img src="/img/statistics/ref-img4.jpg" alt="Business" title="Business"></a></li>
-		<li><a href="#" title="PR blogger"><img src="/img/statistics/ref-img5.jpg" alt="PR blogger" title="PR blogger"></a></li>
-		<li><a href="#" title="ED Netz"><img src="/img/statistics/ref-img6.jpg" alt="ED Netz" title="ED Netz"></a></li>
+		<li><a href="http://www.mailingwork.de/" title="Mailing Work" target="_blank"><img src="/img/statistics/ref-img1.jpg" alt="Mailing Work" title="Mailing Work"></a></li>
+		<li><a href="http://www.abacho.de/" title="Abacho" target="_blank"><img src="/img/statistics/ref-img2.jpg" alt="Abacho" title="Abacho"></a></li>
+		<li><a href="http://www.pressehof.de/" title="Pressehof" target="_blank"><img src="/img/statistics/ref-img3.jpg" alt="Pressehof" title="Pressehof"></a></li>
+		<li><a href="http://www.ibusiness.de/" title="Business" target="_blank"><img src="/img/statistics/ref-img4.jpg" alt="Business" title="Business"></a></li>
+		<li><a href="http://pr-blogger.de/" title="PR blogger" target="_blank"><img src="/img/statistics/ref-img5.jpg" alt="PR blogger" title="PR blogger"></a></li>
+		<li><a href="http://www.ednetz.de/" title="ED Netz" target="_blank"><img src="/img/statistics/ref-img6.jpg" alt="ED Netz" title="ED Netz"></a></li>
 	</ul>
 	<span class="more"><a href="<?php echo url_for('@customer');?>" title="See More Spread.ly costumers &raquo;">See More Spread.ly costumers &raquo;</a></span>
 </section>
