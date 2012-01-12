2a6458475cd95c739cdc2459ab555d5272c5da84
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-01-12 09:11:48 +0000

    text

--- plugins/sfForkedDoctrineApplyPlugin/modules/sfApply/templates/resetRequestSuccess.php
+++ plugins/sfForkedDoctrineApplyPlugin/modules/sfApply/templates/resetRequestSuccess.php
@@ -6,11 +6,11 @@
 	<section class="container_12">
 		<div class="sf_apply sf_apply_reset_request">
 
-		<h2 class="green_style"><?php echo __("Reset Password", array(), 'sfForkedApply') ?></h2>
+		<h2 class="green_style"><?php echo __("Passwort zurücksetzen", array(), 'sfForkedApply') ?></h2>
 		<form method="POST" action="<?php echo url_for('sfApply/resetRequest') ?>" name="sf_apply_reset_request" id="sf_apply_reset_request">
 
 		<p>
-			<?php echo __('Forgot your username or password? No problem! Just enter your username <strong>or</strong>your email address and click "Reset My Password." You will receive an email message containing both your username and a link permitting you to change your password if you wish.', array(), 'sfForkedApply') ?>
+			<?php echo __('Username oder Passwort vergessen? Kein Problem! Einfach Benutzername oder Email-Adresse eingeben und bestätigen und wir schicken ihnen eine Email mit ihrem Benutzernamen und einem Link zum Zurücksetzen des Passworts.', array(), 'sfForkedApply') ?>
 		</p>
 
 		<table>
@@ -20,8 +20,8 @@
 		  <tfoot>
 		    <tr>
 		      <td id="forgot-pw-button-row" colspan="2">
-		        <button class="blue-btn alignright" type="submit"><span><?php echo __("Reset My Password", array(), 'sfForkedApply') ?></span></button>
-		        <?php echo link_to('<span>'.__("Cancel", array(), 'sfForkedApply').'</span>', sfConfig::get('app_sfApplyPlugin_after', '@homepage'), array('class' => 'blue-btn alignright')) ?>
+		        <button class="blue-btn alignright" type="submit"><span><?php echo __("Passwort zurücksetzen", array(), 'sfForkedApply') ?></span></button>
+		        <?php echo link_to('<span>'.__("Abbrechen", array(), 'sfForkedApply').'</span>', sfConfig::get('app_sfApplyPlugin_after', '@homepage'), array('class' => 'blue-btn alignright')) ?>
 		      </td>
 		    </tr>
 		  </tfoot>
