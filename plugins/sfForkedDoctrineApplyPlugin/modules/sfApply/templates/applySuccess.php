4035ec9cbd20fa990f95c5e3b0626f9dd4f9259e
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-01-12 09:07:08 +0000

    text

--- plugins/sfForkedDoctrineApplyPlugin/modules/sfApply/templates/applySuccess.php
+++ plugins/sfForkedDoctrineApplyPlugin/modules/sfApply/templates/applySuccess.php
@@ -11,7 +11,7 @@
 	<section class="container_12">
 
 		<div class="sf_apply sf_apply_apply">
-		  <h2 class="verifytitle"><?php echo __("Apply for an Account", array(), 'sfForkedApply') ?></h2>
+		  <h2 class="verifytitle"><?php echo __("Registrierung", array(), 'sfForkedApply') ?></h2>
 
 		  <form method="post" action="<?php echo url_for('sfApply/apply') ?>" name="sf_apply_apply_form" id="sf_apply_apply_form">
 		  <table>
@@ -21,8 +21,8 @@
 		    <tfoot>
 		      <tr>
 		        <td colspan="2" id="signup-button-row">
-		            <button type="submit" class="blue-btn alignright"><?php echo __("Create My Account", array(), 'sfForkedApply') ?></button>
-		            <?php echo link_to(__("Cancel", array(), 'sfForkedApply'), sfConfig::get('app_sfApplyPlugin_after', '@homepage'), array('class' => 'blue-btn alignright')) ?>
+		            <button type="submit" class="blue-btn alignright"><?php echo __("Weiter", array(), 'sfForkedApply') ?></button>
+		            <?php echo link_to(__("Abbrechen", array(), 'sfForkedApply'), sfConfig::get('app_sfApplyPlugin_after', '@homepage'), array('class' => 'blue-btn alignright')) ?>
 		        </td>
 		      </tr>
 		    </tfoot>
