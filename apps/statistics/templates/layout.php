1aadeacbc2ca66d170e352591ae53040bf182d2d
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-01-12 11:15:44 +0000

    google analytics

--- apps/statistics/templates/layout.php
+++ apps/statistics/templates/layout.php
@@ -127,6 +127,18 @@
         <?php include_slot('js_document_ready'); ?>
       <?php } ?>
     });
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-24814204-1']);
+    _gaq.push(['_setDomainName', 'spreadly.com']);
+    _gaq.push(['_setAllowHash', 'false']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
 	</script>
 	<img id="general-ajax-loader" style="display:none;" src="/img/global/ajax-loader-bar-circle.gif" />
 </body>
