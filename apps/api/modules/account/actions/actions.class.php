b0ceaebb454935144a5813666ea55708b30cb613
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-01-19 16:18:38 +0000

    api

---
+++ apps/api/modules/account/actions/actions.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * account actions.
+ *
+ * @package    yiid
+ * @subpackage account
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class accountActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeCreate(sfWebRequest $request) {
+    $this->getResponse()->setContentType('application/json');
+    $this->setLayout(false);
+
+    // first api only accepts posts
+    if ($request->getMethod() != "POST") {
+      $this->getResponse()->setStatusCode(405);
+      return $this->renderPartial("wrong_method");
+    }
+
+
+  }
+}
---
+++ apps/api/modules/account/templates/_wrong_method.php
@@ -0,0 +1,1 @@
+<?php echo json_encode(array("error" => array("code" => 405, "message" => "API only supports post-requests"))); ?>
