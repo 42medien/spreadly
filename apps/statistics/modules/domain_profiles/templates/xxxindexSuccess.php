67b5e1228293a1dc57840a768a34c47265423f27
karina <karina@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-02-14 14:10:10 +0000

    delete old templates

--- apps/statistics/modules/domain_profiles/templates/xxxindexSuccess.php
+++
@@ -1,9 +0,0 @@
-<?php use_helper('Date') ?>
-<div id="add-domain-profiles">
-  <?php include_partial('domain_profiles/form', array('form'=> $form)); ?>
-</div>
-
-<?php //slot('content') ?>
-<?php include_partial('domain_profiles/domain_profiles_table', array('domain_profiles' => $domain_profiles)); ?>
-<?php //end_slot(); ?>
-<?php //include_partial('global/graybox'); ?>
