0d221e4fcfa28ccbd3e0ad8405ab4ed848c07e7d
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-05-14 09:26:20 +0000

    added flattr import client

---
+++ lib/utils/api-clients/import-clients/FlattrImportClient.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * class to import twitter contacts
+ *
+ * @author Matthias Pfefferle
+ * @author Karina Mies
+ */
+class FlattrImportClient {
+  /**
+   * import twitter contacts
+   *
+   * @author Matthias Pfefferle
+   * @author Karina Mies
+   */
+  public static function importContacts($pOnlineIdentity) {
+    // so long...
+    // and thanks for the fish!
+  }
+}
