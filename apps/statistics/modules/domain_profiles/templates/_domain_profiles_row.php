b98651b0c278e7e97cadd0b47a7658a0bfd50012
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-07-09 15:38:28 +0000

    new domain profile table (added flattr)

--- apps/statistics/modules/domain_profiles/templates/_domain_profiles_row.php
+++ apps/statistics/modules/domain_profiles/templates/_domain_profiles_row.php
@@ -1,4 +1,5 @@
 <?php use_helper('Date', 'DomainProfiles'); ?>
-	<tr id="domain-profile-row-<?php echo $domain_profile->getId(); ?>" class="<?php echo ($domain_profile->getState() == DomainProfileTable::STATE_PENDING)? "unverifiedrow":""; ?> ">
-		<?php include_partial('domain_profiles/domain_profiles_row_content', array('domain_profile' => $domain_profile));?>
-	</tr>
+
+<tr id="domain-profile-row-<?php echo $domain_profile->getId(); ?>" class="<?php echo ($domain_profile->getState() == DomainProfileTable::STATE_PENDING)? "unverifiedrow":""; ?> ">
+  <?php include_partial('domain_profiles/domain_profiles_row_content', array('domain_profile' => $domain_profile));?>
+</tr>
