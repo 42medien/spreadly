a9d5ba3d97ab716fa8b3673833a7e2c74a3f4c5c
matthias <matthias@70ff4a32-ba2f-4920-8d78-daa59c3f94b2>
2012-07-11 09:37:49 +0000

    flattr support

--- apps/widget/modules/like/actions/components.class.php
+++ apps/widget/modules/like/actions/components.class.php
@@ -3,6 +3,8 @@ class likeComponents extends sfComponents {
 
 	public function executeShare_section(sfWebRequest $request) {
     $this->pIdentities = OnlineIdentityTable::getPublishingEnabledByUserId($this->getUser()->getUserId());
+
+    $this->domain_profile = DomainProfileTable::getInstance()->retrieveByUrl($request->getParameter("url", null));
   }
 }
 ?>
