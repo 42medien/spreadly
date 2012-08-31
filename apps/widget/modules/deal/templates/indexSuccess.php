6f839998a705a66c7b6d85ce3758daaf3220f9f6
Matthias Pfefferle <pfefferle@gmail.com>
2012-08-31 13:02:17 +0200

    new deal template

--- apps/widget/modules/deal/templates/indexSuccess.php
+++ apps/widget/modules/deal/templates/indexSuccess.php
@@ -2,46 +2,42 @@
 use_helper('Avatar', 'Text');
 ?>
 
-<header>
-	<h1 class="success"><?php echo __('Thanks for sharing!'); ?></h1>
-	<span id="deal-marker"><?php echo __('Grab your deal!'); ?></span>
-	<h2><?php echo $deal->getMotivationTitle(); ?><span id="motivation"><?php echo $deal->getMotivationText(); ?></span></h2>
-</header>
-
-<!-- weisser Content -->
-<div id="content-inner" class="clearfix deal-content-inner">
-
-    <div class="clearfix">
-      <p class="area-like-comment-mirror"></p>
-      <div id="like-select-img" class="alignleft">
-        <div class="scrollable bordered-light shadow-light" id="myscroll">
-          <!-- root element for the items -->
-          <div class="items alignleft" id="scroll-meta-images">
-            <img id="meta-img" src="<?php echo $deal->getSpreadImg(); ?>" width="80" />
-          </div>
-        </div>
-      </div>
-      <div class="alignleft clearfix" id="like-content">
-        <h4 title="<?php echo $deal->getSpreadTitle(); ?>">
-        <?php echo truncate_text($deal->getSpreadTitle(), 50); ?>
-        </h4>
-        <p>
-          <?php echo truncate_text($deal->getSpreadText(), 150); ?>
-        </p>
-        <p title="<?php echo $deal->getSpreadUrl(); ?>">
-          <?php echo truncate_text($deal->getSpreadUrl(), 50); ?>
-        </p>
-      </div>
-    </div>
-
-  <form action="" name="popup-like-form" id="popup-like-form" class="deal-form" method="post">
-    <div id="comment-area" class="clearfix deal-comment">
-      <textarea id="area-like-comment" class="mirror-value bordered gradient shadow-wide" name="like[comment]" placeholder="<?php echo __("add your comment (optional) ..."); ?> <?php echo __('Feel free to add some hashtags, for example:'); ?> #deal"></textarea>
-      <span id="comment-area-counter-box"><span id="area-like-comment_counter">0</span> <?php echo __('Zeichen'); ?></span>
-    </div>
-    <div id="like-submit">
-    	<?php include_component("like", "share_section"); ?>
-    </div>
-    <input type="hidden" name="like[d_id]" id="deal-img-value" value="<?php echo $deal->getId(); ?>" />
-  </form>
+<div class="title">
+  <h1><?php echo __("Your share &amp; bookmark was successful!"); ?></h1>
 </div>
+<div class="clear"></div>
+
+<div class="red_bg_sh">
+  <div class="red_bg sh_content">
+  <div class="sh_img_block scrollable" id="myscroll">
+    <div id="meta-images" class="imgbg items">
+      <img id="meta-img" src="<?php echo $deal->getSpreadImg(); ?>" width="80" />
+    </div>
+  </div>
+  <div class="sh_info">
+    <h2 title="<?php echo $deal->getSpreadTitle(); ?>"><?php echo truncate_text($deal->getSpreadTitle(), 50); ?></h2>
+    <p>
+      <?php echo truncate_text($deal->getSpreadText(), 150); ?>
+    </p>
+    <p><a href="<?php echo $deal->getSpreadUrl(); ?>" target="_blank" title="<?php echo $deal->getSpreadUrl(); ?>">
+      <?php echo truncate_text($deal->getSpreadUrl(), 50); ?></a></p>
+  </div>
+  <div class="clear"></div>
+  <div class="pers_comments">
+    <textarea id="texarea_count" rows="2" cols="20" placeholder="<?php echo __("add your comment (optional) ..."); ?> <?php echo __('Feel free to add some hashtags, for example:'); ?> #like"></textarea>
+    <div class="clear"></div>
+    <span id="chars_counter">0</span>
+  </div>
+  </div>
+</div>
+
+<form action="" name="popup-like-form" id="popup-like-form" class="deal-form" method="post">
+<div class="sh_networks">
+  <div class="gradient">
+    <h1>Choose the networks you want to post to</h1>
+    <?php include_component("like", "share_section", array('pError' => $pError)); ?>
+    <input type="hidden" name="like[d_id]" id="deal-img-value" value="<?php echo $deal->getId(); ?>" />
+    <div class="clear"></div>
+  </div>
+</div>
+</form>
