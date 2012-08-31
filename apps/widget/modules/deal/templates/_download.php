a89035cc347eca985cce17f0b93650a35115304c
Matthias Pfefferle <pfefferle@gmail.com>
2012-08-31 16:54:22 +0200

    reformatted

--- apps/widget/modules/deal/templates/_download.php
+++ apps/widget/modules/deal/templates/_download.php
@@ -5,15 +5,15 @@
   </p>
 </div>
 <div class="voucher">
-    <div class="voucher_brd">
-        <div class="voucher_data">
-            <p class="percent"><?php echo $deal->getCouponTitle(); ?></p>
-            <p class="song"><?php echo $deal->getCouponText(); ?></p>
-            <div class="btn">
-                <span>
-                    <a href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Download"); ?></a>
-                </span>
-            </div>                   
-        </div>
-    </div>                   
+  <div class="voucher_brd">
+    <div class="voucher_data">
+      <p class="percent"><?php echo $deal->getCouponTitle(); ?></p>
+      <p class="song"><?php echo $deal->getCouponText(); ?></p>
+      <div class="btn">
+        <span>
+          <a href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Download"); ?></a>
+        </span>
+      </div>                   
+    </div>
+  </div>                   
 </div>
--- apps/widget/modules/deal/templates/_unique_code.php
+++ apps/widget/modules/deal/templates/_unique_code.php
@@ -7,18 +7,18 @@
 <div class="voucher">
   <div class="voucher_brd">
     <div class="voucher_data">
-            <p class="offer"><?php echo $deal->getCouponTitle(); ?></p>
-            <a href="<?php echo $deal->getCouponRedeemUrl(); ?>" target="_blank"><?php echo __("Visit the shop"); ?></a>
-            <div class="v_code">
-                <input type="text" value="<?php echo $sf_request->getParameter("u_code", "error"); ?>" />
-                <div class="btn">
-                    <span>
-                        <span>copy code</span>
-                    </span>
-                </div>
-                <div class="clear"></div>
-            </div>
-            <p class="bt_text"><?php echo $deal->getCouponText(); ?></p>
+      <p class="offer"><?php echo $deal->getCouponTitle(); ?></p>
+      <a href="<?php echo $deal->getCouponRedeemUrl(); ?>" target="_blank"><?php echo __("Visit the shop"); ?></a>
+      <div class="v_code">
+        <input type="text" value="<?php echo $sf_request->getParameter("u_code", "error"); ?>" />
+        <div class="btn">
+          <span>
+            <span>copy code</span>
+          </span>
         </div>
+        <div class="clear"></div>
+      </div>
+      <p class="bt_text"><?php echo $deal->getCouponText(); ?></p>
     </div>
+  </div>
 </div>
