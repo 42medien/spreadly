a705a68c489b295533eb8d24540fbef4b6d85a79
Matthias Pfefferle <pfefferle@gmail.com>
2012-09-03 11:29:38 +0200

    fixed link buttons

--- apps/widget/modules/deal/templates/_download.php
+++ apps/widget/modules/deal/templates/_download.php
@@ -7,11 +7,11 @@
 <div class="voucher">
   <div class="voucher_brd">
     <div class="voucher_data">
-      <p class="percent"><?php echo $deal->getCouponTitle(); ?></p>
+      <p class="offer"><?php echo $deal->getCouponTitle(); ?></p>
       <p class="song"><?php echo $deal->getCouponText(); ?></p>
       <div class="btn">
         <span>
-          <a href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Download"); ?></a>
+          <span><a class="link" href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Download"); ?></a></span>
         </span>
       </div>                   
     </div>
--- apps/widget/modules/deal/templates/_url.php
+++ apps/widget/modules/deal/templates/_url.php
@@ -7,11 +7,11 @@
 <div class="voucher">
   <div class="voucher_brd">
     <div class="voucher_data">
-      <p class="percent"><?php echo $deal->getCouponTitle(); ?></p>
+      <p class="offer"><?php echo $deal->getCouponTitle(); ?></p>
       <p class="song"><?php echo $deal->getCouponText(); ?></p>
       <div class="btn">
         <span>
-          <a class="link B" href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Go to the reward website"); ?></a>
+          <span><a class="link" href="<?php echo $deal->getCouponUrl(); ?>" target="_blank"><?php echo __("Visit"); ?></a></span>
         </span>
       </div>                   
     </div>
