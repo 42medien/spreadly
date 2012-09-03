d02486c2130ee88010ce256e3d2f528bc040129e
Matthias Pfefferle <pfefferle@gmail.com>
2012-09-03 10:12:20 +0200

    removed settings link

--- apps/widget/templates/_footer.php
+++ apps/widget/templates/_footer.php
@@ -5,7 +5,7 @@
   <div class="footer_shadow"></div>
     <span class="left">
       <?php if ($sf_user->isAuthenticated()) { ?>
-      <span title="<?php echo $sf_user->getUser()->getUsername(); ?>" id="nav-username"><?php echo __('Hi'); ?> <a id="edit-settings-link" href="<?php echo url_for('@widget_settings'); ?>"><?php echo truncate_text($sf_user->getUser()->getUsername(), 10); ?></a></span> |
+      <span title="<?php echo $sf_user->getUser()->getUsername(); ?>" id="nav-username"><?php echo __('Hi'); ?> <?php echo truncate_text($sf_user->getUser()->getUsername(), 10); ?></span> |
       <!-- li><?php echo link_to(__('Likes'), '@widget_likes'); ?></li>
       <li><?php echo link_to(__('Deals'), '@widget_deals'); ?></li-->
       <?php echo link_to(__('Logout'), '@signout'); ?>
