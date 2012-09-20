ceb72175bcf7475ef608d1143cda494500a6b8e4
pfefferle <pfefferle@gmail.com>
2012-09-20 12:07:08 +0200

    added new advertisers

--- apps/widget/modules/api/actions/actions.class.php
+++ apps/widget/modules/api/actions/actions.class.php
@@ -31,7 +31,7 @@ class apiActions extends sfActions {
     $url = $request->getParameter("url");
     $domain = parse_url($url, PHP_URL_HOST);
     
-    switch($domain) {
+    switch(strtolower($domain)) {
       case ("deraktionaer.de"):
       case ("www.deraktionaer.de"):
         $this->setTemplate("deraktionaer_ad");
@@ -41,88 +41,105 @@ class apiActions extends sfActions {
       case ("www.yigg.de"):
         $this->setTemplate("yigg_ad");
         break;
-      
-      case: ("urlaubspartner.net"):
-      case: ("www.urlaubspartner.net"):
+
+      case ("urlaubspartner.net"):
+      case ("www.urlaubspartner.net"):
         $this->setTemplate("urlaubspartner_ad");
         break;
+
+      case ("docinsider.de"):
+      case ("www.docinsider.de"):
+        $this->setTemplate("docinsider_ad");
+        break;
       
-      case: ("istlokal.de"):
-      case: ("www.istlokal.de"):
-      case: ("16vor.de"):
-      case: ("www.16vor.de"):
-      case: ("dippolds.info"):
-      case: ("www.dippolds.info"):
-      case: ("dahogn.de"):
-      case: ("www.dahogn.de"):
-      case: ("dossenheimblog.de"):
-      case: ("www.dossenheimblog.de"):
-      case: ("ebersberger-nachrichten.de"):
-      case: ("www.ebersberger-nachrichten.de"):
-      case: ("edingenneckarhausenblog.de"):
-      case: ("www.edingenneckarhausenblog.de"):
-      case: ("eichwalder-nachrichten.de"):
-      case: ("www.eichwalder-nachrichten.de"):
-      case: ("ferienstimme.de"):
-      case: ("www.ferienstimme.de"):
-      case: ("fuerther-freiheit.info"):
-      case: ("www.fuerther-freiheit.info"):
-      case: ("heddesheimblog.de"):
-      case: ("www.heddesheimblog.de"):
-      case: ("hemsbachblog.de"):
-      case: ("www.hemsbachblog.de"):
-      case: ("hirschbergblog.de"):
-      case: ("www.hirschbergblog.de"):
-      case: ("homberger-hingucker.de"):
-      case: ("www.homberger-hingucker.de"):
-      case: ("ilvesheimblog.de"):
-      case: ("www.ilvesheimblog.de"):
-      case: ("klaerwerk-blog.de"):
-      case: ("www.klaerwerk-blog.de"):
-      case: ("ladenburgblog.de"):
-      case: ("www.ladenburgblog.de"):
-      case: ("laudenbachblog.de"):
-      case: ("www.laudenbachblog.de"):
-      case: ("leer-meinung.de"):
-      case: ("www.leer-meinung.de"):
-      case: ("mecklenburgerstimme.de"):
-      case: ("www.mecklenburgerstimme.de"):
-      case: ("mittelhessenblog.de"):
-      case: ("www.mittelhessenblog.de"):
-      case: ("nadr.de"):
-      case: ("www.nadr.de"):
-      case: ("nrwz.de"):
-      case: ("www.nrwz.de"):
-      case: ("pottblog.de"):
-      case: ("www.pottblog.de"):
-      case: ("prenzlauerberg-nachrichten.de"):
-      case: ("www.prenzlauerberg-nachrichten.de"):
-      case: ("regensburg-digital.de"):
-      case: ("www.regensburg-digital.de"):
-      case: ("rheinneckarblog.de"):
-      case: ("www.rheinneckarblog.de"):
-      case: ("rhein-onliner.de"):
-      case: ("www.rhein-onliner.de"):
-      case: ("freiehonnefer.de"):
-      case: ("www.freiehonnefer.de"):
-      case: ("rostockerjournal.de"):
-      case: ("www.rostockerjournal.de"):
-      case: ("rothenburg.info"):
-      case: ("www.rothenburg.info"):
-      case: ("ruhrbarone.de"):
-      case: ("www.ruhrbarone.de"):
-      case: ("schriesheimblog.de"):
-      case: ("www.schriesheimblog.de"):
-      case: ("schweinfurter-echo.de"):
-      case: ("www.schweinfurter-echo.de"):
-      case: ("singold-bote.de"):
-      case: ("www.singold-bote.de"):
-      case: ("solinger-bote.de"):
-      case: ("www.solinger-bote.de"):
-      case: ("tegernseerstimme.de"):
-      case: ("www.tegernseerstimme.de"):
-      case: ("viernheimblog.de"):
-      case: ("www.viernheimblog.de"):
+      case ("istlokal.de"):
+      case ("www.istlokal.de"):
+      case ("16vor.de"):
+      case ("www.16vor.de"):
+      case ("dippolds.info"):
+      case ("www.dippolds.info"):
+      case ("dahogn.de"):
+      case ("www.dahogn.de"):
+      case ("dossenheimblog.de"):
+      case ("www.dossenheimblog.de"):
+      case ("ebersberger-nachrichten.de"):
+      case ("www.ebersberger-nachrichten.de"):
+      case ("edingenneckarhausenblog.de"):
+      case ("www.edingenneckarhausenblog.de"):
+      case ("eichwalder-nachrichten.de"):
+      case ("www.eichwalder-nachrichten.de"):
+      case ("ferienstimme.de"):
+      case ("www.ferienstimme.de"):
+      case ("fuerther-freiheit.info"):
+      case ("www.fuerther-freiheit.info"):
+      case ("heddesheimblog.de"):
+      case ("www.heddesheimblog.de"):
+      case ("hemsbachblog.de"):
+      case ("www.hemsbachblog.de"):
+      case ("hirschbergblog.de"):
+      case ("www.hirschbergblog.de"):
+      case ("homberger-hingucker.de"):
+      case ("www.homberger-hingucker.de"):
+      case ("ilvesheimblog.de"):
+      case ("www.ilvesheimblog.de"):
+      case ("klaerwerk-blog.de"):
+      case ("www.klaerwerk-blog.de"):
+      case ("ladenburgblog.de"):
+      case ("www.ladenburgblog.de"):
+      case ("laudenbachblog.de"):
+      case ("www.laudenbachblog.de"):
+      case ("leer-meinung.de"):
+      case ("www.leer-meinung.de"):
+      case ("mecklenburgerstimme.de"):
+      case ("www.mecklenburgerstimme.de"):
+      case ("mittelhessenblog.de"):
+      case ("www.mittelhessenblog.de"):
+      case ("nadr.de"):
+      case ("www.nadr.de"):
+      case ("nrwz.de"):
+      case ("www.nrwz.de"):
+      case ("pottblog.de"):
+      case ("www.pottblog.de"):
+      case ("prenzlauerberg-nachrichten.de"):
+      case ("www.prenzlauerberg-nachrichten.de"):
+      case ("regensburg-digital.de"):
+      case ("www.regensburg-digital.de"):
+      case ("rheinneckarblog.de"):
+      case ("www.rheinneckarblog.de"):
+      case ("rhein-onliner.de"):
+      case ("www.rhein-onliner.de"):
+      case ("freiehonnefer.de"):
+      case ("www.freiehonnefer.de"):
+      case ("rostockerjournal.de"):
+      case ("www.rostockerjournal.de"):
+      case ("rothenburg.info"):
+      case ("www.rothenburg.info"):
+      case ("ruhrbarone.de"):
+      case ("www.ruhrbarone.de"):
+      case ("schriesheimblog.de"):
+      case ("www.schriesheimblog.de"):
+      case ("schweinfurter-echo.de"):
+      case ("www.schweinfurter-echo.de"):
+      case ("singold-bote.de"):
+      case ("www.singold-bote.de"):
+      case ("solinger-bote.de"):
+      case ("www.solinger-bote.de"):
+      case ("tegernseerstimme.de"):
+      case ("www.tegernseerstimme.de"):
+      case ("viernheimblog.de"):
+      case ("www.viernheimblog.de"):
+      case ("weilburger-nachrichten.de"):
+      case ("www.weilburger-nachrichten.de"):
+      case ("weinheimblog.de"):
+      case ("www.weinheimblog.de"):
+      case ("weiterstadtnetz.de"):
+      case ("www.weiterstadtnetz.de"):
+      case ("wendland-net.de"):
+      case ("www.wendland-net.de"):
+      case ("wir-im-vorgebirge.de"):
+      case ("www.wir-im-vorgebirge.de"):
+      case ("schiebener.eu"):
+      case ("www.schiebener.eu"):
         $this->setTemplate("istlokal_ad");
         break;
     }
