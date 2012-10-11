c26b049ccd2609d514618abccd106530e28039f3
pfefferle <pfefferle@gmail.com>
2012-10-11 09:35:30 +0200

    changed loading mechanism of the spreadly button

--- web/js/api_sources/loader.js
+++ web/js/api_sources/loader.js
@@ -1,19 +1,21 @@
-(function() {
-  var head = document.getElementsByTagName("head")[0];
+(function(w, d, s) {
+  var head = d.getElementsByTagName("head")[0];
   // adds css
-  var css = document.createElement('link');
-  css.type = 'text/css';
-  css.rel = 'stylesheet';
+  var css = d.createElement('link');
+  css.type = 'text/css'; css.rel = 'stylesheet';
   css.href = '##YIID_BUTTON_HOST##/css/v1/button.css';
   head.appendChild(css);
-  // adds button.js
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.src = '##YIID_BUTTON_HOST##/js/v1/button.js';
-  head.appendChild(s);
-  
-  var a = document.createElement('script');
-  a.type = 'text/javascript';
-  a.src = '##YIID_BUTTON_HOST##/js/v1/advertisement.js';
-  head.appendChild(a);
-})();
+
+  function go(){
+    var js, fjs = d.getElementsByTagName(s)[0], load = function(url, id) {
+	    if (d.getElementById(id)) {return;}
+	    js = d.createElement(s); js.src = url; js.id = id;
+	    fjs.parentNode.insertBefore(js, fjs);
+	  };
+    load('##YIID_BUTTON_HOST##/js/v1/button.js', 'spreadly_button');
+    load('##YIID_BUTTON_HOST##/js/v1/advertisement.js', 'spreadly_ads');
+    load('##YIID_BUTTON_HOST##/css/v1/button.css', 'spreadly_css');
+  }
+  if (w.addEventListener) { w.addEventListener("load", go, false); }
+  else if (w.attachEvent) { w.attachEvent("onload",go); }
+} (window, document, 'script'));
