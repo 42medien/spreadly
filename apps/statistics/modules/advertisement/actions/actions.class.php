bd75a371f86affc864750f721dfd35df33cff169
pfefferle <pfefferle@gmail.com>
2012-10-15 13:54:00 +0200

    temp comit

---
+++ apps/statistics/modules/advertisement/actions/actions.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * advertisement actions.
+ *
+ * @package    yiid
+ * @subpackage advertisement
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class advertisementActions extends sfActions {
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request) {
+
+  }
+}
