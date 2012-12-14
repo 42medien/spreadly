e4c60ff7760ba849fee97bc9982c7483555186e7
pfefferle <pfefferle@gmail.com>
2012-12-14 16:44:25 +0100

    added "c" of crud

---
+++ apps/widget/modules/api/templates/adsSuccess.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+<style type="text/css">
+body { margin:0 auto; padding:0; border: 0; display: block; overflow: hidden; }
+</style>
+</head>
+<body><div style="margin: 0 auto; width: auto;"><?php echo $ad_code; ?></div></body>
+</html>
