959f54a0d0f8fd996b051efdba916de3918f17bf
pfefferle <pfefferle@gmail.com>
2012-12-14 16:45:11 +0100

    added ad_id to tell backend the required ad

--- web/w/like/javascript.php
+++ web/w/like/javascript.php
@@ -6,6 +6,6 @@ header("Content-type: text/javascript");
 include("./../../js/v1/button.js");
 ?>
 <?php if ($jsu->displayAd()) { ?>
-var spreadly_ad_height = <?php echo $jsu->getHeight(); ?>;var spreadly_ad_width = <?php echo $jsu->getWidth(); ?>; var spreadly_ad_mute = <?php echo $jsu->getMute(); ?>;
+var spreadly_ad_height = <?php echo $jsu->getHeight(); ?>; var spreadly_ad_width = <?php echo $jsu->getWidth(); ?>; var spreadly_ad_mute = <?php echo $jsu->getMute(); ?>; var spreadly_ad_id = "<?php echo addslashes($jsu->getAdId()); ?>";
 <?php include("./../../js/v1/advertisement.js"); ?>
 <?php } ?>
