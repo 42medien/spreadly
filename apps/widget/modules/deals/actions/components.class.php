a00c0261fbdc564fc2a32a38700bd43e470a9aa5
pfefferle <pfefferle@gmail.com>
2013-01-10 11:57:30 +0100

    deleted old api calls

--- apps/widget/modules/deals/actions/components.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class dealsComponents extends sfComponents {
-
-	public function executeCoupon_used(sfWebRequest $request) {
-	  $dm = MongoManager::getDM();
-		$this->pActivity = $dm->getRepository("Documents\YiidActivity")->find($this->pActivityId);
-  }
-}
-?>
