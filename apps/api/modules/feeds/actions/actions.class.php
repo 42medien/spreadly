e01a2ea79e63a2e41dd1279777a80cd2f27d29a2
pfefferle <pfefferle@gmail.com>
2013-01-28 15:36:14 +0100

    some api mods

--- apps/api/modules/feeds/actions/actions.class.php
+++ apps/api/modules/feeds/actions/actions.class.php
@@ -9,6 +9,11 @@
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class feedsActions extends sfActions {
+  public function executeIndex() {
+    $this->getResponse()->setHttpHeader("Content-Type", 'text/xml');
+    $this->setLayout(false);
+  }
+
   public function executeGlobal() {
     $lDm = MongoManager::getDM();
     $this->activities = $lDm->getRepository("Documents\YiidActivity")->findAll()->sort(array("c" => -1))->limit(100);
---
+++ apps/api/modules/feeds/templates/indexSuccess.php
@@ -0,0 +1,13 @@
+<?php echo '<?xml version="1.0" encoding="ISO-8859-1"?>'. "\n"; ?>
+<opml version="2.0">
+	<head>
+		<title>Spreadly: public feeds</title>
+		<dateCreated>Sat, 18 Jun 2005 12:11:52 GMT</dateCreated>
+		<dateModified>Tue, 02 Aug 2005 21:42:48 GMT</dateModified>
+		<ownerName>Spreadly</ownerName>
+		<ownerEmail>info@spreadly.com</ownerEmail>
+	</head>
+	<body>
+		<outline text="global share stream" description="Spreadlys global share stream" htmlUrl="http://spreadly.com/" language="unknown" title="CNET News.com" type="atom" version="atom" xmlUrl="<?php echo url_for("feeds/global", true); ?>"/>
+	</body>
+</opml>
