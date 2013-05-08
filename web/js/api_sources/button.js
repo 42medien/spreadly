8f380174b343d3aa4999d01a9045bd08821f7dfd
pfefferle <pfefferle@gmail.com>
2013-05-08 10:30:03 +0200

    fixed encoding problem!

--- web/js/api_sources/button.js
+++ web/js/api_sources/button.js
@@ -69,7 +69,7 @@ function spreadly_init_click_handler(link) {
   
 function spreadly_open_layer(url) {
   if ( (window.screen && window.screen.width && window.screen.width < 600) || (window.innerWidth < 600) ) {
-    window.open('http://##YIID_WIDGET_HOST##/?url='+decodeURIComponent(url)+'&iframe=1','spreadly-popup');
+    window.open('http://##YIID_WIDGET_HOST##/?url='+encodeURIComponent(url)+'&iframe=1','spreadly-popup');
   } else {
     spreadly_insert_iframe(url);
     document.getElementById('spreadly-overlay').style.visibility = 'visible';
@@ -79,7 +79,7 @@ function spreadly_open_layer(url) {
 }
     
 function spreadly_insert_iframe(url) {
-  document.getElementById("spreadly-iframe").insertAdjacentHTML('afterbegin', '<div class="spreadly-close-button" onclick="spreadly_close_layer(); return false;"><div class="spreadly-close-icon"></div></div><iframe src="http://##YIID_WIDGET_HOST##/?url='+decodeURIComponent(url)+'&iframe=1" style="width: 600px; height: 600px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
+  document.getElementById("spreadly-iframe").insertAdjacentHTML('afterbegin', '<div class="spreadly-close-button" onclick="spreadly_close_layer(); return false;"><div class="spreadly-close-icon"></div></div><iframe src="http://##YIID_WIDGET_HOST##/?url='+encodeURIComponent(url)+'&iframe=1" style="width: 600px; height: 600px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
   return false;
 }
 
