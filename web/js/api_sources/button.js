8d1acc2d0266368bc3869b4dfcc7f5610246caec
pfefferle <pfefferle@gmail.com>
2013-12-10 16:15:57 +0100

    flat button

--- web/js/api_sources/button.js
+++ web/js/api_sources/button.js
@@ -1,11 +1,11 @@
 /*
- * @nocombine test 
+ * @nocombine test
  */
 
 function spreadly_init_buttons() {
   var buttons = spreadly_get_elements_by_class_name(document,'spreadly-button');
   for (var i = 0; i < buttons.length; i++) {
-    spreadly_init_images(buttons[i]);
+    spreadly_init_services(buttons[i]);
     spreadly_init_click_handler(buttons[i]);
   }
 }
@@ -16,13 +16,14 @@ function spreadly_init_simple_buttons() {
     spreadly_init_click_handler(buttons[i]);
   }
 }
-  
-function spreadly_init_images(link) {
+
+function spreadly_init_services(link) {
   link.text = "";
   link.innerHTML = "";
   var services = ["fb", "tw", "li"];
   var data_services = link.getAttribute("data-services");
   var data_counter = link.getAttribute("data-counter");
+  var data_style = link.getAttribute("data-style");
   var available_services = {"facebook":"fb","twitter":"tw","linkedin":"li","tumblr":"tm","xing":"xg","flattr":"ft"};
   var url = document.location.href;
 
@@ -30,7 +31,7 @@ function spreadly_init_images(link) {
     services = [];
   } else if (data_services) {
     var data_services = data_services.split(",");
-    
+
     if (typeof(data_services)=='object' && (data_services instanceof Array)) {
       services = [];
       for( var i=0; i<data_services.length; i++ ) {
@@ -40,28 +41,38 @@ function spreadly_init_images(link) {
       }
     }
   }
-  
-  for (var i=0; i<services.length; i++) {
-    link.insertAdjacentHTML('beforeEnd', '<img src="##YIID_BUTTON_HOST##/img/button/28/'+services[i]+'.png" alt="" class="spreadly-service-icon" />');
+
+  if (data_style == "none" || data_style == null) {
+    data_style = "classic";
   }
-  
-  link.insertAdjacentHTML('beforeEnd', '<img src="##YIID_BUTTON_HOST##/img/button/28/sl.png" alt="" class="spreadly-service-icon" />');
+
+  for (var i=0; i<services.length; i++) {
+    link.insertAdjacentHTML('beforeEnd', spreadly_generate_service_item(services[i], data_style));
+  }
+
+  link.insertAdjacentHTML('beforeEnd',  spreadly_generate_service_item("sl", data_style));
 
   if (link.href) {
     url = link.href;
   } else if (link.getAttribute("data-spreadly-url")) {
     url = link.getAttribute("data-spreadly-url");
   }
-  
+
   if (data_counter == "none" || data_counter == null) {
-    link.insertAdjacentHTML('beforeEnd', '<iframe src="##YIID_BUTTON_HOST##/w/like/statistics.php?url='+encodeURIComponent(url)+'" style="width: 0px; height: 0px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
+    link.insertAdjacentHTML('beforeEnd', '<iframe src="##YIID_BUTTON_HOST##/w/like/statistics.php?url='+encodeURIComponent(url)+'&style='+data_style+'" style="width: 0px; height: 0px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
   } else {
-    
-    
-    link.insertAdjacentHTML('beforeEnd', '<iframe src="##YIID_BUTTON_HOST##/w/like/counter.php?url='+encodeURIComponent(url)+'" style="width: 50px; height: 22px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
+    link.insertAdjacentHTML('beforeEnd', '<iframe src="##YIID_BUTTON_HOST##/w/like/counter.php?url='+encodeURIComponent(url)+'&style='+data_style+'" style="width: 50px; height: 22px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
   }
 }
-  
+
+function spreadly_generate_service_item(service, style) {
+  if (style == "flat") {
+    return '<i class="si-'+service+'"></i>';
+  }
+
+  return '<img src="##YIID_BUTTON_HOST##/img/button/28/'+service+'.png" alt="" class="spreadly-service-icon" />';
+}
+
 function spreadly_init_click_handler(link) {
   var url = document.location.href;
 
@@ -70,12 +81,12 @@ function spreadly_init_click_handler(link) {
   } else if (link.getAttribute("data-spreadly-url")) {
     url = link.getAttribute("data-spreadly-url");
   }
-  
+
   link.onclick = function () { spreadly_open_layer(url); return false; };
-  
+
   return false;
 }
-  
+
 function spreadly_open_layer(url) {
   if ( (window.screen && window.screen.width && window.screen.width < 600) || (window.innerWidth < 600) ) {
     window.open('http://##YIID_WIDGET_HOST##/?url='+encodeURIComponent(url)+'&iframe=1','spreadly-popup');
@@ -86,7 +97,7 @@ function spreadly_open_layer(url) {
 
   return false;
 }
-    
+
 function spreadly_insert_iframe(url) {
   document.getElementById("spreadly-iframe").insertAdjacentHTML('afterbegin', '<div class="spreadly-close-button" onclick="spreadly_close_layer(); return false;"><div class="spreadly-close-icon"></div></div><iframe src="http://##YIID_WIDGET_HOST##/?url='+encodeURIComponent(url)+'&iframe=1" style="width: 600px; height: 600px; border:0;" frameborder="0" scrolling="no" marginheight="0" allowTransparency="true"></iframe>');
   return false;
@@ -143,7 +154,7 @@ function spreadly_get_cookie(name) {
     var p = cookie.indexOf("=");
     var n = cookie.substring(0,p);
     var v = cookie.substring(p+1);
-    
+
     if (n == name) {
       return decodeURIComponent(v);
     }
